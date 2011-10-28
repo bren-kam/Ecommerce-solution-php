d62fd58b7a57d79efba9f33e0bbfdc450ff95d71
Kerry Jones <kerry@studio98.com>
2011-10-28 12:15:53 -0700

    Fixed social media merge bug

--- account/theme/social-media/facebook/email-sign-up.php
+++ account/theme/social-media/facebook/email-sign-up.php
@@ -55,7 +55,7 @@ get_header();
 	<br clear="all" /><br />
 	<?php get_sidebar( 'social-media/' ); ?>
 	<div id="subcontent">
-		<?php if ( !isset( $email_sign_up['fb_page_id'] ) || 0 == $email_sign_up['fb_page_id'] ) { ?>
+		<?php if ( 0 == $email_sign_up['fb_page_id'] ) { ?>
 			<h2 class="title"><?php echo _('Step 1: Go to the Email Sign Up application.'); ?></h2>
 			<p><?php echo _('Go to the'); ?> <a href="http://www.facebook.com/apps/application.php?id=165553963512320" title="<?php echo _('Online Platform - Email Sign Up'); ?>" target="_blank"><?php echo _('Email Sign Up'); ?></a> <?php echo _('application page'); ?>.</p>
 			<br /><br />
