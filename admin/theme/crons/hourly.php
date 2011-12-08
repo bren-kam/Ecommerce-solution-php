05b82ceb897615b94b2052192bb3a84d6a8c4494
Kerry Jones <kerry@studio98.com>
2011-12-07 18:13:15 -0800

    More FB App Changes

--- admin/theme/crons/hourly.php
+++ admin/theme/crons/hourly.php
@@ -9,4 +9,27 @@ $e->update_scheduled_emails();
 
 $t = new Tickets;
 $t->clean_uploads();
+
+// Send Autoposts
+$sm = new Social_Media;
+
+$posts = $sm->get_auto_posting_posts();
+
+if ( is_array( $posts ) ) {
+	$fb = new FB( '268649406514419', '6ca6df4c7e9d909a58d95ce7360adbf3' );
+	
+	$sm_auto_posting_post_ids = array();
+	
+	foreach ( $posts as $p ) {
+		$fb->setAccessToken( $p['access_token'] );
+		
+		// Information:
+		// http://developers.facebook.com/docs/reference/api/page/#posts
+		if ( $fb->api( $p['fb_page_id'] . '/feed', 'POST', array( 'message' => $p['post'], 'link' => $p['link'] ) )
+			$sm_auto_posting_post_ids[] = $p['sm_auto_posting_post_id'];
+	}
+	
+	// Mark as posted
+	$sm->complete_auto_posting_posts( $sm_auto_posting_post_ids );
+}
 ?>
---
+++ includes/classes/admin/social-media.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Handles all the social media
+ *
+ * @package Imagine Retailer
+ * @since 1.0
+ */
+class Social_Media extends Base_Class {
+	/**
+	 * Construct initializes data
+	 */
+	public function __construct() {
+		// Need to load the parent constructor
+		if ( !parent::__construct() )
+			return false;
+	}
+	
+	/**
+	 * Get Auto Posting Posts
+	 *
+	 * @return array
+	 */
+	public function get_auto_posting_posts() {
+		// Get the auto posting posts
+		$posts = $this->db->get_row( "SELECT a.`sm_auto_posting_post_id`, a.`access_token`, a.`post`, a.`link`, b.`fb_page_id` FROM `sm_auto_posting_posts` AS a LEFT JOIN `sm_auto_posting` AS b ON ( a.`website_id` = b.`website_id` ) WHERE a.`status` = 0 AND NOW() > a.`date_posted`", ARRAY_A );
+		
+		// Handle any error
+		if ( $this->db->errno() ) {
+			$this->err( 'Failed to get the posts.', __LINE__, __METHOD__ );
+			return false;
+		}
+		
+		return $posts;
+	}
+	
+	/**
+	 * Get Auto Posting Posts
+	 *
+	 * @param array $sm_auto_posting_post_ids
+	 * @return bool
+	 */
+	public function complete_auto_posting_posts( $sm_auto_posting_post_ids ) {
+		if ( !is_array( $sm_auto_posting_post_ids ) )
+			return false;
+		
+		// Make sure they are all integers
+		foreach ( $sm_auto_posting_post_ids as &$id ) {
+			$id = (int) $id;
+		}
+		
+		$this->db->query( 'UPDATE `sm_auto_posting_posts` SET `status` = 1 WHERE `sm_auto_posting_post_id` IN(' . implode( ',', $sm_auto_posting_post_ids ) . ')' );
+		
+		// Handle any error
+		if ( $this->db->errno() ) {
+			$this->err( 'Failed to complete the auto posting posts.', __LINE__, __METHOD__ );
+			return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Report an error
+	 *
+	 * Make the parent error function a little less complicated
+	 *
+	 * @param string $message the error message
+	 * @param int $line (optional) the line number
+	 * @param string $method (optional) the class method that is being called
+	 */
+	private function err( $message, $line = 0, $method = '' ) {
+		return $this->error( $message, $line, __FILE__, dirname(__FILE__), '', __CLASS__, $method );
+	}
+}
