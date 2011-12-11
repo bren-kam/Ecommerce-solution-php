95e2b3f5c8e6a778b4198acb1e7cfd412e41ae90
Kerry Jones <kerry@studio98.com>
2011-12-11 09:09:43 -0800

    Finished making the website status work

--- admin/ajax/websites/list.php
+++ admin/ajax/websites/list.php
@@ -94,7 +94,7 @@ $aaData = array();
 if ( is_array( $websites ) )
 foreach ( $websites as $web ) {
 	$image = '<img src="/images/icons/companies/' . $web['company_id'] . '.gif" alt="" width="24" height="24" />';
-	
+
 	$title = '<a href="http://' . $web['domain'] . '/" target="_blank"><strong title="' . $web['domain'] . ' - ' . $web['online_specialist'] . '">' . $web['title'] . '</strong></a><br />';
 	$title .= '<span class="web-actions" style="display: block"><a href="/websites/edit/?wid=' . $web['website_id'] . '" title="' . _('Edit') . ' ' . $web['title'] . '">' . _('Edit') . '</a> | ';
 	$title .= '<a href="/websites/control/?wid=' . $web['website_id'] . '" title="' . _('Control') . ' ' . $web['title'] . '" target="_blank">' . _('Control Website') . '</a> | ';
--- includes/classes/account/websites.php
+++ includes/classes/account/websites.php
@@ -22,7 +22,10 @@ class Websites extends Base_Class {
 	 * @return array
 	 */
 	public function get_website( $website_id ) {
-		$website = $this->db->get_row( 'SELECT `website_id`, `os_user_id`, `user_id`, `domain`, `subdomain`, `title`, `theme`, `logo`, `phone`, `pages`, `products`, `product_catalog`, `link_brands`, `blog`, `email_marketing`, `shopping_cart`, `seo`, `room_planner`, `craigslist`, `social_media`, `domain_registration`, `additional_email_addresses`, `ga_profile_id`, `ga_tracking_key`, `wordpress_username`, `wordpress_password`, `mc_list_id`, `type`, `version`, `live`, `date_created`, `date_updated`  FROM `websites` WHERE `website_id` = ' . (int) $website_id, ARRAY_A );
+        // Type Jugglin
+        $website_id = (int) $website_id;
+
+		$website = $this->db->get_row( "SELECT `website_id`, `os_user_id`, `user_id`, `domain`, `subdomain`, `title`, `theme`, `logo`, `phone`, `pages`, `products`, `product_catalog`, `link_brands`, `blog`, `email_marketing`, `shopping_cart`, `seo`, `room_planner`, `craigslist`, `social_media`, `domain_registration`, `additional_email_addresses`, `ga_profile_id`, `ga_tracking_key`, `wordpress_username`, `wordpress_password`, `mc_list_id`, `type`, `version`, `live`, `date_created`, `date_updated`  FROM `websites` WHERE `website_id` = $website_id AND `status` = 1", ARRAY_A );
 	
 		// Handle any error
 		if ( $this->db->errno() ) {
@@ -255,7 +258,7 @@ class Websites extends Base_Class {
 	/**
 	 * List Pages
 	 *
-	 * @param array( $where, $order_by, $limit )
+	 * @param $variables array( $where, $order_by, $limit )
 	 * @return array
 	 */
 	public function list_pages( $variables ) {
@@ -500,6 +503,7 @@ class Websites extends Base_Class {
 	 * @param string $message the error message
 	 * @param int $line (optional) the line number
 	 * @param string $method (optional) the class method that is being called
+     * @return bool
 	 */
 	private function err( $message, $line = 0, $method = '' ) {
 		return $this->error( $message, $line, __FILE__, dirname(__FILE__), '', __CLASS__, $method );
