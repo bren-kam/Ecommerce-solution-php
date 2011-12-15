3cf5fb9e624d51d934075af9354d1be44b1d1f40
Kerry Jones <kerry@studio98.com>
2011-12-14 22:28:47 -0800

    Changed DB to Amazon RDS and updated classes for new website titles

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,12 @@
 <?php
 /** MySQL database name */
-define('DB_NAME', 'imaginer_system');
+define('DB_NAME', 'gsrsystem');
 
 /** MySQL database username */
-define('DB_USER', 'imaginer_admin');
+define('DB_USER', 'gsradmin');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'rbDxn6kkj2e4');
+define('DB_PASSWORD', 'dxX9C87yAy');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'gsr.chdocfam1d6r.us-east-1.rds.amazonaws.com');
