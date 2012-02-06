f4a3b3715d4c587650e6b77a4703d63286817ae9
Kerry Jones <kerry@studio98.com>
2012-02-06 11:10:13 -0800

    Fixed control logins

--- includes/classes/account/users.php
+++ includes/classes/account/users.php
@@ -14,7 +14,10 @@ class Users extends Base_Class {
 	 * @access public
 	 */
 	public $user_id = 0;
-	
+
+	/**
+	 * Construct initializes data
+	 */
 	/**
 	 * Construct initializes data
 	 */
@@ -22,28 +25,28 @@ class Users extends Base_Class {
 		// Need to load the parent constructor
 		if ( !parent::__construct() )
 			return false;
-		
+
 		// Find out if the user has a cookie set, if so, sign him or her in
 		if ( isset( $_COOKIE[SECURE_AUTH_COOKIE] ) ) {
 			$this->encrypted_email = $_COOKIE[SECURE_AUTH_COOKIE];
 		} elseif ( isset( $_COOKIE[AUTH_COOKIE] ) ) {
 			$this->encrypted_email = $_COOKIE[AUTH_COOKIE];
 		}
-		
+
 		if ( !empty( $this->encrypted_email ) ) {
             global $user;
-			
+
             $user = $this->get_user_by_email( security::decrypt( base64_decode( $this->encrypted_email ), security::hash( COOKIE_KEY, 'secure-auth' ) ), security::hash( COOKIE_KEY, 'secure-auth' ) );
 
             // Get website
             $user['websites'] = ar::assign_key( $this->get_websites( $user['user_id'], $user['role'] ), 'website_id' );
 
             // Don't send them into an infinite loop if you can avoid it
-            if ( !is_array( $user['websites'] ) || 0 == count( $user['websites'] ) ) {
+            if ( ( !is_array( $user['websites'] ) || 0 == count( $user['websites'] ) ) && !isset( $_COOKIE['wid'] ) ) {
                 $this->logout();
                 url::redirect('/');
             }
-            
+
             if ( !isset( $_COOKIE['wid'] ) ) {
                 $user['website'] = '';
                 set_cookie( 'wid', $user['website']['website_id'], 172800 ); // 2 days
@@ -68,7 +71,7 @@ class Users extends Base_Class {
             }
 		}
 	}
-	
+
 	/**********************************/
 	/********** USER SECTION **********/
 	/**********************************/
@@ -88,13 +91,13 @@ class Users extends Base_Class {
 	 */
 	public function create( $company_id, $email, $password, $contact_name, $store_name, $role ) {
 		$this->db->insert( 'users', array( 'company_id' => $company_id, 'email' => $email, 'password' => md5( $password ), 'contact_name' => $contact_name, 'store_name' => $store_name, 'role' => $role, 'date_created' => dt::now() ), 'issssis' );
-		
+
 		// Handle any error
 		if ( $this->db->errno() ) {
 			$this->err( 'Failed to create user.', __LINE__, __METHOD__ );
 			return false;
 		}
-		
+
 		return $this->db->insert_id;
 	}
 
@@ -109,15 +112,15 @@ class Users extends Base_Class {
 	 */
 	public function create_authorized_user( $email, $role = 1 ) {
 		global $user;
-		
+
 		$this->db->insert( 'users', array( 'email' => $email, 'company_id' => $user['company_id'], 'role' => $role, 'status' => 1, 'date_created' => ( dt::date('Y-m-d H:i:s') ) ), 'siiis' );
-		
+
 		// Handle any error
 		if ( $this->db->errno() ) {
 			$this->err( 'Failed to create authorized user.', __LINE__, __METHOD__ );
 			return false;
 		}
-		
+
 		return $this->db->insert_id;
 	}
 
@@ -132,25 +135,25 @@ class Users extends Base_Class {
 	 */
 	public function update_information( $user_id, $information ) {
 		global $user;
-		
+
 		if ( isset( $information['password'] ) )
 			$information['password'] = md5( $information['password'] );
-		
+
 		$this->db->update( 'users', $information, array( 'user_id' => $user_id ), str_repeat( 's', count( $information ) ), 'i' );
-		
+
 		// Handle any error
 		if ( $this->db->errno() ) {
 			$this->err( 'Failed to update information for user.', __LINE__, __METHOD__ );
 			return false;
 		}
-		
+
 		// If it was this user, update it
 		if ( $user['user_id'] == $user_id )
 			$user = array_merge( $user, $information );
-		
+
 		return $user_id;
 	}
-	
+
 	/**
 	 * Activates a user
 	 *
@@ -161,13 +164,13 @@ class Users extends Base_Class {
 	 */
 	public function activate( $user_id ) {
 		$this->db->update( 'users', array( 'status' => 1 ), array( 'user_id' => $user_id, 'status' => -1 ), 'i', 'ii' );
-		
+
 		// Handle any error
 		if ( $this->db->errno() ) {
 			$this->err( 'Failed to activate user.', __LINE__, __METHOD__ );
 			return false;
 		}
-		
+
 		return true;
 	}
 
@@ -180,41 +183,41 @@ class Users extends Base_Class {
 	public function login( $email, $password, $remember_me ) {
 		// Prepare the statement
 		$user = $this->db->prepare( 'SELECT `user_id`, `company_id`, `email`, `contact_name`, `store_name`, `products`, `role` FROM `users` WHERE `status` = 1 AND `email` = ? AND `password` = MD5(?)', 'ss', $email, $password )->get_row( '', ARRAY_A );
-		
+
 		// Handle any error
 		if ( $this->db->errno() ) {
 			$this->err( 'Failed to sign in user.', __LINE__, __METHOD__ );
 			return false;
 		}
-		
+
 		// If no user was found, return false
 		if ( !$user )
 			return false;
-		
+
 		$expiration = ( $remember_me ) ? 1209600 : 172800; // Two Weeks : Two Days
 		$auth_cookie = ( security::is_ssl() ) ? AUTH_COOKIE : SECURE_AUTH_COOKIE;
 		set_cookie( $auth_cookie, base64_encode( security::encrypt( $email, security::hash( COOKIE_KEY, 'secure-auth' ) ) ), $expiration );
-		
+
 		// Record the login
 		$this->record_login( $user['user_id'] );
-		
+
 		return $user;
 	}
-	
+
 	/**
 	 * Logs out
 	 */
 	public function logout() {
 		global $mc;
-		
+
 		// Removing both of these cookies will destroy everything
 		remove_cookie( AUTH_COOKIE );
 		remove_cookie( SECURE_AUTH_COOKIE );
 		remove_cookie( 'action' );
-		
+
 		$mc->delete( $this->encrypted_email );
 	}
-	
+
 	/**
 	 * Get websites
 	 *
@@ -229,7 +232,7 @@ class Users extends Base_Class {
 		if ( $role > 1 ) {
 			// @Fix should `phone` and `logo` be removed and put in the websites:get_website function (meaning theme/website/top needs to change)
 			$websites = $this->db->get_results( "SELECT `website_id`, `os_user_id`, IF( '' = `subdomain`, `domain`, CONCAT( `subdomain`, '.', `domain` ) ) AS domain, `phone`, `logo`, `title`, `pages`, `products`, `product_catalog`, `link_brands`, `blog`, `email_marketing`, `shopping_cart`, `seo`, `room_planner`, `craigslist`, `social_media`, `wordpress_username`, `wordpress_password`, `mc_list_id`,  `ga_profile_id`, `mc_list_id`, `live`, `type` FROM `websites` WHERE `user_id` = $user_id AND `status` = 1", ARRAY_A );
-			
+
 			// Handle any error
 			if ( $this->db->errno() ) {
 				$this->err( 'Failed to predetermine website.', __LINE__, __METHOD__ );
@@ -238,17 +241,17 @@ class Users extends Base_Class {
 		} else {
 			// @Fix -- look off `products` or `product_catalog`
 			$websites = $this->db->get_results( "SELECT a.`website_id`, a.`os_user_id`, a.`domain`, a.`subdomain`, a.`title`, a.`product_catalog`, a.`link_brands`, a.`seo`, a.`room_planner`, a.`craigslist`, a.`social_media`, a.`wordpress_username`, a.`wordpress_password`, a.`mc_list_id`, a.`live`, a.`type`, a.`pages`, ( b.`products` * a.`products` * a.`product_catalog` ) AS products, a.`ga_profile_id`, b.`blog`, b.`email_marketing`, b.`shopping_cart` FROM `websites` AS a LEFT JOIN `auth_user_websites` AS b ON ( a.`website_id` = b.`website_id` ) WHERE b.`user_id` = $user_id AND `status` = 1", ARRAY_A );
-			
+
 			// Handle any error
 			if ( $this->db->errno() ) {
 				$this->err( 'Failed to predetermine authorized user website.', __LINE__, __METHOD__ );
 				return false;
 			}
 		}
-		
+
 		return $websites;
 	}
-	
+
 	/**
 	 * Record Login Date/Time
 	 *
@@ -258,16 +261,16 @@ class Users extends Base_Class {
 	private function record_login( $user_id ) {
 		// Set the last login date to now
 		$this->db->update( 'users', array( 'last_login' => dt::date('Y-m-d H:i:s') ), array( 'user_id' => $user_id ), 's', 'i' );
-	
+
 		// Handle any error
 		if ( $this->db->errno() ) {
 			$this->err( 'Failed to record login.', __LINE__, __METHOD__ );
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Forgot Your Password
 	 *
@@ -277,32 +280,32 @@ class Users extends Base_Class {
 	public function forgot_password( $email ) {
 		// Get the user
 		$user = $this->db->prepare( "SELECT `user_id`, `account_type_id`, CONCAT( `first_name`, ' ', `last_name` ) AS name, `status` FROM `users` WHERE `email` = ?", 's', $email )->get_row( '', ARRAY_A );
-		
+
 		// Handle any error
 		if ( $this->db->errno() ) {
 			$this->err( 'Failed to get user data.', __LINE__, __METHOD__ );
 			return false;
 		}
-		
+
 		if ( $user ) {
 			$e = new Emails();
-			
+
 			if ( -1 == $user['status'] ) {
 				// This means their account was never activated, so send the same email
 				$e->send_confirmation( $user['user_id'], $email );
-				
+
 				return 1;
 			} else {
 				// This means it is a legitimate forgot password request
 				$e->reset_password( $user['user_id'], $user['name'], $email );
-				
+
 				return 2;
 			}
-		} else { 
+		} else {
 			return 0;
 		}
 	}
-	
+
 	/**
 	 * Gets a user by their id
 	 *
@@ -313,9 +316,9 @@ class Users extends Base_Class {
 	 */
 	public function get_user( $user_id ) {
 		global $mc;
-		
+
 		$user = $mc->get( 'get_user > ' . $user_id );
-		
+
 		if ( empty( $user ) ) {
 			// Prepare the statement
 			$user = $this->db->prepare( 'SELECT `user_id`, `company_id`, `email`, `contact_name`, `store_name`, `products`, `role` FROM `users` WHERE `user_id` = ? AND `status` = 1', 'i', $user_id )->get_row( '', ARRAY_A );
@@ -325,10 +328,10 @@ class Users extends Base_Class {
 				$this->err( 'Failed to get user.', __LINE__, __METHOD__ );
 				return false;
 			}
-			
+
 			$mc->add( 'get_user > ' . $user_id, $user, 7200 );
 		}
-		
+
 		return $user;
 	}
 
@@ -344,19 +347,19 @@ class Users extends Base_Class {
 	public function get_user_by_email( $email, $assign_user_id = true ) {
 		// Prepare the statement
 		$user = $this->db->prepare( 'SELECT `user_id`, `company_id`, `email`, `contact_name`, `store_name`, `products`, `role` FROM `users` WHERE `status` = 1 AND `email` = ?', 's', $email )->get_row( '', ARRAY_A );
-		
+
 		// Handle any error
 		if ( $this->db->errno() ) {
 			$this->err( 'Failed to get user by email.', __LINE__, __METHOD__ );
 			return false;
 		}
-		
+
 		if ( $assign_user_id )
 			$this->user_id = (int) $user['user_id'];
-		
+
 		return $user;
 	}
-	
+
 	/**
 	 * Report an error
 	 *
