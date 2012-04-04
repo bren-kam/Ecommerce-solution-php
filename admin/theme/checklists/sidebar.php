d4bc4b08b8401638e7edc1653faa67423f324be7
Kerry Jones <kerry@studio98.com>
2012-04-04 13:50:38 -0700

    Added the ability to add checklists

--- admin/theme/checklists/sidebar.php
+++ admin/theme/checklists/sidebar.php
@@ -1,8 +1,9 @@
 <div id="sidebar">
 	<h2><?php echo _('Actions'); ?></h2>
 	<a href="/checklists/" title="<?php echo _('Checklists'); ?>" class="top"><?php echo _('Checklists'); ?></a>
-		<a href="/checklists/" title="<?php echo _('In Progress'); ?>" class="sub in-progress first"><?php echo _('In Progress'); ?></a>
-		<a href="/checklists/completed/" title="<?php echo _('Completed'); ?>" class="sub completed last"><?php echo _('Completed'); ?></a>
+		<a href="/checklists/" title="<?php echo _('In Progress'); ?>" class="sub first"><?php echo _('In Progress'); ?></a>
+		<a href="/checklists/completed/" title="<?php echo _('Completed'); ?>" class="sub"><?php echo _('Completed'); ?></a>
+		<a href="/checklists/add-checklist/" title="<?php echo _('Add Checklist'); ?>" class="sub last"><?php echo _('Add Checklist'); ?></a>
     <?php
     global $user;
     if ( $user['role'] >= 8 ) {
