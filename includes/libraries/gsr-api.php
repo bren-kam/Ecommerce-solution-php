cb7a731380782c33e93a80c0e2d8a34025eb20d0
Kerry Jones <kerry@studio98.com>
2012-04-24 10:42:29 -0700

    Tweaks to API to make it create unique users

--- includes/libraries/gsr-api.php
+++ includes/libraries/gsr-api.php
@@ -7,21 +7,26 @@
  * This handles all API Calls
  * @version 1.0.0
  */
-class GSRAPI {
+class GSR_API {
 	/**
 	 * Constant paths to include files
 	 */
-	const PATH_S98_LIB = '/path/to/s98lib/init.php';
+	const PATH_S98LIB = '/path/to/s98lib/init.php';
 	const URL_API = 'https://www.imagineretailer.com/api/';
 	const DEBUG = false;
 	
 	/**
 	 * A few variables that will determine the basic status
 	 */
-	public $message = NULL;
-	public $success = false;
-	public $response = NULL;
-	
+    private $api_key;
+    private $message = NULL;
+    private $success = FALSE;
+	private $raw_request = NULL;
+	private $request = NULL;
+	private $raw_response = NULL;
+	private $response = NULL;
+    private $error = NULL;
+    
 	/**
 	 * Construct class will initiate and run everything
 	 *
@@ -29,17 +34,17 @@ class GSRAPI {
 	 */
 	public function __construct( $api_key ) {
 		// Do we need to debug
-		if( self::DEBUG )
+		if ( self::DEBUG )
 			error_reporting( E_ALL );
 		
 		// Include S98 Framework
-		require_once( self::PATH_S98_LIB );
+		require self::PATH_S98LIB;
 		
 		$this->api_key = $api_key;
 	}
 	
 	/*************************/
-	/* Start: IR API Methods */
+	/* Start: GSR API Methods */
 	/*************************/	
 	
 	/**
@@ -54,7 +59,7 @@ class GSRAPI {
 	 */
 	public function add_order_item( $order_id, $item, $quantity, $amount, $monthly ) {
 		// Execute the command
-		$this->execute( 'add_order_item', compact( 'order_id', 'item', 'quantity', 'amount', 'monthly' ) );
+		$this->_execute( 'add_order_item', compact( 'order_id', 'item', 'quantity', 'amount', 'monthly' ) );
 		
 		// Return the user id successful
 		return ( $this->success ) ? true : false;
@@ -70,7 +75,7 @@ class GSRAPI {
 	 */
 	public function create_order( $user_id, $setup, $monthly ) {
 		// Execute the command
-		$this->execute( 'create_order', compact( 'user_id', 'setup', 'monthly' ) );
+		$this->_execute( 'create_order', compact( 'user_id', 'setup', 'monthly' ) );
 		
 		// Return the user id successful
 		return ( $this->success ) ? $this->response->order_id : false;
@@ -95,7 +100,7 @@ class GSRAPI {
 	 */
 	public function create_user( $email, $password, $contact_name, $store_name, $work_phone, $cell_phone, $billing_first_name, $billing_last_name, $billing_address1, $billing_city, $billing_state, $billing_zip ) {
 		// Execute the command
-		$this->execute( 'create_user', compact( 'email', 'password', 'contact_name', 'store_name', 'work_phone', 'cell_phone', 'billing_first_name', 'billing_last_name', 'billing_address1', 'billing_city', 'billing_state', 'billing_zip' ) );
+		$this->_execute( 'create_user', compact( 'email', 'password', 'contact_name', 'store_name', 'work_phone', 'cell_phone', 'billing_first_name', 'billing_last_name', 'billing_address1', 'billing_city', 'billing_state', 'billing_zip' ) );
 		
 		// Return the user id successful
 		return ( $this->success ) ? $this->response->user_id : false;
@@ -130,7 +135,7 @@ class GSRAPI {
 			$additional_email_addresses = 0;
 		
 		// Execute the command
-		$this->execute( 'create_website', compact( 'user_id', 'domain', 'title', 'plan_name', 'plan_description', 'type', 'pages', 'product_catalog', 'blog', 'email_marketing', 'shopping_cart', 'seo', 'room_planner', 'craigslist', 'social_media', 'domain_registration', 'additional_email_addresses', 'products' ) );
+		$this->_execute( 'create_website', compact( 'user_id', 'domain', 'title', 'plan_name', 'plan_description', 'type', 'pages', 'product_catalog', 'blog', 'email_marketing', 'shopping_cart', 'seo', 'room_planner', 'craigslist', 'social_media', 'domain_registration', 'additional_email_addresses', 'products' ) );
 		
 		// Return the user id successful
 		return ( $this->success ) ? $this->response->website_id : false;
@@ -145,7 +150,7 @@ class GSRAPI {
 	 */
 	public function update_social_media( $website_id, $social_media_add_ons ) {
 		// Execute the command
-		$this->execute( 'update_social_media', compact( 'website_id', 'social_media_add_ons' ) );
+		$this->_execute( 'update_social_media', compact( 'website_id', 'social_media_add_ons' ) );
 		
 		// Return the user id successful
 		return ( $this->success ) ? true : false;
@@ -171,7 +176,7 @@ class GSRAPI {
 	 */
 	public function update_user( $email, $password, $contact_name, $store_name, $work_phone, $cell_phone, $billing_first_name, $billing_last_name, $billing_address1, $billing_city, $billing_state, $billing_zip, $user_id ) {
 		// Execute the command
-		$this->execute( 'update_user', compact( 'email', 'password', 'contact_name', 'store_name', 'work_phone', 'cell_phone', 'billing_first_name', 'billing_last_name', 'billing_address1', 'billing_city', 'billing_state', 'billing_zip', 'user_id' ) );
+		$this->_execute( 'update_user', compact( 'email', 'password', 'contact_name', 'store_name', 'work_phone', 'cell_phone', 'billing_first_name', 'billing_last_name', 'billing_address1', 'billing_city', 'billing_state', 'billing_zip', 'user_id' ) );
 		
 		// Return the user id successful
 		return ( $this->success ) ? true : false;
@@ -188,16 +193,79 @@ class GSRAPI {
 	 */
 	public function set_arb_subscription( $arb_subscription_id, $website_id ) {
 		// Execute the command
-		$this->execute( 'set_arb_subscription', compact( 'arb_subscription_id', 'website_id' ) );
+		$this->_execute( 'set_arb_subscription', compact( 'arb_subscription_id', 'website_id' ) );
 		
 		// Return the user id successful
 		return ( $this->success ) ? true : false;
 	}
 	
 	/***********************/
-	/* END: IR API Methods */
+	/* END: GSR API Methods */
 	/***********************/
-	
+
+    /**
+     * Get private message variable
+     *
+     * @return string
+     */
+    public function message() {
+        return $this->message;
+    }
+
+    /**
+     * Get private success variable
+     *
+     * @return string
+     */
+    public function success() {
+        return $this->success;
+    }
+
+    /**
+     * Get private raw_request variable
+     *
+     * @return string
+     */
+    public function raw_request() {
+        return $this->raw_request();
+    }
+
+    /**
+     * Get private request variable
+     *
+     * @return array Object
+     */
+    public function request() {
+        return $this->request;
+    }
+
+    /**
+     * Get private raw_response variable
+     *
+     * @return string
+     */
+    public function raw_response() {
+        return $this->raw_response;
+    }
+
+    /**
+     * Get private response variable
+     *
+     * @return stdClass Object
+     */
+    public function response() {
+        return $this->response;
+    }
+
+    /**
+     * Get private error variable
+     *
+     * @return string
+     */
+    public function error() {
+        return $this->error;
+    }
+
 	/**
 	 * This sends sends the actual call to the API Server and parses the response
 	 *
@@ -205,14 +273,16 @@ class GSRAPI {
 	 *
 	 * @param string $method The method being called
 	 * @param array $params an array of the parameters to be sent
+     * @return string
 	 */
-	private function execute( $method, $params ) {
+	private function _execute( $method, $params ) {
 		if( empty( $this->api_key ) ) {
 			$this->error = 'Cannot send request without an API Key.';
 			$this->success = false;
 		}
-		
-		$post_vars = http_build_query( array_merge( array( 'auth_key' => $this->api_key, 'method' => $method ), $params ) );
+
+        $this->request = array_merge( array( 'auth_key' => $this->api_key, 'method' => $method ), $params );
+        $this->raw_request = http_build_query( $this->request );
 		
 		$ch = curl_init();
 		curl_setopt( $ch, CURLOPT_URL, self::URL_API );
@@ -220,16 +290,32 @@ class GSRAPI {
 		curl_setopt( $ch, CURLOPT_TIMEOUT, 20 );
 		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, $post_vars );
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, $this->raw_request );
 		curl_setopt( $ch, CURLOPT_POST, 1 );
-		$this->response = json_decode( curl_exec( $ch ) );
+
+        // Perform the request and get the response
+        $this->raw_response = curl_exec( $ch );
+
+		$this->response = json_decode( $this->raw_response );
+
 		curl_close($ch);
-		
-		if( $this->response->success )
+		if( $this->response->success ) {
 			$this->success = true;
-		
+        } else {
+            $this->error = $this->response->message;
+        }
+
 		$this->message = $this->response->message;
-		
+
+        // If we're debugging lets give as much info as possible
+        if( self::DEBUG ) {
+            echo "<h1>URL</h1>\n<p>", self::URL_API, "</p>\n<hr />\n<br /><br />\n";
+            echo "<h1>Raw Request</h1>\n<pre>", $this->raw_request, "</pre>\n<hr />\n<br /><br />\n";
+            echo "<h1>Request</h1>\n\n<pre>", var_export( $this->request, true ), "</pre>\n<hr />\n<br /><br />\n";
+            echo "<h1>Raw Response</h1>\n<pre>", $this->raw_response, "</pre>\n<hr />\n<br /><br />\n";
+            echo "<h1>Response</h1>\n<pre>", var_export( $this->response, true ), "</pre>\n<hr />\n<br /><br />\n";
+        }
+
 		return $this->response;
 	}
 }
