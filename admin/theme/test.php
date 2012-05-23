c62f48a950cfadc177fb1ea1cab9ccb166d7f0be
Kerry Jones <kerry@studio98.com>
2012-05-23 09:07:49 -0700

    Tweak to test.php

--- admin/theme/test.php
+++ admin/theme/test.php
@@ -2,7 +2,7 @@
 
 $b = new Base_Class();
 
-$website_images = $b->db->get_col( "SELECT DISTINCT `image_url` FROM `website_categories` WHERE `image_url` NOT LIKE '%/small/%'" );
+$website_images = $b->db->get_col( "SELECT DISTINCT `image_url` FROM `website_categories` WHERE `status` <> 0 AND `image_url` NOT LIKE '%/small/%'" );
 
 function _small_image( $image ) {
 	return preg_replace( '/(.+\/products\/[0-9]+\/)(?:small\/)?([a-zA-Z0-9-.]+)/', "$1small/$2", $image );
@@ -12,8 +12,10 @@ foreach ( $website_images as $wi ) {
 	echo $wi;
 	$new_image = _small_image( $wi );
 	
-	if ( !getimagesize( $new_image ) )
+	if ( !getimagesize( $new_image ) ) {
+        $b->db->update( 'website_categories', array( 'status' => 0 ), array( 'image_url' => $wi ), 'i', 's' );
 		continue;
+    }
 	
 	$b->db->update( 'website_categories', array( 'image_url' => $new_image ), array( 'image_url' => $wi ), 's', 's' );
 }
