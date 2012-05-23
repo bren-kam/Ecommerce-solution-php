fb5e82e589073824e3037a6639c0109a13ef60d2
Kerry Jones <kerry@studio98.com>
2012-05-23 00:02:26 -0700

    Updated tickets to sort priority properly

--- admin/theme/test.php
+++ admin/theme/test.php
@@ -1,39 +1,19 @@
 <?php
 
 $b = new Base_Class();
-library('rb');
-R::setup('mysql:host=199.204.138.78;dbname=imaginer_system','imaginer_admin','rbDxn6kkj2e4');
-R::debug(true); //easy way
-R::freeze( true );
-exit;
-$normal_reads = new timer();
 
-for ( $i = 0; $i < 5000; $i++ ) {
-    $b->db->get_results( "SELECT `product_id`, `title`, `slug`, `brand_id`, `industry_id` FROM `products` WHERE `publish_visibility` <> 'deleted' LIMIT 10000", ARRAY_A );
+$website_images = $b->db->get_col( "SELECT DISTINCT `image_url` FROM `website_categories` WHERE `image_url` NOT LIKE '%/small/%'" );
+
+function _small_image( $image ) {
+	return preg_replace( '/(.+\/products\/[0-9]+\/)(?:small\/)?([a-zA-Z0-9-.]+)/', "$1small/$2", $image );
 }
 
-echo 'Normal Reads: ' . $normal_reads->stop() . "\n<br />";
-
-$normal_writes = new timer();
-
-for ( $i = 0; $i < 5000; $i++ ) {
-    $b->db->insert( 'test', array( 'name' => 'Random Name', 'message' => 'Hello there! How are you? Im glad I could finally meet you!', 'date_created' => '2012-05-13 10:08:00' ), 'sss' );
+foreach ( $website_images as $wi ) {
+	echo $wi;
+	$new_image = _small_image( $wi );
+	
+	if ( !getimagesize( $new_image ) )
+		continue;
+	
+	$b->db->update( 'website_categories', array( 'image_url' => $new_image ), array( 'image_url' => $wi ), 's', 's' );
 }
-
-echo 'Normal Writes: ' . $normal_writes->stop() . "\n<br />";
-
-$rb_reads = new timer();
-
-for( $i = 0; $i < 5000; $i++ ) {
-    R::getAll( "SELECT `product_id`, `title`, `slug`, `brand_id`, `industry_id` FROM `products` WHERE `publish_visibility` <> 'deleted' LIMIT 10000" );
-}
-
-echo 'RedBean Reads: ' . $rb_reads->stop() . "\n<br />";
-
-$rb_writes = new timer();
-
-for ( $i = 0; $i < 5000; $i++ ) {
-    R::exec( "INSERT INTO `test` ( `name`, `message`, date_created` ) VALUES ( :name, :message, :date_created)", array(  ':name' => 'Random Name', ':message' => 'Hello there! How are you? Im glad I could finally meet you!', ':date_created' => '2012-05-13 10:08:00' ) );
-}
-
-echo 'RedBean Writes: ' . $rb_writes->stop() . "\n<br />";
