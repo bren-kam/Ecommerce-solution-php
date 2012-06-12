1546e187ccb0e218a80972d33d4fce50176c5a4f
Kerry Jones <kerry@studio98.com>
2012-06-11 22:10:03 -0700

    Fixed a type and made the links for sidebar image have to be full links including http

--- account/ajax/website/sidebar/update-extra.php
+++ account/ajax/website/sidebar/update-extra.php
@@ -11,7 +11,17 @@ $ajax->ok( $user, _('You must be signed in to update this field.') );
 // Instantiate class
 $wa = new Website_Attachments();
 
+// Empty it the link they didn't enter anything
+if ( 'Enter Link...' == $_POST['extra'] )
+    $_POST['extra'] = '';
+
 $meta = ( isset( $_POST['meta'] ) ) ? $_POST['meta'] : '';
+
+// Do validation
+$v = new Validator();
+$v->add_validation( 'extra', 'URL' );
+
+$ajax->ok( empty( $errs ) && stristr( $_POST['extra'], 'http' ), _('Please make sure you enter in a valid link') );
 
 // Make sure it updated successfully
 $ajax->ok( $wa->update_extra( $_POST['hWebsiteAttachmentID'], $_POST['extra'], $meta ), _('An error occurred while trying to update your field. Please refresh the page and try again.') );
