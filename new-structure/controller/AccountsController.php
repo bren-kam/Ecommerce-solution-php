2e942938ff80c7a4b704c8fee343941e3b4d71c1
PedroSena <sena.pedro@gmail.com>
2012-07-05 17:28:26 -0300

    Suggestion for structure

---
+++ new-structure/controller/AccountsController.php
@@ -0,0 +1,14 @@
+<?php
+class AccountsController extends BaseController {
+
+
+    protected function new_account() {
+        /*
+         * Here I'll parse the HTTP, create a Account model and call some method on it, probably save
+         * I'll check for exceptions(not if($account->save())) and display a proper message in case of errors
+        */
+    }
+
+}
+
+
---
+++ new-structure/lib/VeryUsefulClass.php
@@ -0,0 +1,6 @@
+<?php
+
+//This is a useful class that will be used on many Modules
+class VeryUsefulClass {
+
+}
