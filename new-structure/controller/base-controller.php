eed909fa636d910a0cab9a38f182ef344013af08
PedroSena <sena.pedro@gmail.com>
2012-07-06 15:43:04 -0300

    Fixed mispelled var

--- new-structure/controller/base-controller.php
+++ new-structure/controller/base-controller.php
@@ -1,25 +1,25 @@
 <?php
 abstract class BaseController {
 
-    private $avaialble_actions;
+    private $available_actions;
 
     public function __construct() {
-        $this->avaialble_actions = array();
+        $this->available_actions = array();
         $reflection_class = new ReflectionClass( get_class( $this ) );
         foreach ( $reflection_class->getMethods() as $method ) { //All methods from any Controller inheriting from BaseController
             $method_name = $method->getName();
             $nonce = nonce::create( $method_name );
-            $this->avaialble_actions[$nonce] = $method_name;
+            $this->available_actions[$nonce] = $method_name;
         }
     }
 
     //Cant be changed, every Controller will have a method for each possible action
     public final function run() {
-        if ( sizeof( $this->avaialble_actions ) < 2 ) {//2 because this method will count
+        if ( sizeof( $this->available_actions ) < 2 ) {//2 because this method will count
             throw new ControllerException( "No actions registered for controller " . get_class( $this ) );
         }
         $actionName = $_REQUEST['_nonce'];
-        $method_name = $this->avaialble_actions[$actionName];
+        $method_name = $this->available_actions[$actionName];
         if ( NULL == $method_name ) {
             throw new ControllerException( "There is no such action" );
         }
