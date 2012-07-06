463ade70f838c23eb30bede60d00961f8540851e
PedroSena <sena.pedro@gmail.com>
2012-07-06 17:00:21 -0300

    changed var names to match standard

--- new-structure/controller/base-controller.php
+++ new-structure/controller/base-controller.php
@@ -8,18 +8,18 @@ abstract class BaseController {
      * Contain the available methods for subclass
      * @var array
      */
-    private $availableActions;
+    private $available_actions;
 
     /**
      * Setup standard reflection
      */
     public function __construct() {
-        $this->availableActions = array();
+        $this->available_actions = array();
         $reflectionClass = new ReflectionClass( get_class( $this ) );
         foreach ( $reflectionClass->getMethods() as $method ) { //All methods from any Controller inheriting from BaseController
             $methodName = $method->getName();
             $nonce = nonce::create( $methodName );
-            $this->availableActions[$nonce] = $methodName;
+            $this->available_actions[$nonce] = $methodName;
         }
     }
 
@@ -30,11 +30,11 @@ abstract class BaseController {
      */
     public final function run() {
         // 2 because this method will count
-        if ( sizeof( $this->availableActions ) < 2 )
+        if ( sizeof( $this->available_actions ) < 2 )
             throw new ControllerException( "No actions registered for controller " . get_class( $this ) );
 
         $actionName = $_REQUEST['_nonce'];
-        $methodName = $this->availableActions[$actionName];
+        $methodName = $this->available_actions[$actionName];
 
         if ( is_null( $methodName ) )
             throw new ControllerException( "There is no such action" );
