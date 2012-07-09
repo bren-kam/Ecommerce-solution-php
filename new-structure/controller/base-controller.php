8460a04b51775d2f0c79f32cf2b84cf3879fe242
Kerry Jones <kerry@studio98.com>
2012-07-09 12:25:59 -0700

    Moved get_template_response to base_controller

--- new-structure/controller/base-controller.php
+++ new-structure/controller/base-controller.php
@@ -4,23 +4,32 @@
  * The base class for all other controllers
  */
 abstract class BaseController {
-
     /**
      * Contain the available methods for subclass
      * @var array
      */
-    private $available_actions;
+    private $_available_actions;
+
+    /**
+     * Define the base for the views
+     * @param string
+     */
+    private $_view_base;
 
     /**
      * Setup standard reflection
+     *
+     * @param string $view_base [optional]
      */
-    public function __construct() {
-        $this->available_actions = array();
+    public function __construct( $view_base = '') {
+        $this->_view_base = $view_base;
+
+        $this->_available_actions = array();
         $reflectionClass = new ReflectionClass( get_class( $this ) );
         foreach ( $reflectionClass->getMethods() as $method ) { //All methods from any Controller inheriting from BaseController
             $methodName = $method->getName();
             $nonce = nonce::create( $methodName );
-            $this->available_actions[$nonce] = $methodName;
+            $this->_available_actions[$nonce] = $methodName;
         }
     }
 
@@ -31,11 +40,11 @@ abstract class BaseController {
      */
     public final function run() {
         // 2 because this method will count
-        if ( sizeof( $this->available_actions ) < 2 )
+        if ( sizeof( $this->_available_actions ) < 2 )
             throw new ControllerException( "No actions registered for controller " . get_class( $this ) );
 
         $actionName = $_REQUEST['_nonce'];
-        $methodName = $this->available_actions[$actionName];
+        $methodName = $this->_available_actions[$actionName];
 
         if ( is_null( $methodName ) )
             throw new ControllerException( "There is no such action" );
@@ -45,6 +54,16 @@ abstract class BaseController {
          */
         $response = $this->$methodName();
         $response->send_response();
+    }
+    
+    /**
+     * Return a new template response with the right path
+     *
+     * @param string $file
+     * @return TemplateResponse
+     */
+    protected function get_template_response( $file ) {
+        return new TemplateResponse( $this->_view_base . $file );
     }
 }
 
