76e30c02093cf7d5124a5f5a4d47818ff5c40a05
Kerry Jones <kerry@studio98.com>
2012-07-12 00:07:57 -0700

    Changed about us

--- account/theme/social-media/facebook/about-us.php
+++ account/theme/social-media/facebook/about-us.php
@@ -59,49 +59,43 @@ get_header();
 	<?php get_sidebar( 'social-media/' ); ?>
 	<div id="subcontent">
 		<?php if ( !isset( $about_us['fb_page_id'] ) || 0 == $about_us['fb_page_id'] ) { ?>
-		<h2 class="title"><?php echo _('Step 1: Go to the About Us application.'); ?></h2>
+		    <h2 class="title"><?php echo _('Step 1: Go to the About Us application.'); ?></h2>
 			<p><?php echo _('Go to the'); ?> <a href="http://apps.facebook.com/op-about-us/" title="<?php echo _('Online Platform - About Us'); ?>" target="_blank"><?php echo _('About Us'); ?></a> <?php echo _('application page'); ?>.</p>
 			<br /><br />
-			
-			<h2 class="title"><?php echo _('Step 2: Install on your Fan Page'); ?></h2>
-			<p><?php echo _('Click'); ?> <strong><?php echo _('Add to my Page (bottom left of your page).'); ?></strong></p>
-			<p><strong><?php echo _('NOTE'); ?>:</strong> <?php echo _("If you do not see this link, it means you either don't have administrative access to any fan pages, or you already have this application installed. (If it is already installed, please ahead skip to Step 4.)"); ?></p>
+
+            <h2 class="title"><?php echo _('Step 2: Install The App'); ?></h2>
+            <p><?php echo _('Click'); ?> <strong><?php echo _('Install This App.'); ?></strong> <?php echo _('on the page shown below:'); ?></p>
 			<br />
-			<p><img src="http://account.imagineretailer.com/images/social-media/facebook/about-us/step2.jpg" class="image-border" width="884" height="521" alt="<?php echo _('Step 2'); ?>" /></p>
-			<br /><br />
-			
-			<h2 class="title"><?php echo _('Step 3: Click on the Add to Page Button.'); ?></h2>
-			<p><?php echo _('Choose the Facebook Fan Page you want to add your app to by clicking on the'); ?> <strong><?php echo _('Add to Page'); ?></strong> <?php echo _('button to the right of the Fan Page name.'); ?></p>
-			<br />
-			<p><img src="http://account.imagineretailer.com/images/social-media/facebook/about-us/step3.jpg" class="image-border" width="883" height="240" alt="<?php echo _('Step 3'); ?>" /></p>
-			<br /><br />
-			
-			<h2 class="title"><?php echo _('Step 4: Click on the App.'); ?></h2>
-			<p><?php echo _('Go to your Fan Page and click on the App you are installing from the list on the left.'); ?></p>
-			<br />
-			<p><img src="http://account.imagineretailer.com/images/social-media/facebook/about-us/step4.jpg" class="image-border" width="750" height="594" alt="<?php echo _('Step 4 - 1'); ?>" /></p>
-			<br />
-			<p><?php echo _('Click on'); ?> <strong><?php echo _('Update Settings'); ?></strong> <?php echo _('right under the app name. Note: This is only visible to you because you are the admin for this page.'); ?></p>
-			<br />
-			<p><img src="http://account.imagineretailer.com/images/social-media/facebook/about-us/step4-1.jpg" class="image-border" width="673" height="187" alt="<?php echo _('Step 4 - 2'); ?>" /></p>
-			<br /><br />
-			
-			<h2 class="title"><?php echo _('Step 5: Connect the application with your dashboard account'); ?></h2>
-			<p><?php echo _('Copy the connection key listed below and paste into the Facebook app.'); ?></p>
-			<p><?php echo _('Facebook Connection Key'); ?>: <?php echo $about_us['key']; ?></p>
-			<p><strong><?php echo _('NOTE'); ?></strong>: <?php echo _('You may see a request for permissions. If this is the case, you first need to Allow Permissions to the application before you will be able to move on.'); ?></p>
-			<br />
-			<p><img src="http://account.imagineretailer.com/images/social-media/facebook/about-us/step5.jpg" class="image-border" width="750" height="203" alt="<?php echo _('Step 5'); ?>" /></p>
-			<br />
-			<p><?php echo _('When you click Connect, you will see'); ?> <span class="error"><?php echo _('(Not Connected)'); ?></span> <?php echo _('in red change to'); ?> <span class="success"><?php echo _('(Connected)'); ?></span> <?php echo _('in green.'); ?></p>
-			<br /><br />
-			
-			<h2 class="title"><?php echo _('Step 6: Final App Activation.'); ?></h2>
-			<p><?php echo _('Click the activate link to complete the installation process. You will then be able to control all the content for the app from this dashboard.'); ?></p>
-			<p><a href="/social-media/facebook/about-us/" title="<?php echo _('Activate'); ?>"><?php echo _('Activate'); ?></a></p>
-			<br />
-			<p><img src="http://account.imagineretailer.com/images/social-media/facebook/about-us/step6.jpg" class="image-border" width="489" height="187" alt="<?php echo _('Step 6'); ?>" /></p>
-			<br /><br />
+            <p><a href="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Capture16.jpg"><img alt="" class="aligncenter size-full wp-image-2475" height="720" src="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Capture16.jpg" title="Capture" width="740" /></a></p>
+            <br /><br />
+
+            <h2 class="title"><?php echo _('Step 3: Choose Your Page'); ?></h2>
+            <p><?php echo _('(Note - You must first be an admin of the page to install the App)'); ?></p>
+            <br />
+            <p><a href="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Capture17.jpg"><img alt="" class="aligncenter size-full wp-image-2476" height="176" src="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Capture17.jpg" title="Capture" width="796" /></a></p>
+            <br /><br />
+
+            <h2 class="title"><?php echo _('Step 4: Click Add Online Platform - About Us'); ?></h2>
+            <br />
+            <p><a href="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Untitled12.jpg"><img alt="" class="aligncenter size-full wp-image-2484" height="277" src="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Untitled12.jpg" title="Untitled" width="787" /></a></p>
+            <br /><br />
+
+            <h2 class="title"><?php echo _('Step 5: Click on the Fan Offer App'); ?></h2>
+            <p><?php echo _("Click on the Fan Offer App on Your Facebook Page</strong> Scroll down below the banner, and you&#39;ll see your apps (you may need to click on the arrow on the right-hand side to find the app you're looking for) and click on the About Us"); ?></p>
+            <br />
+            <p><a href="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Untitled9.jpg"><img alt="" class="aligncenter size-full wp-image-2477" height="337" src="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Untitled9.jpg" title="Untitled" width="517" /></a></p>
+            <br /><br />
+
+            <h2 class="title"><?php echo _('Step 6: Click on the Update Settings'); ?></h2>
+            <br />
+            <p><a href="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Capture4.jpg"><img alt="" class="aligncenter size-full wp-image-2456" height="25" src="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Capture4.jpg" title="Capture" width="172" /></a></p>
+            <br /><br />
+
+            <h2 class="title"><?php echo _('Step 7: Copy and Paste'); ?></h2>
+            <p><?php echo _('Copy and paste the connection code below into the Facebook Connection Key box shown below (when done it will say Connected)'); ?></p>
+            <br />
+            <p><a href="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Untitled2.jpg"><img alt="" class="aligncenter size-full wp-image-2458" height="409" src="http://admin.imagineretailer.com/help/wp-content/uploads/2012/06/Untitled2.jpg" title="Untitled" width="787" /></a></p>
+            <br /><br />
 		<?php } else { ?>
 			<p align="right"><a href="http://www.facebook.com/pages/ABC-Company/<?php echo $about_us['fb_page_id']; ?>?sk=app_233746136649331" title="<?php echo _('View Facebook Page'); ?>" target="_blank"><?php echo _('View Facebook Page'); ?></a></p>
 			<?php if ( $success ) { ?>
