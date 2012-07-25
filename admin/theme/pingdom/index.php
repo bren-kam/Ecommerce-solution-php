1258f56c46d3bc486b94b40b89df3c2873151c6a
Kerry Jones <kerry@studio98.com>
2012-07-25 14:59:33 -0700

    Last change to pingdom status check

--- admin/theme/pingdom/index.php
+++ admin/theme/pingdom/index.php
@@ -4,7 +4,7 @@ $timer = new Timer();
 header::type('xml');
 $xml = new SimpleXMLElement('<pingdom_http_custom_check/>');
 $xml->addChild( 'status', 'OK' );
-$xml->addChild( 'response_time', $timer->stop() );
+$xml->addChild( 'response_time', round( $timer->stop() * 1000000, 3 )  );
 
 echo $xml->asXML();
 ?>
