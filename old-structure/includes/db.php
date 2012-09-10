fe01ecf01f06b4a90bafdc90836c330fcedc4e8d
Kerry Jones <kerry@studio98.com>
2012-09-09 21:01:26 -0700

    Added old structure

---
+++ old-structure/includes/db.php
@@ -0,0 +1,12 @@
+<?php
+/** MySQL database name */
+define('DB_NAME', 'imaginer_system');
+
+/** MySQL database username */
+define('DB_USER', 'imaginer_admin');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'rbDxn6kkj2e4');
+
+/** MySQL hostname */
+define('DB_HOST', '199.204.138.78');
