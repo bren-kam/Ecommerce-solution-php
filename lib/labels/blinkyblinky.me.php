26af894b24b33839094e3218094fabc9da740226
Kerry Jones <kerry@studio98.com>
2012-10-22 13:20:30 -0400

    Added more stuff for golds gym

---
+++ lib/labels/blinkyblinky.me.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Label Definitions
+ *
+ * This contains all the definitions specific to a label
+ *
+ * @domain BlinkyBlinky.me
+ * @since 1.0
+ */
+
+define( 'TITLE', "Gold's Gym" );
+define( 'LOGO_WIDTH', 375 );
+define( 'LOGO_HEIGHT', 67 );
+define( 'COMPANY_ID', 18 );
