ef9dfd3db471b1ead00546620150cd0e49985ab5
Kerry Jones <kerry@studio98.com>
2012-10-22 18:41:31 -0400

    Fix ticket upload links

--- view/admin/tickets/ticket.php
+++ view/admin/tickets/ticket.php
@@ -202,7 +202,7 @@ nonce::field( 'upload_to_comment', '_upload_to_comment' );
         if ( is_array( $comment->uploads ) )
         foreach ( $comment->uploads as $upload ) {
         ?>
-            <p><a href="http://s3.amazonaws.com/retailcatalog.us/attachments/<?php echo $upload['link']; ?>" target="_blank" title="<?php echo _('Download'); ?>"><?php echo $upload['name']; ?></a></p>
+            <p><a href="<?php echo $upload['link']; ?>" target="_blank" title="<?php echo _('Download'); ?>"><?php echo $upload['name']; ?></a></p>
         <?php } ?>
         </div>
         <br clear="left" />
