5521b37e786e9955f3e548fc8b2f3ef6c0ca272e
Kerry Jones <kerry@studio98.com>
2012-10-25 11:54:01 -0700

    Updated feed so that when it does get run for all of them, it only grabs live sites

--- model/admin/ashley-specific-feed-gateway.php
+++ model/admin/ashley-specific-feed-gateway.php
@@ -209,7 +209,7 @@ class AshleySpecificFeedGateway extends ActiveRecordBase {
      * @return mixed
      */
     protected function get_feed_accounts() {
-        return $this->get_results( "SELECT `website_id` FROM `website_settings` WHERE `key` = 'ashley-ftp-password' AND `value` <> ''", PDO::FETCH_CLASS, 'Account' );
+        return $this->get_results( "SELECT ws.`website_id` FROM `website_settings` AS ws LEFT JOIN `websites` AS w ON ( w.`website_id` = ws.`website_id` ) WHERE ws.`key` = 'ashley-ftp-password' AND ws.`value` <> '' AND w.`status` = 1", PDO::FETCH_CLASS, 'Account' );
     }
 
     /**
