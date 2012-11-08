1fae26b4a3a37943a1f8978fa26889ec8ab0b6be
Kerry Jones <kerry@studio98.com>
2012-11-08 00:01:58 -0800

    Updated mobile marketing to use iframe

--- old/account/theme/mobile-marketing/trumpia-form.php
+++ old/account/theme/mobile-marketing/trumpia-form.php
@@ -3,9 +3,10 @@ $w = new Websites();
 $settings = $w->get_settings( 'trumpia-username', 'trumpia-password' );
 ?>
 
-<form action="http://greysuitmobile.com/" method="post">
+<form action="http://greysuitmobile.com/action/index.act?mode=signin" method="post">
     <input type="hidden" name="id" value="<?php echo $settings['trumpia-username']; ?>" />
     <input type="hidden" name="password" value="<?php echo $settings['trumpia-password']; ?>" />
+	<input type="hidden" name="version" value="2" />
 </form>
 <script type="text/javascript">
     document.forms[0].submit();
