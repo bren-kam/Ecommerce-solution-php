c7b7d7320b769678c2876a0a6b3504cee468abd6
Kerry Jones <kerry@studio98.com>
2013-01-15 14:15:21 -0800

    Added checks to stop mass deletion of products

--- model/admin/ashley-specific-feed-gateway.php
+++ model/admin/ashley-specific-feed-gateway.php
@@ -9,8 +9,8 @@ class AshleySpecificFeedGateway extends ActiveRecordBase {
 	const FTP_URL = 'ftp.ashleyfurniture.com';
     const USER_ID = 353; // Ashley
 
-    protected $testing_sites = array( 186, 335, 477, 571, 476, 458, 357, 337, 674, 743, 829, 1067, 1101, 1140, 1141, 1184
-        , 1197, 1198
+    protected $testing_sites = array( 186, 335, 477, 571, 476, 458, 357, 337, 674, 743, 829, 1067, 1101, 1140, 1141
+        , 1148, 1184, 1197, 1198, 1199
     );
 
 	/**
@@ -127,6 +127,14 @@ class AshleySpecificFeedGateway extends ActiveRecordBase {
         $packages = $this->get_ashley_packages();
         $skus = $remove_products = $new_product_skus = $all_skus = array();
 
+        // Check #1 - Stop mass deletion
+        if ( 0 == count( $this->xml->items->item ) ) {
+            // We want to skip this account
+            mail( 'kerry@greysuitretail.com, jeff@greysuitretail.com, jack@greysuitretail.com', 'Ashley Feed w/ No Products - ' . $account->title, 'Title says it all' );
+            return;
+        }
+
+
         /**
          * @var SimpleXMLElement $item
          */
@@ -202,6 +210,15 @@ class AshleySpecificFeedGateway extends ActiveRecordBase {
 			if ( !in_array( $sku, $all_skus ) )
 				$remove_products[] = (int) $product_id;
 		}
+
+        // Check #2 - Stop mass deletion
+        $remove_product_count = count( $remove_products );
+
+        if ( $remove_product_count > 100 ) {
+            // We want to skip this account
+            mail( 'kerry@greysuitretail.com, jeff@greysuitretail.com, jack@greysuitretail.com', 'Ashley Feed Removing Too Many Products - ' . $account->title, 'Trying to remove ' . $remove_product_count . ' products' );
+            return;
+        }
 
 		// Add new products
         $industries = $account->get_industries();
