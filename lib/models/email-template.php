51286b5f499bcd6966959d74f05fb551b0e74e2f
Kerry Jones <kerry@studio98.com>
2013-01-16 14:21:41 -0800

    Added ability to add email templates

--- lib/models/email-template.php
+++ lib/models/email-template.php
@@ -1,6 +1,6 @@
 <?php
 class EmailTemplate extends ActiveRecordBase {
-    public $id, $email_template_id, $name, $template, $type, $date_created;
+    public $id, $email_template_id, $name, $template, $image, $thumbnail, $type, $date_created;
 
     /**
      * Setup the account initial data
@@ -14,6 +14,19 @@ class EmailTemplate extends ActiveRecordBase {
     }
 
     /**
+     * Get Default
+     *
+     * @param int $account_id
+     */
+    public function get_default( $account_id ) {
+        $this->prepare(
+            "SELECT * FROM `email_templates` AS et LEFT JOIN `email_template_associations` AS eta ON ( eta.`email_template_id` = et.`email_template_id` ) WHERE et.`type` = 'default' AND eta.`object_id` = :object_id AND eta.`type` = 'website'"
+            , 'i'
+            , array( ':object_id' => $account_id )
+        )->get_row( PDO::FETCH_INTO, $this );
+    }
+
+    /**
      * Create Email List
      */
     public function create() {
@@ -22,11 +35,26 @@ class EmailTemplate extends ActiveRecordBase {
         $this->insert( array(
             'name' => $this->name
             , 'template' => $this->template
+            , 'image' => $this->image
+            , 'thumbnail' => $this->thumbnail
             , 'type' => $this->type
             , 'date_created' => $this->date_created
-        ), 'ssss' );
+        ), 'ssssss' );
 
         $this->id = $this->email_template_id = $this->get_insert_id();
+    }
+
+    /**
+     * Update Email List
+     */
+    public function save() {
+        $this->update( array(
+            'name' => $this->name
+            , 'template' => $this->template
+            , 'image' => $this->image
+            , 'thumbnail' => $this->thumbnail
+            , 'type' => $this->type
+        ), array( 'email_template_id' => $this->id, ), 'sssss', 'i' );
     }
 
     /**
@@ -37,11 +65,12 @@ class EmailTemplate extends ActiveRecordBase {
      */
     public function add_association( $object_id, $type ) {
         $this->prepare(
-            'INSERT INTO `email_template_associations` ( `email_template_id`, `object_id`, `type` ) VALUES ( :email_template_id, :object_id, :type )'
-            , 'iis'
+            'INSERT INTO `email_template_associations` ( `email_template_id`, `object_id`, `type` ) VALUES ( :email_template_id, :object_id, :type ) ON DUPLICATE KEY UPDATE `object_id` = :object_id2'
+            , 'iiis'
             , array(
                 ':email_template_id' => $this->id
                 , ':object_id' => $object_id
+                , ':object_id2' => $object_id
                 , ':type' => $type
             )
         )->query();
