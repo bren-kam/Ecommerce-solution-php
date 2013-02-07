8087c95d49813cd90ea4fcbeef64dd85ad71894f
Kerry Jones <kerry@studio98.com>
2013-02-07 11:39:24 -0800

    Working on Add Bulk

--- view/account/products/add-bulk.php
+++ view/account/products/add-bulk.php
@@ -1,31 +1,19 @@
 <?php
 /**
  * @package Grey Suit Retail
- * @page Catalog Dump | Products
+ * @page Add Bulk | Products
  *
  * Declare the variables we have available from other sources
  * @var Resources $resources
  * @var Template $template
  * @var User $user
- * @var string $errs
- * @var string $js_validation
+ * @var string $form
  */
 
-echo $template->start( _('Catalog Dump') );
-
-if ( !empty( $errs ) )
-    echo "<p class='red'>$errs</p>";
+echo $template->start( _('Add Bulk') );
 ?>
-<p><?php echo _('NOTE: This will add <em>every</em> item in a selected brand into your product catalog.'); ?></p>
-<p><input type="text" class="tb" name="tAutoComplete" id="tAutoComplete" tmpval="<?php echo _('Enter Brand'); ?>..." /></p>
-<form action="/products/catalog-dump/" method="post" name="fCatalogDump">
-    <p><input type="submit" class="button" value="<?php echo _('Dump Brand'); ?>" /></p>
-    <input type="hidden" id="hBrandID" name="hBrandID" />
-    <?php nonce::field('catalog_dump'); ?>
-</form>
+<p><?php echo _("Separate SKU's by putting one on each line."); ?></p>
 <?php
-echo $js_validation;
-nonce::field( 'autocomplete', '_autocomplete' );
-
+echo $form;
 echo $template->end();
 ?>
