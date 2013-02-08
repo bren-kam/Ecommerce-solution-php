030dd5debeae90e9fb22766250e5f6f3dedbd27e
Kerry Jones <kerry@studio98.com>
2013-02-07 18:11:18 -0800

    Added Hide Categories

--- view/account/products/hide-categories.php
+++ view/account/products/hide-categories.php
@@ -1,49 +1,54 @@
 <?php
 /**
  * @package Grey Suit Retail
- * @page Block Products | Products
+ * @page Hide Categories | Products
  *
  * Declare the variables we have available from other sources
  * @var Resources $resources
  * @var Template $template
  * @var User $user
  * @var string $form
- * @var Product[] $blocked_products
+ * @var Category[] $hidden_categories
  */
 
-echo $template->start( _('Block Products') );
+echo $template->start( _('Hide Categories') );
+echo $form;
 ?>
-<p><?php echo _("Separate SKU's by putting one on each line."); ?></p>
-<?php echo $form; ?>
 <br /><br />
-<?php if ( !empty( $blocked_products ) ) { ?>
-    <h2><?php echo _('Blocked Products'); ?></h2>
+<?php if ( !empty( $hidden_categories ) ) { ?>
+    <h2><?php echo _('Hidden Categories'); ?></h2>
     <br />
-    <form action="/products/unblock-products/" method="post" name="fUnblockProducts">
+    <form action="/products/unhide-categories/" method="post" name="fUnblockCategories">
         <table class="width-auto">
             <thead>
                 <tr>
                     <th>&nbsp;</th>
                     <th class="text-left"><strong><?php echo _('Name'); ?></strong></th>
-                    <th class="text-left"><strong><?php echo _('SKU'); ?></strong></th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ( $blocked_products as $product ) { ?>
+                <?php
+                foreach ( $hidden_categories as $category ) {
+                    $parent_categories = $category->get_all_parents( $category->id );
+                    $name = $category->name;
+
+                    foreach ( $parent_categories as $pc ) {
+                        $name = $pc->name . ' &gt; ' . $name;
+                    }
+                ?>
                 <tr>
-                    <td><input type="checkbox" class="cb" name="unblock-products[]" value="<?php echo $product->id; ?>" /></td>
-                    <td><?php echo $product->name; ?></td>
-                    <td><?php echo $product->sku; ?></td>
+                    <td><input type="checkbox" class="cb" name="unhide-categories[]" value="<?php echo $category->id; ?>" id="cbUnhideCategory<?php echo $category->id; ?>" /></td>
+                    <td><label for="cbUnhideCategory<?php echo $category->id; ?>"><?php echo $name; ?></label></td>
                 </tr>
                 <?php } ?>
                 <tr><td colspan="2">&nbsp;</td></tr>
                 <tr>
                     <td>&nbsp;</td>
-                    <td><input type="submit" class="button" value="<?php echo _('Unblock Products'); ?>" /></td>
+                    <td><input type="submit" class="button" value="<?php echo _('Unhide Categories'); ?>" /></td>
                 </tr>
             </tbody>
         </table>
-        <?php nonce::field('unblock_products'); ?>
+        <?php nonce::field('unhide_categories'); ?>
     </form>
 <?php
 }
