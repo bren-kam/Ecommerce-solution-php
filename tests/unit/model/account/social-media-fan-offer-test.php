e211812198e3c3e3d3eacd141fd84fded568943f
Kerry Jones <kerry@studio98.com>
2013-03-07 16:44:29 -0800

    Added tests for Social Media Fan Offer

--- tests/unit/model/account/social-media-fan-offer-test.php
+++ tests/unit/model/account/social-media-fan-offer-test.php
@@ -17,9 +17,74 @@ class SocialMediaFanOfferTest extends BaseDatabaseTest {
     }
 
     /**
-     * Test
+     * Get
      */
-    public function testReplace() {
+    public function testGet() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $fb_page_id = -7;
+
+        // Insert
+        $this->db->insert( 'sm_fan_offer', compact( 'sm_facebook_page_id', 'fb_page_id' ), 'ii' );
+
+        // Get
+        $this->sm_fan_offer->get( $sm_facebook_page_id );
+
+        $this->assertEquals( $fb_page_id, $this->sm_fan_offer->fb_page_id );
+
+        // Clean up
+        $this->db->delete( 'sm_fan_offer', compact( 'sm_facebook_page_id' ), 'i' );
+    }
+
+    /**
+     * Test create
+     */
+    public function testCreate() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $key = 'Poke';
+
+        // Create
+        $this->sm_fan_offer->sm_facebook_page_id = $sm_facebook_page_id;
+        $this->sm_fan_offer->key = $key;
+        $this->sm_fan_offer->create();
+
+        // Get
+        $retrieved_key = $this->db->get_var( "SELECT `key` FROM `sm_fan_offer` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
+
+        $this->assertEquals( $retrieved_key, $this->sm_fan_offer->key );
+
+        // Clean up
+        $this->db->delete( 'sm_fan_offer', compact( 'sm_facebook_page_id' ), 'i' );
+    }
+
+    /**
+     * Save
+     *
+     * @depends testCreate
+     */
+    public function testSave() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $before = 'Poke';
+        $new_before = 'ekoP';
+
+        // Create
+        $this->sm_fan_offer->sm_facebook_page_id = $sm_facebook_page_id;
+        $this->sm_fan_offer->before = $before;
+        $this->sm_fan_offer->create();
+
+        // Update test
+        $this->sm_fan_offer->before = $new_before;
+        $this->sm_fan_offer->save();
+
+        // Now check it!
+        $retrieved_before = $this->db->get_var( "SELECT `before` FROM `sm_fan_offer` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
+
+        $this->assertEquals( $retrieved_before, $new_before );
+
+        // Clean up
+        $this->db->delete( 'sm_fan_offer', compact( 'sm_facebook_page_id' ), 'i' );
     }
 
     /**
