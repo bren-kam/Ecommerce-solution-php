7d887977fb5793db1e678666886f1e351e09d4b4
Kerry Jones <kerry@studio98.com>
2013-03-07 16:41:00 -0800

    Added Email Sign Up Tests

--- tests/unit/model/account/social-media-email-sign-up-test.php
+++ tests/unit/model/account/social-media-email-sign-up-test.php
@@ -17,9 +17,72 @@ class SocialMediaEmailSignUpTest extends BaseDatabaseTest {
     }
 
     /**
-     * Test
+     * Get
      */
-    public function testReplace() {
+    public function testGet() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $fb_page_id = -7;
+
+        // Insert
+        $this->db->insert( 'sm_email_sign_up', compact( 'sm_facebook_page_id', 'fb_page_id' ), 'ii' );
+
+        // Get
+        $this->sm_email_sign_up->get( $sm_facebook_page_id );
+
+        $this->assertEquals( $fb_page_id, $this->sm_email_sign_up->fb_page_id );
+
+        // Clean up
+        $this->db->delete( 'sm_email_sign_up', compact( 'sm_facebook_page_id' ), 'i' );
+    }
+
+    /**
+     * Test create
+     */
+    public function testCreate() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $key = 'Poke';
+
+        // Create
+        $this->sm_email_sign_up->sm_facebook_page_id = $sm_facebook_page_id;
+        $this->sm_email_sign_up->key = $key;
+        $this->sm_email_sign_up->create();
+
+        // Get
+        $retrieved_key = $this->db->get_var( "SELECT `key` FROM `sm_email_sign_up` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
+
+        $this->assertEquals( $retrieved_key, $this->sm_email_sign_up->key );
+
+        // Clean up
+        $this->db->delete( 'sm_email_sign_up', compact( 'sm_facebook_page_id' ), 'i' );
+    }
+
+    /**
+     * Save
+     *
+     * @depends testCreate
+     */
+    public function testSave() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $tab = 'Fogger';
+
+        // Create
+        $this->sm_email_sign_up->sm_facebook_page_id = $sm_facebook_page_id;
+        $this->sm_email_sign_up->create();
+
+        // Update test
+        $this->sm_email_sign_up->tab = $tab;
+        $this->sm_email_sign_up->save();
+
+        // Now check it!
+        $retrieved_tab = $this->db->get_var( "SELECT `tab` FROM `sm_email_sign_up` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
+
+        $this->assertEquals( $retrieved_tab, $tab );
+
+        // Clean up
+        $this->db->delete( 'sm_email_sign_up', compact( 'sm_facebook_page_id' ), 'i' );
     }
 
     /**
