ebe862c8f067cf62837f944a05ed761706fd31a5
Kerry Jones <kerry@studio98.com>
2013-03-07 16:35:26 -0800

    Added Contact Us Test

--- tests/unit/model/account/social-media-contact-us-test.php
+++ tests/unit/model/account/social-media-contact-us-test.php
@@ -17,9 +17,74 @@ class SocialMediaContactUsTest extends BaseDatabaseTest {
     }
 
     /**
-     * Test
+     * Get
      */
-    public function testReplace() {
+    public function testGet() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $fb_page_id = -7;
+
+        // Insert
+        $this->db->insert( 'sm_contact_us', compact( 'sm_facebook_page_id', 'fb_page_id' ), 'ii' );
+
+        // Get
+        $this->sm_contact_us->get( $sm_facebook_page_id );
+
+        $this->assertEquals( $fb_page_id, $this->sm_contact_us->fb_page_id );
+
+        // Clean up
+        $this->db->delete( 'sm_contact_us', compact( 'sm_facebook_page_id' ), 'i' );
+    }
+
+    /**
+     * Test create
+     */
+    public function testCreate() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $website_page_id = -7;
+
+        // Create
+        $this->sm_contact_us->sm_facebook_page_id = $sm_facebook_page_id;
+        $this->sm_contact_us->website_page_id = $website_page_id;
+        $this->sm_contact_us->create();
+
+        // Get
+        $retrieved_website_page_id = $this->db->get_var( "SELECT `website_page_id` FROM `sm_contact_us` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
+
+        $this->assertEquals( $retrieved_website_page_id, $this->sm_contact_us->website_page_id );
+
+        // Clean up
+        $this->db->delete( 'sm_contact_us', compact( 'sm_facebook_page_id' ), 'i' );
+    }
+
+    /**
+     * Save
+     *
+     * @depends testCreate
+     */
+    public function testSave() {
+        // Declare variables
+        $sm_facebook_page_id = -5;
+        $content = 'Poke';
+        $new_content = 'ekoP';
+
+        // Create
+        $this->sm_contact_us->sm_facebook_page_id = $sm_facebook_page_id;
+        $this->sm_contact_us->content = $content;
+        $this->sm_contact_us->create();
+
+        // Update test
+        $this->sm_contact_us->content = $new_content;
+        $this->sm_contact_us->save();
+
+        // Now check it!
+        $retrieved_content = $this->db->get_var( "SELECT `content` FROM `sm_contact_us` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
+
+        $this->assertEquals( $retrieved_content, $new_content );
+
+        // Clean up
+        $this->db->delete( 'sm_contact_us', compact( 'sm_facebook_page_id' ), 'i' );
     }
 
     /**
