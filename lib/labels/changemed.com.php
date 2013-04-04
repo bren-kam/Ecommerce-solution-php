b8013b3e98e9d0b2cc6a62f6f96c968edf0b478e
Kerry Jones <kerry@studio98.com>
2013-04-04 14:19:27 -0700

    Added new white label

---
+++ lib/labels/changemed.com.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Label Definitions
+ *
+ * This contains all the definitions specific to a label
+ *
+ * @domain ChangeMed.com
+ * @since 1.0
+ */
+
+define( 'TITLE', 'ChangeMed' );
+define( 'LOGO_WIDTH', 220 );
+define( 'LOGO_HEIGHT', 59 );
+define( 'COMPANY_ID', 19 );
