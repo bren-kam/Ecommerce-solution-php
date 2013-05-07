f75491cd72460c4a2404c3712bd70bbd24138c16
Kerry Jones <kerry@studio98.com>
2013-05-07 00:18:35 -0700

    Added 5 tests

--- tests/unit/model/email-list-test.php
+++ tests/unit/model/email-list-test.php
@@ -18,41 +18,166 @@ class EmailListTest extends BaseDatabaseTest {
     /**
      * Test Getting the Default Email List
      */
-    public function testGetDefaultEmailList() {
+    public function testGet() {
         // Declare variables
-        $account_id = -5;
-        $category_id = 0;
+        $website_id = -5;
+        $name = 'Dig My Dipper';
 
         // Insert an email list
-        $this->db->insert( 'email_lists', array( 'website_id' => $account_id, 'category_id' => $category_id ), 'ii' );
+        $this->db->insert( 'email_lists', compact( 'website_id', 'name' ), 'is' );
 
         $email_list_id = $this->db->get_insert_id();
 
-        $this->email_list->get_default_email_list( $account_id );
+        $this->email_list->get( $email_list_id, $website_id );
+
+        $this->assertEquals( $name, $this->email_list->name );
+
+        // Delete
+        $this->db->delete( 'email_lists',compact( 'website_id' ), 'i' );
+    }
+
+    /**
+     * Test Getting the Default Email List
+     */
+    public function testGetDefaultEmailList() {
+        // Declare variables
+        $website_id = -5;
+        $category_id = 0;
+
+        // Insert an email list
+        $this->db->insert( 'email_lists', compact( 'website_id', 'category_id' ), 'ii' );
+
+        $email_list_id = $this->db->get_insert_id();
+
+        $this->email_list->get_default_email_list( $website_id );
 
         $this->assertEquals( $email_list_id, $this->email_list->id );
 
         // Delete
-        $this->db->delete( 'email_lists', array( 'website_id' => $account_id ), 'i' );
+        $this->db->delete( 'email_lists',compact( 'website_id' ), 'i' );
+    }
+
+    /**
+     * Test Getting by account
+     */
+    public function testGetByAccount() {
+        // Declare variables
+        $website_id = -5;
+
+        // Insert an email list
+        $this->db->insert( 'email_lists', compact( 'website_id' ), 'i' );
+
+        $email_lists = $this->email_list->get_by_account( $website_id );
+
+        $this->assertTrue( current( $email_lists ) instanceof EmailList );
+
+        // Delete
+        $this->db->delete( 'email_lists',compact( 'website_id' ), 'i' );
+    }
+
+    /**
+     * Test Get Count by account
+     */
+    public function testGetCountByAccount() {
+        
     }
 
     /**
      * Test create
      */
     public function testCreate() {
-        $this->email_list->website_id = -3;
-        $this->email_list->name = 'Test Default';
+        // Declare variables
+        $website_id = -3;
+        $name = 'Test Default';
+
+        $this->email_list->website_id = $website_id;
+        $this->email_list->name = $name;
         $this->email_list->create();
 
-        $this->assertTrue( !is_null( $this->email_list->id ) );
-
         // Make sure it's in the database
-        $name = $this->db->get_var( 'SELECT `name` FROM `email_lists` WHERE `email_list_id` = ' . (int) $this->email_list->id );
+        $fetched_name = $this->db->get_var( 'SELECT `name` FROM `email_lists` WHERE `email_list_id` = ' . (int) $this->email_list->id );
 
-        $this->assertEquals( 'Test Default', $name );
+        $this->assertEquals( $name, $fetched_name );
 
         // Delete
-        $this->db->delete( 'email_lists', array( 'website_id' => $this->email_list->website_id ), 'i' );
+        $this->db->delete( 'email_lists', compact( 'website_id' ), 'i' );
+    }
+
+    /**
+     * Test save
+     *
+     * @depends testCreate
+     */
+    public function testSave() {
+        // Declare variables
+        $website_id = -3;
+        $name = 'Test Default';
+
+        // Create
+        $this->email_list->website_id = $website_id;
+        $this->email_list->create();
+
+        // Save
+        $this->email_list->name = $name;
+        $this->email_list->save();
+
+        // Make sure it's in the database
+        $fetched_name = $this->db->get_var( 'SELECT `name` FROM `email_lists` WHERE `email_list_id` = ' . (int) $this->email_list->id );
+
+        $this->assertEquals( $name, $fetched_name );
+
+        // Delete
+        $this->db->delete( 'email_lists', compact( 'website_id' ), 'i' );
+    }
+
+    /**
+     * List All
+     */
+    public function testListAll() {
+        $user = new User();
+        $user->get_by_email('test@greysuitretail.com');
+
+        // Determine length
+        $_GET['iDisplayLength'] = 30;
+        $_GET['iSortingCols'] = 1;
+        $_GET['iSortCol_0'] = 1;
+        $_GET['sSortDir_0'] = 'asc';
+
+        $dt = new DataTableResponse( $user );
+        $dt->order_by( 'el.`name`', 'el.`description`', 'el.`date_created`' );
+
+        $email_lists = $this->email_list->list_all( $dt->get_variables() );
+
+        // Make sure we have an array
+        $this->assertTrue( current( $email_lists ) instanceof EmailList );
+
+        // Get rid of everything
+        unset( $user, $_GET, $dt, $email_lists );
+    }
+
+    /**
+     * Count All
+     */
+    public function testCountAll() {
+        $user = new User();
+        $user->get_by_email('test@greysuitretail.com');
+
+        // Determine length
+        $_GET['iDisplayLength'] = 30;
+        $_GET['iSortingCols'] = 1;
+        $_GET['iSortCol_0'] = 1;
+        $_GET['sSortDir_0'] = 'asc';
+
+        $dt = new DataTableResponse( $user );
+        $dt->order_by( 'el.`name`', 'el.`description`', 'el.`date_created`' );
+
+        $count = $this->email_list->count_all( $dt->get_count_variables() );
+
+        // Make sure they exist
+        $this->assertGreaterThan( 0, $count );
+
+        // Get rid of everything
+        unset( $user, $_GET, $dt, $count );
     }
 
     /**
