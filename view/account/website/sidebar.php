9d1fd7e75c5f83198f56d721ee84bb5b322d5ef4
Kerry Jones <kerry@studio98.com>
2013-05-21 17:02:50 -0700

    Added restriction for room planner [Scrumwise task #36, status: done]

--- view/account/website/sidebar.php
+++ view/account/website/sidebar.php
@@ -3,6 +3,8 @@
  * @var Template $template
  * @var User $user
  */
+
+$industries = $user->account->get_industries();
 ?>
 <div id="sidebar">
     <a href="/website/" title="<?php echo _('Pages'); ?>" class="top first<?php $template->select('pages'); ?>"><?php echo _('Pages'); ?></a>
@@ -14,8 +16,13 @@
     <a href="/website/sidebar/" title="<?php echo _('Sidebar'); ?>" class="top<?php $template->select('sidebar'); ?>"><?php echo _('Sidebar'); ?></a>
     <a href="/website/banners/" title="<?php echo _('Banners'); ?>" class="top<?php $template->select('banners'); ?>"><?php echo _('Banners'); ?></a>
     <a href="/website/sale/" title="<?php echo _('Sale'); ?>" class="top<?php $template->select('sale'); ?>"><?php echo _('Sale'); ?></a>
-    <a href="/website/room-planner/" title="<?php echo _('Room Planner'); ?>" class="top<?php $template->select('room-planner'); ?>"><?php echo _('Room Planner'); ?></a>
-    <?php if ( $user->has_permission( User::ROLE_ONLINE_SPECIALIST ) ) { ?>
+    <?php if ( in_array( Industry::FURNITURE, $industries ) && $user->account->product_catalog ) { ?>
+        <a href="/website/room-planner/" title="<?php echo _('Room Planner'); ?>" class="top<?php $template->select('room-planner'); ?>"><?php echo _('Room Planner'); ?></a>
+    <?php
+    }
+
+    if ( $user->has_permission( User::ROLE_ONLINE_SPECIALIST ) ) {
+        ?>
         <a href="/website/settings/" title="<?php echo _('Settings'); ?>" class="top last<?php $template->select('settings'); ?>"><?php echo _('Settings'); ?></a>
     <?php } ?>
 </div>
