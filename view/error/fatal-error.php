e0d0bd7193711516752f4d26638a5f3758d49af3
Kerry Jones <kerry@studio98.com>
2013-05-29 10:48:33 -0700

    Added error screen to live site

---
+++ view/error/fatal-error.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Grey Suit Retail
+ * @page Errors > Fatal
+ *
+ * Declare the variables we have available from other sources
+ */
+?>
+<style type="text/css">
+    body {  background: none !important;}
+    body > * { display:  none }
+    html { background: transparent url(/images/error-bg.png); }
+    .fatal-error { display: block !important; }
+    img.fatal-error { margin: 200px auto 0; width: 601px; }
+</style>
+</div></div></div></body>
+<html>
+    <div class="fatal-error"><img src="/images/error.png" class="fatal-error" width="601" height="323" alt="Error" /></div>
+</html>
