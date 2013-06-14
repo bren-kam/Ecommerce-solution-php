bbab00b9de6d0e1be08855a025caf307b0fc90b6
Kerry Jones <kerry@studio98.com>
2013-06-14 10:27:48 -0700

    Fixed report

--- model/admin/reports/website-cancellations-report.php
+++ model/admin/reports/website-cancellations-report.php
@@ -27,9 +27,9 @@ class WebsiteCancellationsReport extends CustomReport {
         if ( !$this->user->has_permission( User::ROLE_ADMIN ) )
             $where .= ' AND u.`company_id` = ' . (int) $this->user->company_id;
 
-        $report = $this->get_results( "SELECT w.`title`, CONCAT( 'http://', w.`domain`, '/' ), w.`type`, w.`date_updated` FROM `websites` AS w LEFT JOIN `users` AS u ON ( u.`user_id` = w.`user_id` ) WHERE w.`status` = 0 $where ORDER BY w.`date_updated` DESC", PDO::FETCH_ASSOC );
+        $report = $this->get_results( "SELECT w.`title`, CONCAT( 'http://', w.`domain`, '/' ), w.`type`, w.`date_updated`, c.`name`, u2.`contact_name` FROM `websites` AS w LEFT JOIN `users` AS u ON ( u.`user_id` = w.`user_id` ) LEFT JOIN `companies` AS c ON ( c.`company_id` = u.`company_id` ) LEFT JOIN `users` AS u2 ON ( u2.`user_id` = w.`os_user_id` ) WHERE w.`status` = 0 $where ORDER BY w.`date_updated` DESC", PDO::FETCH_ASSOC );
 
-        array_unshift( $report, array( 'Website Title', 'Link', 'Type', 'Cancellation Date' ) );
+        array_unshift( $report, array( 'Website Title', 'Link', 'Type', 'Cancellation Date', 'Company', 'Online Specialist' ) );
 
         return $report;
     }
