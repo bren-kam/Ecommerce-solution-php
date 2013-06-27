1a9f9c695440ff521bcb1574adb8135fb57e7441
Kerry Jones <kerry@studio98.com>
2013-06-27 12:33:18 -0700

    Fixed support dropdown

--- view/account/header.php
+++ view/account/header.php
@@ -49,7 +49,7 @@ $resources->javascript( 'sparrow', 'jquery.notify', 'header' );
 	</div>
 	<div id="nav">
 		<div id="nav-links">
-            <a href="/" title="<?php echo _('Dashboard'); ?>"<?php $template->select( 'dashboard', true ); ?>><?php echo _('Dashboard'); ?></a>
+            <a href="/" title="<?php echo _('Dashboard'); ?>" class="nav-link <?php $template->select( 'dashboard' ); ?>"><?php echo _('Dashboard'); ?></a>
             <?php
             $links = array(
                 'pages'				    => array( 'website', _('Website') )
@@ -81,14 +81,14 @@ $resources->javascript( 'sparrow', 'jquery.notify', 'header' );
                         <input type="hidden" name="log" value="<?php echo security::decrypt( base64_decode( $user->account->wordpress_username ), ENCRYPTION_KEY ); ?>" />
                         <input type="hidden" name="pwd" value="<?php echo security::decrypt( base64_decode( $user->account->wordpress_password ), ENCRYPTION_KEY ); ?>" />
                     </form>
-                    <a href="javascript:document.getElementById('fBlogForm').submit();" title="<?php echo $link[1]; ?>"><?php echo $link[1]; ?></a>
+                    <a href="javascript:document.getElementById('fBlogForm').submit();" title="<?php echo $link[1]; ?>" class="nav-link"><?php echo $link[1]; ?></a>
                     <?php
                     break;
 
                     default:
                         $selection = ( isset( $exceptions[$link[0]] ) ) ? $exceptions[$link[0]] : $link[0];
                         ?>
-                        <a href="/<?php echo $link[0]; ?>/" title="<?php echo $link[1]; ?>"<?php $template->select( $selection, true ); ?>><?php echo $link[1]; ?></a>
+                        <a href="/<?php echo $link[0]; ?>/" title="<?php echo $link[1]; ?>" class="nav-link <?php $template->select( $selection ); ?>"><?php echo $link[1]; ?></a>
                         <?php
                     break;
                 }
@@ -96,20 +96,31 @@ $resources->javascript( 'sparrow', 'jquery.notify', 'header' );
 			?>
             <div id="nav-right">
                 <div id="support">
-                    <a href="#" id="aSupport" title="<?php echo _('Support'); ?>"><?php echo _('Support'); ?></a>
+                    <a href="#" id="aSupport" title="<?php echo _('Support'); ?>" class="nav-link"><?php echo _('Support'); ?></a>
                     <div id="support-drop-down" class="hidden">
-                        <a href="#" id="aTicket" title="<?php echo _('Support'); ?>"<?php echo $template->v('section_support'); ?>><?php echo _('Support Request'); ?></a>
-                        <a href="/kb/" title="<?php echo _('Knowledge Base'); ?>"<?php echo $template->v('section_support'); ?>><?php echo _('Knowledge Base'); ?></a>
+                        <a href="#" id="aTicket" title="<?php echo _('Support'); ?>" class="first top"><?php echo _('Support Request'); ?></a>
+                        <a href="/kb/" title="<?php echo _('Knowledge Base'); ?>" class="top"><?php echo _('Knowledge Base'); ?></a>
                         <?php
                         if ( !empty( $kbh_articles ) ) {
-                            foreach( $kbh_articles as $kbh_article ) {
+                            $article_count = count( $kbh_articles );
+                            $i = 0;
+
+                            foreach ( $kbh_articles as $kbh_article ) {
+                                $i++;
+                                $class = '';
+
+                                if ( 1 == $i ) {
+                                    $class = ' first';
+                                } elseif( $i == $article_count ) {
+                                    $class = ' last';
+                                }
                             ?>
-                            <a href="<?php echo url::add_query_arg( 'aid', $kbh_article->id, '/kb/article/' ); ?>" title="<?php echo $kbh_article->title; ?>" class="article" target="_blank"><?php echo $kbh_article->title; ?></a>
+                            <a href="<?php echo url::add_query_arg( 'aid', $kbh_article->id, '/kb/article/' ); ?>" title="<?php echo $kbh_article->title; ?>" class="article<?php echo $class; ?>" target="_blank"><?php echo $kbh_article->title; ?></a>
                             <?php
                             }
                         }
                         ?>
-                        <a href="/kb/browser/" title="<?php echo _('Browser Support'); ?>"><?php echo _('Browser Support'); ?></a>
+                        <a href="/kb/browser/" title="<?php echo _('Browser Support'); ?>" class="last top"><?php echo _('Browser Support'); ?></a>
                     </div>
                 </div>
             </div>
--- view/admin/header.php
+++ view/admin/header.php
@@ -13,7 +13,7 @@
 $resources->css_before( 'labels/' . DOMAIN, 'redactor', 'style');
 $resources->javascript( 'sparrow', 'jquery.notify', 'header' );
 
-$template->set( 'section_' . format::slug( $template->v('section') ), ' class="selected"');
+$template->set( 'section_' . format::slug( $template->v('section') ), ' selected');
 ?>
 <!DOCTYPE html>
 <!--[if IE 7]>
@@ -50,41 +50,53 @@ $template->set( 'section_' . format::slug( $template->v('section') ), ' class="s
 	</div>
 	<div id="nav">
 		<div id="nav-links">
-            <a href="/accounts/" title="<?php echo _('Accounts'); ?>"<?php echo $template->v('section_accounts'); ?>><?php echo _('Accounts'); ?></a>
+            <a href="/accounts/" title="<?php echo _('Accounts'); ?>" class="nav-link<?php echo $template->v('section_accounts'); ?>"><?php echo _('Accounts'); ?></a>
             <?php if ( $user->has_permission( User::ROLE_ONLINE_SPECIALIST ) ) { ?>
-                <a href="/products/" title="<?php echo _('Products'); ?>"<?php echo $template->v('section_products'); ?>><?php echo _('Products'); ?></a>
+                <a href="/products/" title="<?php echo _('Products'); ?>" class="nav-link<?php echo $template->v('section_products'); ?>"><?php echo _('Products'); ?></a>
                 <?php if ( $user->has_permission( User::ROLE_ONLINE_SPECIALIST ) ) { ?>
-                    <a href="/users/" title="<?php echo _('Users'); ?>"<?php echo $template->v('section_users'); ?>><?php echo _('Users'); ?></a>
+                    <a href="/users/" title="<?php echo _('Users'); ?>" class="nav-link<?php echo $template->v('section_users'); ?>"><?php echo _('Users'); ?></a>
                 <?php } ?>
-                <a href="/checklists/" title="<?php echo _('Checklists'); ?>"<?php echo $template->v('section_checklists'); ?>><?php echo _('Checklists'); ?></a>
-                <a href="/tickets/" title="<?php echo _('Tickets'); ?>"<?php echo $template->v('section_tickets'); ?>><?php echo _('Tickets'); ?></a>
+                <a href="/checklists/" title="<?php echo _('Checklists'); ?>" class="nav-link<?php echo $template->v('section_checklists'); ?>"><?php echo _('Checklists'); ?></a>
+                <a href="/tickets/" title="<?php echo _('Tickets'); ?>" class="nav-link<?php echo $template->v('section_tickets'); ?>"><?php echo _('Tickets'); ?></a>
                 <?php if ( $user->has_permission( User::ROLE_ONLINE_SPECIALIST ) ) { ?>
-                    <a href="/reports/" title="<?php echo _('Reports'); ?>"<?php echo $template->v('section_reports'); ?>><?php echo _('Reports'); ?></a>
+                    <a href="/reports/" title="<?php echo _('Reports'); ?>" class="nav-link<?php echo $template->v('section_reports'); ?>"><?php echo _('Reports'); ?></a>
                 <?php
                 }
             }
 
             if ( $user->has_permission( User::ROLE_ADMIN ) ) {
             ?>
-                <a href="/knowledge-base/" title="<?php echo _('Knowledge Base'); ?>"<?php echo $template->v('section_knowledge-base'); ?>><?php echo _('Knowledge Base'); ?></a>
+                <a href="/knowledge-base/" title="<?php echo _('Knowledge Base'); ?>" class="nav-link<?php echo $template->v('section_knowledge-base'); ?>"><?php echo _('Knowledge Base'); ?></a>
             <?php } ?>
             <div id="nav-right">
                 <div id="support">
-                    <a href="#" id="aSupport" title="<?php echo _('Support'); ?>"><?php echo _('Support'); ?></a>
+                    <a href="#" id="aSupport" title="<?php echo _('Support'); ?>" class="nav-link"><?php echo _('Support'); ?></a>
                     <div id="support-drop-down" class="hidden">
-                        <a href="#" id="aTicket" title="<?php echo _('Support'); ?>"<?php echo $template->v('section_support'); ?>><?php echo _('Support Request'); ?></a>
-                        <a href="/kb/" title="<?php echo _('Knowledge Base'); ?>"<?php echo $template->v('section_support'); ?>><?php echo _('Knowledge Base'); ?></a>
+                        <a href="#" id="aTicket" title="<?php echo _('Support'); ?>" class="first top"><?php echo _('Support Request'); ?></a>
+                        <a href="/kb/" title="<?php echo _('Knowledge Base'); ?>" class="top"><?php echo _('Knowledge Base'); ?></a>
                         <?php
                         if ( !empty( $kbh_articles ) ) {
-                            foreach( $kbh_articles as $kbh_article ) {
+                            $article_count = count( $kbh_articles );
+                            $i = 0;
+
+                            foreach ( $kbh_articles as $kbh_article ) {
+                                $i++;
+                                $class = '';
+
+                                if ( 1 == $i ) {
+                                    $class = ' first';
+                                } elseif( $i == $article_count ) {
+                                    $class = ' last';
+                                }
                             ?>
-                            <a href="<?php echo url::add_query_arg( 'aid', $kbh_article->id, '/kb/article/' ); ?>" title="<?php echo $kbh_article->title; ?>" class="article" target="_blank"><?php echo $kbh_article->title; ?></a>
+                            <a href="<?php echo url::add_query_arg( 'aid', $kbh_article->id, '/kb/article/' ); ?>" title="<?php echo $kbh_article->title; ?>" class="article<?php echo $class; ?>" target="_blank"><?php echo $kbh_article->title; ?></a>
                             <?php
                             }
                         }
                         ?>
-                        <a href="/kb/browser/" title="<?php echo _('Browser Support'); ?>"><?php echo _('Browser Support'); ?></a>
+                        <a href="/kb/browser/" title="<?php echo _('Browser Support'); ?>" class="last top"><?php echo _('Browser Support'); ?></a>
                     </div>
+                </div>
                 </div>
             </div>
 		</div>
