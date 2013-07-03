847263a2ddc342f16fe28bf969ab98e8a6c2af47
Kerry Jones <kerry@studio98.com>
2013-07-03 21:18:24 +0200

    Starting to add Active Campaign

---
+++ lib/ext/ac/config.php
@@ -0,0 +1,6 @@
+<?php
+
+	define("ACTIVECAMPAIGN_URL", "https://www.activecampaign.com");
+	define("ACTIVECAMPAIGN_API_KEY", "d1b7dc478909f6fc34eab65d0fa4192054db2145edd43f0aa58ae2341307850ab095ff91");
+
+?>
