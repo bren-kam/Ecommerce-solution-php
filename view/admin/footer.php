14dd73b8ea7e5204554c44c1ae8b13d357a05266
Kerry Jones <kerry@studio98.com>
2013-08-13 18:55:31 +0200

    Added Google Analytics

--- view/admin/footer.php
+++ view/admin/footer.php
@@ -49,5 +49,15 @@
 <!-- End: Footer -->
 <script type="text/javascript">head.js( '/resources/js/?f=<?php echo $resources->get_javascript_file(); ?>');</script>
 <?php $template->get_footer(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43153622-1', 'greysuitretail.com');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
