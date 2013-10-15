367a6db733e979788f2be26ce963083c5ea67991
Kerry Jones <kerry@studio98.com>
2013-10-16 00:53:27 +0200

    Added Ace Code Editor and notification for ajax resposnes

--- view/admin/accounts/customize/css.php
+++ view/admin/accounts/customize/css.php
@@ -7,9 +7,25 @@
  * @var Resources $resources
  * @var Template $template
  * @var User $user
+ * @var string $css
+ * @var Account $account
  */
 
-echo $template->start( _('CSS'), '../sidebar' );
-echo $template->v('form');
-echo $template->end();
+echo $template->start( _('CSS for ') . $account->title, '../sidebar' );
+nonce::field('save_css');
 ?>
+
+<div id="editor-container">
+    <div id="editor"><?php echo $css; ?></div>
+</div>
+<br>
+<p><a href="<?php echo url::add_query_arg( 'aid', $account->id, '/accounts/customize/save-css/' ); ?>" class="button" id="save-css" title="<?php echo _('Save'); ?>"><?php echo _('Save'); ?></a></p>
+
+<script src="http://ajaxorg.github.io/ace-builds/src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/chrome");
+    editor.getSession().setMode("ace/mode/css");
+</script>
+
+<?php echo $template->end(); ?>
