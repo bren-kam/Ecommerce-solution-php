23bba6c3bc6da6e80cc9179224606c4bd10df90d
Kerry Jones <kerry@studio98.com>
2013-11-08 09:05:25 +0000

    Added SendGrid emails

---
+++ lib/ext/sendgrid-api/email.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * SendGrid - List - API Library
+ *
+ * Library based on documentation available on 08/11/2013 from
+ * @url http://sendgrid.com/docs/API_Reference/Marketing_Emails_API/lists.html
+ *
+ */
+
+class SendGridListAPI {
+    const PREFIX = 'newsletter/lists/email/';
+
+    /**
+     * @var SendGridApi $sendgrid
+     */
+    protected $sendgrid;
+
+
+	/**
+	 * Construct class will initiate and run everything
+     *
+     * @param SendGridApi $sendgrid
+	 */
+	public function __construct( SendGridAPI $sendgrid ) {
+        $this->sendgrid = $sendgrid;
+	}
+
+    /************************************/
+    /* Start: SendGrid List API Methods */
+    /************************************/
+
+    /**
+     * Add
+     *
+     * @param string $list
+     * @param array $emails
+     * @return bool
+     */
+    public function add( $list, array $emails ) {
+        $data = array();
+
+        foreach ( $emails as $email ) {
+            $data[] = compact( 'email' );
+        }
+
+        $data = json_encode( $data );
+
+        $this->api( 'add', compact( 'list', 'data' ) );
+
+        return $this->sendgrid->success();
+    }
+
+    /**
+     * Delete
+     *
+     * @param string $list
+     * @param string $email
+     * @return bool
+     */
+    public function delete( $list, $email ) {
+        $this->api( 'delete', compact( 'list', 'email' ) );
+
+        return $this->sendgrid->success();
+    }
+
+    /**
+     * Edit
+     *
+     * @return int
+     *
+     * @param string $list
+     * @param string $newlist
+     * @return bool
+     */
+    public function edit( $list, $newlist ) {
+        $this->api( 'edit', compact( 'list', 'newlist' ) );
+
+        return $this->sendgrid->success();
+    }
+
+    /**********************************/
+    /* End: SendGrid List API Methods */
+    /**********************************/
+
+    /**
+     * API
+     *
+     * @param string $method
+     * @param $params [optional]
+     * @return stdClass object
+     */
+    protected function api( $method, $params = array() ) {
+        return $this->sendgrid->execute( self::PREFIX . $method, $params );
+    }
+}
