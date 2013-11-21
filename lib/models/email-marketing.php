bc5d980bc9b486511b5b9352f1dd1d1e935f9281
Kerry Jones <kerry@studio98.com>
2013-11-21 18:34:05 +0100

    Add cron to mark emails as sent

--- lib/models/email-marketing.php
+++ lib/models/email-marketing.php
@@ -7,60 +7,10 @@ class EmailMarketing extends ActiveRecordBase {
         parent::__construct( '' );
     }
 
-    /***** PROTECTED FUNCTIONS *****/
-
     /**
-     * Bulk unsubscribe by account
-     *
-     * @param int $account_id
-     * @param array $emails
+     * Mark sent
      */
-    protected function bulk_unsubscribe( $account_id, array $emails ) {
-        // Type Juggling
-        $account_id = (int) $account_id;
-        $email_count = count( $emails );
-        $email_format = substr( str_repeat( ',?', $email_count ), 1 );
-
-        $this->prepare(
-            "UPDATE `emails` SET `status` = 0 WHERE `website_id` = $account_id AND `email` IN ( $email_format )"
-            , str_repeat( 's', $email_count )
-            , $emails
-        );
-    }
-
-    /**
-     * Bulk mark cleaned
-     *
-     * @param int $account_id
-     * @param array $emails
-     */
-    protected function bulk_mark_cleaned( $account_id, array $emails ) {
-        // Type Juggling
-        $account_id = (int) $account_id;
-        $email_count = count( $emails );
-        $email_format = substr( str_repeat( ',?', $email_count ), 1 );
-
-        $this->prepare(
-            "UPDATE `emails` SET `status` = 2 WHERE `website_id` = $account_id AND `email` IN ( $email_format )"
-            , str_repeat( 's', $email_count )
-            , $emails
-        );
-    }
-
-    /**
-     * Synchronize emails
-     *
-     * @param array $email_ids
-     */
-    protected function synchronize_emails( array $email_ids ) {
-        // Type Juggling
-        foreach ( $email_ids as &$email_id ) {
-            $email_id = (int) $email_id;
-        }
-
-        $email_ids = implode( ',', $email_ids );
-
-        // Mark these emails as synced
-        $this->query( "UPDATE `emails` SET `date_synced` = NOW() WHERE `email_id` IN( $email_ids )" );
+    public function mark_sent() {
+        $this->query( "UPDATE `email_messages` SET `status` = 2 WHERE `status` = 1 AND `date_sent` < NOW()" );
     }
 }
