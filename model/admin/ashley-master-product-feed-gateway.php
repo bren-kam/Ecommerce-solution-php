48caeac0acef2912eeee864c046f5843f5025f00
Kerry Jones <kerry@studio98.com>
2013-11-22 17:02:48 +0100

    Adding categorization to master feed

--- model/admin/ashley-master-product-feed-gateway.php
+++ model/admin/ashley-master-product-feed-gateway.php
@@ -340,6 +340,13 @@ class AshleyMasterProductFeedGateway extends ProductFeedGateway {
 				$product->name = $name;
 				$product->slug = str_replace( '---', '-', format::slug( $name ) );
 			}
+
+            // Handle categories
+			if ( $new_product || empty( $product->category_id ) ) {
+                // Get category
+                $product->category_id = $this->get_category( $product->sku, $product->name );
+			}
+
             // $product->name = $this->identical( $name, $product->name, 'name' );
             // $product->slug = $this->identical( str_replace( '---', '-', format::slug( $name ) ), $product->slug, 'slug' );
 
@@ -453,4 +460,359 @@ class AshleyMasterProductFeedGateway extends ProductFeedGateway {
 	protected function get_brand( $retail_sales_category_code ) {
 		return ( array_key_exists( $retail_sales_category_code, $this->codes ) ) ? $this->codes[$retail_sales_category_code] : '';
 	}
+
+    /**
+     * Get Category
+     *
+     * @param string $sku
+     * @param string $name
+     * @return int
+     */
+    protected function get_category( $sku, $name ) {
+        // Setup
+        $category_id = 0;
+        $length = strlen( $sku );
+        $first_character = $sku[0];
+        $last_character = substr( $sku, -1 );
+
+        if ( 7 == $length && is_numeric( $first_character ) ) {
+            // Living Room & Leather
+            $relevant_sku = substr( $sku, 5, 2 );
+
+            // Living Room
+            switch ( $relevant_sku ) {
+                case 60:
+                case 23:
+                case 21:
+                case 20:
+                case 46: // Chairs
+                    $category_id = 221;
+                break;
+
+                case 49:
+                case 48:
+                case 77:
+                case 17:
+                case 16:
+                case 34:
+                case 67:
+                case 56:
+                case 66:
+                case 55: // Sectional Pieces
+                    $category_id = 695;
+                break;
+
+                case 14:
+                case 13:
+                case '08': // Ottomans
+                    $category_id = 229;
+                break;
+
+                case 35: // Loveseat
+                    $category_id = 220;
+                break;
+
+                case 73: // Chair w/ Ottoman
+                    $category_id = 692;
+                break;
+
+                case 37:
+                case 36:
+                case 39: // Sleeper Sofa
+                    $category_id = 425;
+                break;
+
+                case 38: // Sofa
+                    $category_id = 219;
+                break;
+
+                case 15:
+                case 18: // Chaise
+                    $category_id = 249;
+                break;
+
+                case 31:
+                case 29:
+                case 61:
+                case 26:
+                case 30:
+                case 25: // Recliners
+                    $category_id = 222;
+                break;
+
+                case 86:
+                case 94:
+                case 43: // Reclining Furniture > Reclining Love Seats
+                    $category_id = 227;
+                break;
+
+                case 74:
+                case 96:
+                case 91: // Reclining Power Loveseat
+                    $category_id = 671;
+                break;
+
+                case 98: // Power Recliner
+                    $category_id = 672;
+                break;
+
+                case 47:
+                case 87: // Reclining Power Sofa
+                    $category_id = 670;
+                break;
+
+                case 81:
+                case 88: // Reclining Furniture > Reclining Sofas
+                    $category_id = 224;
+                break;
+            }
+        } elseif ( 'T' == $first_character ) {
+            // Living Room > Occasional
+            list( $series, $relevant_sku ) = explode( '-', $sku );
+
+            switch ( $relevant_sku ) {
+                case 407:
+                case 202:
+                case 102:
+                case 3:
+                case 7:
+                case 699:
+                case 684:
+                case 668:
+                case 477:
+                case 371: // Chair Side Tables
+                    $category_id = 237;
+                break;
+
+                case 13: // Three Pack Table Sets
+                    $category_id = 251;
+                break;
+
+                case '8T':
+                case '8B':
+                case 9;
+                case 20:
+                case 8:
+                case 1:
+                case 0: // Cocktail Table
+                    $category_id = 231;
+                break;
+
+                case 232:
+                case 632:
+                case 442:
+                case 430:
+                case 360:
+                case 142:
+                case 40: // Accent Cabinet
+                    $category_id = 736;
+                break;
+
+                case 12:
+                case 306:
+                case 106: // Accent Table
+                    $category_id = 1165;
+                break;
+
+                case 615: // Magazine Racks
+                    $category_id = 738;
+                break;
+
+                case 4: // Sofa Tables
+                    $category_id = 234;
+                break;
+
+                case 17:
+                case 6:
+                case 2: // End Tables
+                    $category_id = 233;
+                break;
+
+                case 705:
+                case 804:
+                case 504: // Console
+                    $category_id = 235;
+                break;
+
+                case 11: // Cabinet
+                    $category_id = 657;
+                break;
+            }
+        } elseif ( 'W' == $first_character ) {
+            // Home Entertainment
+            list( $series, $relevant_sku ) = explode( '-', $sku );
+
+            switch ( $relevant_sku ) {
+                case '01': // Entertainment Accessories
+                    $category_id = 1166;
+                break;
+
+                case 78:
+                case 22:
+                case 68:
+                case 50:
+                case 12:
+                case '23H':
+                case '21H':
+                case 11:
+                case 20:
+                case 80:
+                case 58:
+                case '60H':
+                case 48:
+                case 22:
+                case 31:
+                case 21:
+                case 38:
+                case 28:
+                case 18:
+                case 17:
+                case 10:
+                case 60: // Consoles
+                    $category_id = 333;
+                break;
+
+                case 27:
+                case 35:
+                case 33:
+                case 25:
+                case 24:
+                case 34:
+                case '23B':
+                case 23:
+                case 26: // Wall Systems
+                    $category_id = 336;
+                break;
+
+                case 36: // Media Storage Cabinets
+                    $category_id = 622;
+                break;
+
+                case 400:
+                case 40: // Corner Cabinets
+                    $category_id = 334;
+                break;
+            }
+        } elseif ( 'D' == $first_character ) {
+            // Dining Room
+            list( $series, $relevant_sku ) = explode( '-', $sku );
+
+            switch ( $relevant_sku ) {
+                case '13T':
+                case '13B':
+                case '15T':
+                case '15B':
+                case 32:
+                case 325:
+                case 125:
+                case '50T':
+                case '50B':
+                case 13:
+                case 21:
+                case 26:
+                case 25:
+                case 15: // Tables
+                    $category_id = 130;
+                break;
+
+                case 223:
+                case 65: // Pub Tables
+                    $category_id = 144;
+                break;
+
+                case 225: // Dining Room Groups
+                    $category_id = 347;
+                break;
+
+                case 524:
+                case 424:
+                case 224:
+                case 323:
+                case 320:
+                case 324:
+                case 130:
+                case 124:
+                case 230: // Bar Stools
+                    $category_id = 142;
+                break;
+
+                case 76:
+                case 360:
+                case 160:
+                case 59:
+                case 60: // Servers
+                    $category_id = 726;
+                break;
+
+                case '05':
+                case 102:
+                case 202:
+                case 101:
+                case '06':
+                case '09':
+                case '07':
+                case '04':
+                case '03':
+                case '01': // Side Chairs
+                    $category_id = 132;
+                break;
+
+                case 80: // Buffets
+                    $category_id = 133;
+                break;
+
+                case '00': // Benches
+                    $category_id = 141;
+                break;
+
+                case '03A':
+                case '02A':
+                case '01A': // Arm Chairs
+                    $category_id = 131;
+                break;
+
+                case '65T':
+                case '65B':
+                case 65: // Side Boards
+                    $category_id = 134;
+                break;
+
+                case 76: // Bakers Racks
+                    $category_id = 135;
+                break;
+
+                case 61:
+                case 81: // China Cabinets
+                    $category_id = 212;
+                break;
+            }
+        } elseif ( 'Q' == $first_character && 8 == $length && !is_numeric( $last_character ) ) {
+            // Bedding > Bedding Ensembles
+            $category_id = 179;
+        }  elseif ( 'L' == $first_character && 7 == $length ) {
+            // Accessories > Lamps
+            $category_id = 194;
+        } elseif ( 'R' == $first_character && 7 == $length ) {
+            // Accessories > Rugs
+            $category_id = 338;
+        } elseif ( 'AC2' == substr( $sku, 0, 3 ) && 8 == $length ) {
+            // Accessories > Table Tops
+            $category_id = 341;
+        } elseif ( 'M89' == substr( $sku, 0, 3 ) && 6 == $length ) {
+            // Bedding > Pillows
+            $category_id = 597;
+        } elseif ( stristr( $name, 'Throw' ) ) {
+            // Accessories > Throws
+            $category_id = 342;
+        } elseif ( stristr( $name, 'Wall Art' ) ) {
+            // Accessories > Wall Art
+            $category_id = 339;
+        } elseif ( 'A' == $first_character && stristr( $name, 'Pillow' ) ) {
+            // Bedding > Pillows
+            $category_id = 597;
+        }
+
+
+        return $category_id;
+    }
 }
