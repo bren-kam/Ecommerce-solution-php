11d8b0fd15e68e03aa0d1515b9c6e06cd37d6b3a
Kerry Jones <kerry@studio98.com>
2013-12-02 23:00:13 +0100

    Made Website Reaches reachable from the admin side

--- tests/unit/model/website-reach-test.php
+++ tests/unit/model/website-reach-test.php
@@ -17,7 +17,6 @@ class WebsiteReachTest extends BaseDatabaseTest {
      * Will be executed before every test
      */
     public function setUp() {
-        $_SERVER['MODEL_PATH'] = basename( __DIR__ );
         $this->website_reach = new WebsiteReach();
     }
     
@@ -204,7 +203,6 @@ class WebsiteReachTest extends BaseDatabaseTest {
      * Will be executed after every test
      */
     public function tearDown() {
-        unset( $_SERVER['MODEL_PATH'] );
         $this->website_reach = null;
     }
 }
