ca2edd7c5093753482201114c97c1b9e0b569a39
Kerry Jones <kerry@studio98.com>
2013-12-10 20:16:01 +0100

    Changed CSS Layout

--- view/admin/accounts/customize/css.php
+++ view/admin/accounts/customize/css.php
@@ -7,25 +7,41 @@
  * @var Resources $resources
  * @var Template $template
  * @var User $user
- * @var string $css
+ * @var string $less
  * @var Account $account
+ * @var string|bool $unlocked_less
  */
 
-echo $template->start( _('CSS for ') . $account->title );
-nonce::field('save_css');
-?>
+echo $template->start( _('LESS CSS for ') . $account->title );
+nonce::field('save_less');
 
+if ( $unlocked_less ) {
+?>
+<h2><?php echo _('Core LESS'); ?></h2>
+<div id="core-container">
+    <div id="core"><?php echo $unlocked_less; ?></div>
+</div>
+<br><br>
+<h2><?php echo _('LESS'); ?></h2>
+<?php } ?>
 <div id="editor-container">
-    <div id="editor"><?php echo $css; ?></div>
+    <div id="editor"><?php echo $less; ?></div>
 </div>
 <br>
-<p><a href="<?php echo url::add_query_arg( 'aid', $account->id, '/accounts/customize/save-css/' ); ?>" class="button" id="save-css" title="<?php echo _('Save'); ?>"><?php echo _('Save'); ?></a></p>
+<p><a href="<?php echo url::add_query_arg( 'aid', $account->id, '/accounts/customize/save-less/' ); ?>" class="button" id="save-less" title="<?php echo _('Save'); ?>"><?php echo _('Save'); ?></a></p>
 
 <script src="http://ajaxorg.github.io/ace-builds/src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
 <script>
+    <?php if ( $unlocked_less ) {?>
+    var core = ace.edit("core");
+    core.setReadOnly(true);
+    core.setTheme("ace/theme/chrome");
+    core.getSession().setMode("ace/mode/less");
+    <?php } ?>
+
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/chrome");
-    editor.getSession().setMode("ace/mode/css");
+    editor.getSession().setMode("ace/mode/less");
 </script>
 
 <?php echo $template->end(); ?>
