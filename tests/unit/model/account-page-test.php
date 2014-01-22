88b9e60774a82d8e1ce59164ff01b328c73e22d3
Kerry Jones <kerry@studio98.com>
2014-01-21 21:51:40 -0500

    Finished Account Page tests

--- tests/unit/model/account-page-test.php
+++ tests/unit/model/account-page-test.php
@@ -130,122 +130,118 @@ class AccountPageTest extends BaseDatabaseTest {
         $this->assertEquals( $this->account_page->slug, $ph_website_page->slug );
     }
 
-//    /**
-//     * Test Copy by Account
-//     */
-//    public function testCopyByAccount() {
-//        // Declare variables
-//        $template_account_id = 96;
-//        $account_id = -5;
-//
-//        // Delete before
-//        $this->phactory->delete( 'website_pages', array( 'website_id' => $account_id ) , 'i' );
-//
-//        // Do the copying
-//        $this->account_page->copy_by_account( $template_account_id, $account_id );
-//
-//        // Get account page ids
-//        $account_page_ids = $this->phactory->get_col( "SELECT `website_page_id` FROM `website_pages` WHERE `website_id` = $account_id" );
-//
-//        $this->assertGreaterThan( 5, count( $account_page_ids ) );
-//
-//        // Delete
-//        $this->phactory->delete( 'website_pages', array( 'website_id' => $account_id ) , 'i' );
-//    }
-//
-//    /**
-//     * Remove
-//     *
-//     * @depends testGet
-//     */
-//    public function testRemove() {
-//        // Set variables
-//        $website_id = -7;
-//        $slug = 'currents-offers';
-//
-//        // Create
-//        $website_page_id = $this->phactory->insert( 'website_pages', compact( 'website_id', 'slug' ), 'is' );
-//
-//        // Get
-//        $this->account_page->get( $website_page_id, $website_id );
-//
-//        // Remove/Delete
-//        $this->account_page->remove();
-//
-//        $retrieved_slug = $this->phactory->get_var( "SELECT `slug` FROM `website_pages` WHERE `website_page_id` = $website_page_id" );
-//
-//        $this->assertFalse( $retrieved_slug );
-//    }
-//
-//    /**
-//     * Test Delete Products
-//     */
-//    public function testDeleteProducts() {
-//        // Declare Variables
-//        $this->account_page->id = $website_page_id = -7;
-//        $product_id = -5;
-//
-//        // Insert
-//        $this->phactory->insert( 'website_page_product', compact( 'website_page_id', 'product_id' ), 'ii' );
-//
-//        // Delete
-//        $this->account_page->delete_products();
-//
-//        // Get
-//        $fetched_product_id = $this->phactory->get_var( "SELECT `product_id` FROM `website_page_product` WHERE `website_page_id` = $website_page_id" );
-//
-//        $this->assertFalse( $fetched_product_id );
-//    }
-//
-//    /**
-//     * List All
-//     */
-//    public function testListAll() {
-//        $user = new User();
-//        $user->get_by_email('test@greysuitretail.com');
-//
-//        // Determine length
-//        $_GET['iDisplayLength'] = 30;
-//        $_GET['iSortingCols'] = 1;
-//        $_GET['iSortCol_0'] = 0;
-//        $_GET['sSortDir_0'] = 'asc';
-//
-//        $dt = new DataTableResponse( $user );
-//        $dt->order_by( '`title`');
-//
-//        $account_pages = $this->account_page->list_all( $dt->get_variables() );
-//
-//        // Make sure we have an array
-//        $this->assertTrue( current( $account_pages ) instanceof AccountPage );
-//
-//        // Get rid of everything
-//        unset( $user, $_GET, $dt, $emails );
-//    }
-//
-//    /**
-//     * Count All
-//     */
-//    public function testCountAll() {
-//        $user = new User();
-//        $user->get_by_email('test@greysuitretail.com');
-//
-//        // Determine length
-//        $_GET['iDisplayLength'] = 30;
-//        $_GET['iSortingCols'] = 1;
-//        $_GET['iSortCol_0'] = 1;
-//        $_GET['sSortDir_0'] = 'asc';
-//
-//        $dt = new DataTableResponse( $user );
-//        $dt->order_by( '`title`', '`status`', '`date_updated`' );
-//
-//        $count = $this->account_page->count_all( $dt->get_count_variables() );
-//
-//        // Make sure they exist
-//        $this->assertGreaterThan( 0, $count );
-//
-//        // Get rid of everything
-//        unset( $user, $_GET, $dt, $count );
-//    }
+    /**
+     * Test Copy by Account
+     */
+    public function testCopyByAccount() {
+        // Declare
+        $new_website_id = 55;
+
+        // Create
+        $this->phactory->create( 'website_pages' );
+
+        // Do the copying
+        $this->account_page->copy_by_account( self::WEBSITE_ID, $new_website_id );
+
+        // Get account page ids
+        $ph_website_page = $this->phactory->get( 'website_pages', array( 'website_id' => $new_website_id ) );
+
+        $this->assertEquals( self::SLUG, $ph_website_page->slug );
+    }
+
+    /**
+     * Remove
+     *
+     * @depends testGet
+     */
+    public function testRemove() {
+        // Insert
+        $ph_website_page = $this->phactory->create( 'website_pages' );
+
+        // Delete it!
+        $this->account_page->id = $ph_website_page->website_page_id;
+        $this->account_page->remove();
+
+        // Shouldn't exist
+        $ph_website_page = $this->phactory->get( 'website_pages', array( 'website_page_id' => $ph_website_page->website_page_id ) );
+
+        $this->assertNull( $ph_website_page );
+    }
+
+    /**
+     * Test Delete Products
+     */
+    public function testDeleteProducts() {
+        // Insert
+        $this->phactory->create( 'website_page_product' );
+
+        // Delete
+        $this->account_page->id = self::WEBSITE_PAGE_ID;
+        $this->account_page->delete_products();
+
+        // Get
+        $ph_website_page_product = $this->phactory->get( 'website_page_product', array( 'website_page_id' => self::WEBSITE_PAGE_ID ) );
+
+        $this->assertNull( $ph_website_page_product );
+    }
+
+    /**
+     * List All
+     */
+    public function testListAll() {
+        // Get mock
+        $stub_user = $this->getMock('User');
+
+        // Insert
+        $this->phactory->create( 'website_pages' );
+
+        // Determine length
+        $_GET['iDisplayLength'] = 30;
+        $_GET['iSortingCols'] = 1;
+        $_GET['iSortCol_0'] = 0;
+        $_GET['sSortDir_0'] = 'asc';
+
+        $dt = new DataTableResponse( $stub_user );
+        $dt->order_by( '`title`');
+
+        $account_pages = $this->account_page->list_all( $dt->get_variables() );
+        $account_page = current( $account_pages );
+
+        // Make sure we have an array
+        $this->assertContainsOnlyInstancesOf( 'AccountPage', $account_pages );
+        $this->assertEquals( self::SLUG, $account_page->slug );
+
+        // Get rid of everything
+        unset( $user, $_GET, $dt, $emails );
+    }
+
+    /**
+     * Count All
+     */
+    public function testCountAll() {
+        // Get mock
+        $stub_user = $this->getMock('User');
+
+        // Insert
+        $this->phactory->create( 'website_pages' );
+
+        // Determine length
+        $_GET['iDisplayLength'] = 30;
+        $_GET['iSortingCols'] = 1;
+        $_GET['iSortCol_0'] = 1;
+        $_GET['sSortDir_0'] = 'asc';
+
+        $dt = new DataTableResponse( $stub_user );
+        $dt->order_by( '`title`', '`status`', '`date_updated`' );
+
+        $count = $this->account_page->count_all( $dt->get_count_variables() );
+
+        // Make sure they exist
+        $this->assertGreaterThan( 0, $count );
+
+        // Get rid of everything
+        unset( $user, $_GET, $dt, $count );
+    }
 
     /**
      * Will be executed after every test
