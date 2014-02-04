7bd4deb192862b69800211922cd9e2f8363fc2c3
Kerry Jones <kerry@studio98.com>
2014-02-04 09:26:36 -0500

    Add tag tests

--- test/unit/model/tag-test.php
+++ test/unit/model/tag-test.php
@@ -3,6 +3,10 @@
 require_once 'test/base-database-test.php';
 
 class TagTest extends BaseDatabaseTest {
+    const OBJECT_ID = 7;
+    const TYPE = 'product';
+    const VALUE = 'Nylon';
+
     /**
      * @var Tag
      */
@@ -13,85 +17,63 @@ class TagTest extends BaseDatabaseTest {
      */
     public function setUp() {
         $this->tag = new Tag();
+
+        // Define
+        $this->phactory->define( 'tags', array( 'object_id' => self::OBJECT_ID, 'type' => self::TYPE, 'value' => self::VALUE ) );
+        $this->phactory->recall();
     }
+
     /**
-     * Test Get
+     * Test Adding several tags
      */
-    public function testReplace() {
-        // Do Stuff
+    public function testAddBulk() {
+        // Reset
+        $this->phactory->recall();
+
+        // Add tags
+        $this->tag->add_bulk( self::TYPE, self::OBJECT_ID, array( self::VALUE ) );
+
+        // Get
+        $ph_tag = $this->phactory->get( 'tags', array( 'object_id' => self::OBJECT_ID, 'type' => self::TYPE ) );
+
+        // Assert
+        $this->assertEquals( self::VALUE, $ph_tag->value );
     }
-//
-//    /**
-//     * Test Adding several tags
-//     */
-//    public function testAddBulk() {
-//        // Declare Variables
-//        $object_id = 1;
-//        $type = 'product';
-//        $tags = array( 'abc', 'def', 'ghi' );
-//
-//        // Delete any preexisting tags
-//        $this->phactory->delete( 'tags', array( 'type' => $type, 'object_id' => $object_id ), 'si' );
-//
-//        // Add tags
-//        $this->tag->add_bulk( $type, $object_id, $tags );
-//
-//        // Check to see if they exist
-//        $fetched_tags = $this->phactory->get_col( "SELECT `value` FROM `tags` WHERE `type` = '$type' AND `object_id` = $object_id ORDER BY `value` ASC" );
-//
-//        $this->assertEquals( $tags, $fetched_tags );
-//
-//        // Delete tags
-//        $this->phactory->delete( 'tags', array( 'type' => $type, 'object_id' => $object_id ), 'si' );
-//    }
-//
-//    /**
-//     * Get values by a type
-//     *
-//     * @depends testAddBulk
-//     */
-//    public function testGetValueByType() {
-//        // Declare Variables
-//        $type = 'product';
-//        $object_id = 1;
-//        $tags = array( 'knuckles', 'sonic' );
-//
-//        // Add them
-//        $this->tag->add_bulk( $type, $object_id, $tags );
-//
-//        // Get them
-//        $fetched_tags = $this->tag->get_value_by_type( $type, $object_id );
-//
-//        $this->assertEquals( $tags, $fetched_tags );
-//
-//        // Delete tags
-//        $this->phactory->delete( 'tags', array( 'type' => $type, 'object_id' => $object_id ), 'si' );
-//    }
-//
-//    /**
-//     * Delete by type
-//     *
-//     * @depends testAddBulk
-//     * @depends testGetValueByType
-//     */
-//    public function testDeleteByType() {
-//        // Declare Variables
-//        $type = 'product';
-//        $object_id = 1;
-//        $tags = array( 'mario', 'luigi' );
-//
-//        // Add tags
-//        $this->tag->add_bulk( $type, $object_id, $tags );
-//
-//        // Delete them
-//        $this->tag->delete_by_type( $type, $object_id );
-//
-//        // Fetch them
-//        $fetched_tags = $this->tag->get_value_by_type( $type, $object_id );
-//
-//        // SHouldn't be any
-//        $this->assertTrue( 0 == count( $fetched_tags ) );
-//    }
+
+    /**
+     * Get values by a type
+     */
+    public function testGetValueByType() {
+        // Reset
+        $this->phactory->recall();
+
+        // Create
+        $this->phactory->create('tags');
+
+        // Get
+        $tags = $this->tag->get_value_by_type( self::TYPE, self::OBJECT_ID );
+        $expected_tags = array( self::VALUE );
+
+        // Assert
+        $this->assertEquals( $expected_tags, $tags );
+    }
+
+    /**
+     * Delete by type
+     */
+    public function testDeleteByType() {
+        // Create
+        $this->phactory->create('tags');
+
+        // Delete them
+        $this->tag->delete_by_type( self::TYPE, self::OBJECT_ID );
+
+        // Get
+        $ph_tag = $this->phactory->get( 'tags', array( 'object_id' => self::OBJECT_ID, 'type' => self::TYPE ) );
+
+        // Assert
+        $this->assertNull( $ph_tag );
+    }
 
     /**
      * Will be executed after every test
