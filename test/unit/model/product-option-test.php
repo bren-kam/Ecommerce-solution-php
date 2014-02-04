8d483febe3295f92b01cc37c9afad1656017a0cb
Kerry Jones <kerry@studio98.com>
2014-02-03 23:47:19 -0500

    Added Product Option tests

--- test/unit/model/product-option-test.php
+++ test/unit/model/product-option-test.php
@@ -2,6 +2,12 @@
 require_once 'test/base-database-test.php';
 
 class ProductOptionTest extends BaseDatabaseTest {
+    const TITLE = 'Sofa Colors';
+    const NAME = 'Colors';
+
+    // Product Option Relations
+    const BRAND_ID = 13;
+
     /**
      * @var ProductOption
      */
@@ -12,218 +18,197 @@ class ProductOptionTest extends BaseDatabaseTest {
      */
     public function setUp() {
         $this->product_option = new ProductOption();
+
+        // Define
+        $this->phactory->define( 'product_options', array( 'option_title' => self::TITLE, 'option_name' => self::NAME ) );
+        $this->phactory->define( 'product_option_relations', array( 'brand_id' => self::BRAND_ID ) );
+        $this->phactory->define( 'product_option_list_items' );
+        $this->phactory->define( 'products', array( 'brand_id' => self::BRAND_ID ) );
+        $this->phactory->recall();
+    }
+
+
+    /**
+     * Test Getting an attribute
+     */
+    public function testGet() {
+        // Create
+        $ph_product_option = $this->phactory->create('product_options');
+
+        // Get
+        $this->product_option->get( $ph_product_option->product_option_id );
+
+        // Assert
+        $this->assertEquals( self::TITLE, $this->product_option->title );
     }
 
     /**
-     * Test Get
+     * Test Getting all product options
      */
-    public function testReplace() {
-        // Do Stuff
+    public function testGetAll() {
+        // Create
+        $this->phactory->create('product_options');
+
+        // Get
+        $product_options = $this->product_option->get_all();
+        $product_option = current( $product_options );
+
+        // Assert
+        $this->assertContainsOnlyInstancesOf( 'ProductOption', $product_options );
+        $this->assertEquals( self::TITLE, $product_option->title );
     }
-//
-//    /**
-//     * Test Getting an attribute
-//     */
-//    public function testGet() {
-//        // Declare Variables
-//        $product_option_id = 66;
-//
-//        // Get
-//        $this->product_option->get( $product_option_id );
-//
-//        $this->assertEquals( $this->product_option->title, 'Annual Sales' );
-//    }
-//
-//    /**
-//     * Test Getting all product options
-//     */
-//    public function testGetAll() {
-//        $product_options = $this->product_option->get_all();
-//
-//        $this->assertTrue( current( $product_options ) instanceof ProductOption );
-//    }
-//
-//    /**
-//     * Get By Product
-//     */
-//    public function testGetByProduct() {
-//        // Declare Variables
-//        $option_type = 'text';
-//        $brand_id = -3;
-//
-//        // Create
-//        $product_option_id = $this->phactory->insert( 'product_options', compact( 'option_type' ), 's' );
-//        $this->phactory->insert( 'product_option_relations', compact( 'product_option_id', 'brand_id' ), 'ii' );
-//        $product_id = $this->phactory->insert( 'products', compact( 'brand_id' ), 'i' );
-//
-//        // Get product Options
-//        $product_options = $this->product_option->get_by_product( $product_id );
-//
-//        $this->assertTrue( current( $product_options ) instanceof ProductOption );
-//
-//        // Clean Up
-//        $this->phactory->delete( 'product_options', compact( 'product_option_id' ), 'i' );
-//        $this->phactory->delete( 'product_option_relations', compact( 'product_option_id' ), 'i' );
-//        $this->phactory->delete( 'products', compact( 'brand_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Test create
-//     *
-//     * @depends testGet
-//     */
-//    public function testCreate() {
-//        $this->product_option->type = 'text';
-//        $this->product_option->title = 'Extra Information';
-//        $this->product_option->name = 'Extra Info';
-//        $this->product_option->create();
-//
-//        $this->assertNotNull( $this->product_option->id ) );
-//
-//        // Make sure it's in the database
-//        $this->product_option->get( $this->product_option->id );
-//
-//        $this->assertEquals( 'Extra Info', $this->product_option->name );
-//
-//        // Delete the product option
-//        $this->phactory->delete( 'product_options', array( 'product_option_id' => $this->product_option->id ), 'i' );
-//    }
-//
-//    /**
-//     * Test updating the product option
-//     *
-//     * @depends testCreate
-//     */
-//    public function testUpdate() {
-//        $this->product_option->type = 'text';
-//        $this->product_option->title = 'Extra Information';
-//        $this->product_option->name = 'Extra Info';
-//        $this->product_option->create();
-//
-//        // Update test
-//        $this->product_option->name = 'ofnI artxE';
-//        $this->product_option->save();
-//
-//        // Make sure we have an ID still
-//        $this->assertNotNull( $this->product_option->id ) );
-//
-//        // Now check it!
-//        $this->product_option->get( $this->product_option->id );
-//
-//        $this->assertEquals( 'ofnI artxE', $this->product_option->name );
-//
-//        // Delete the product option list item
-//        $this->phactory->delete( 'product_options', array( 'product_option_id' => $this->product_option->id ), 'i' );
-//    }
-//
-//    /**
-//     * Test Deleting a Product Option
-//     *
-//     * @depends testGet
-//     */
-//    public function testDelete() {
-//        // Create product option
-//        $this->phactory->insert( 'product_options', array( 'option_type' => 'select', 'option_title' => 'Temp Color Test', 'option_name' => 'Color Test' ), 'sss' );
-//
-//        $product_option_id = $this->phactory->get_insert_id();
-//
-//        // Create other relations
-//        $this->phactory->insert( 'product_options_relations', array( 'product_option_id' => $product_option_id, 'brand_id' => -1 ), 'ii' );
-//        $this->phactory->insert( 'product_option_list_items', array( 'product_option_id' => $product_option_id, 'value' => 'Test Value', 'sequence' => 0 ), 'isi' );
-//
-//
-//        // Get it
-//        $this->product_option->get( $product_option_id );
-//
-//        // Delete everything
-//        $this->product_option->delete();
-//
-//        // Make sure the product option it doesn't exist
-//        $title = $this->phactory->get_var( "SELECT `title` FROM `product_options` WHERE `product_option_id` = $product_option_id" );
-//
-//        $this->assertFalse( $title );
-//
-//        // Make sure there are no relations
-//        $brand_id = $this->phactory->get_var( "SELECT `brand_id` FROM `product_option_relations` WHERE `product_option_id` = $product_option_id" );
-//
-//        $this->assertFalse( $brand_id );
-//
-//        // Make sure there are nos
-//        $value = $this->phactory->get_var( "SELECT `value` FROM `product_option_list_items` WHERE `product_option_id` = $product_option_id" );
-//
-//        $this->assertFalse( $value );
-//    }
-//
-//    /**
-//     * Test Deleting product option relations
-//     */
-//    public function testDeleteRelationsByBrand() {
-//        // Fake Brand ID
-//        $brand_id = -5;
-//
-//        // Create relations
-//        $this->phactory->insert( 'product_options_relations', array( 'product_option_id' => 1, 'brand_id' => $brand_id ), 'ii' );
-//
-//        // Delete relations
-//        $this->product_option->delete_relations_by_brand( $brand_id );
-//
-//        // Make sure there are no relations
-//        $product_option_id = $this->phactory->get_var( "SELECT `product_option_id` FROM `product_option_relations` WHERE `brand_id` = $brand_id" );
-//
-//        $this->assertFalse( $product_option_id );
-//    }
-//
-//    /**
-//     * Test Listing all the attributes
-//     */
-//    public function testListAll() {
-//        $user = new User();
-//        $user->get_by_email('test@greysuitretail.com');
-//
-//        // Determine length
-//        $_GET['iDisplayLength'] = 30;
-//        $_GET['iSortingCols'] = 0;
-//        $_GET['iSortCol_0'] = 0;
-//        $_GET['sSortDir_0'] = 'asc';
-//
-//        $dt = new DataTableResponse( $user );
-//        $dt->order_by( '`title`', '`name`', '`type`' );
-//        $dt->search( array( '`title`' => true, '`name`' => true, '`type`' => true ) );
-//
-//        $product_options = $this->product_option->list_all( $dt->get_variables() );
-//
-//        // Make sure they exist
-//        $this->assertTrue( $product_options[0] instanceof ProductOption );
-//
-//        // Get rid of everything
-//        unset( $user, $_GET, $dt, $product_options );
-//    }
-//
-//    /**
-//     * Test counting all the attributes
-//     */
-//    public function testCountAll() {
-//        $user = new User();
-//        $user->get_by_email('test@greysuitretail.com');
-//
-//        // Determine length
-//        $_GET['iDisplayLength'] = 30;
-//        $_GET['iSortingCols'] = 0;
-//        $_GET['iSortCol_0'] = 0;
-//        $_GET['sSortDir_0'] = 'asc';
-//
-//        $dt = new DataTableResponse( $user );
-//        $dt->order_by( '`title`', '`name`', '`type`' );
-//        $dt->search( array( '`title`' => true, '`name`' => true, '`type`' => true ) );
-//
-//        $count = $this->product_option->count_all( $dt->get_count_variables() );
-//
-//        // Make sure they exist
-//        $this->assertGreaterThan( 1, $count );
-//
-//        // Get rid of everything
-//        unset( $user, $_GET, $dt, $count );
-//    }
+
+    /**
+     * Get By Product
+     */
+    public function testGetByProduct() {
+        // Create
+        $ph_product_option = $this->phactory->create('product_options');
+        $this->phactory->create( 'product_option_relations', array( 'product_option_id' => $ph_product_option->product_option_id ) );
+        $ph_product = $this->phactory->create( 'products', compact( 'brand_id' ), 'i' );
+
+        // Get product Options
+        $product_options = $this->product_option->get_by_product( $ph_product->product_id );
+        $product_option = current( $product_options );
+
+        // Assert
+        $this->assertContainsOnlyInstancesOf( 'ProductOption', $product_options );
+        $this->assertEquals( self::NAME, $product_option->name );
+    }
+
+    /**
+     * Test create
+     */
+    public function testCreate() {
+        // Create
+        $this->product_option->name = self::NAME;
+        $this->product_option->create();
+
+        // Assert
+        $this->assertNotNull( $this->product_option->id );
+
+        // Get
+        $ph_product_option = $this->phactory->get( 'product_options', array( 'product_option_id' => $this->product_option->id ) );
+
+        // Assert
+        $this->assertEquals( self::NAME, $ph_product_option->option_name );
+    }
+
+    /**
+     * Test updating the product option
+\     */
+    public function testUpdate() {
+        // Create
+        $ph_product_option = $this->phactory->create('product_options');
+
+        // Update
+        $this->product_option->id = $ph_product_option->product_option_id;
+        $this->product_option->name = 'Shades';
+        $this->product_option->save();
+
+        // Get
+        $ph_product_option = $this->phactory->get( 'product_options', array( 'product_option_id' => $this->product_option->id ) );
+
+        // Assert
+        $this->assertEquals( $this->product_option->name, $ph_product_option->option_name );
+    }
+
+    /**
+     * Test Deleting a Product Option
+     */
+    public function testDelete() {
+        // Create
+        $ph_product_option = $this->phactory->create('product_options');
+
+        // Delete
+        $this->product_option->id = $ph_product_option->product_option_id;
+        $this->product_option->remove();
+
+        // Get
+        $ph_product_option = $this->phactory->get( 'product_options', array( 'product_option_id' => $this->product_option->id ) );
+
+        // Assert
+        $this->assertNull( $ph_product_option );
+    }
+
+    /**
+     * Test Deleting product option relations
+     */
+    public function testDeleteRelationsByBrand() {
+        // Create
+        $this->phactory->create('product_option_relations');
+
+        // Delete relations
+        $this->product_option->delete_relations_by_brand( self::BRAND_ID );
+
+        // Get
+        $ph_product_option_relation = $this->phactory->get( 'product_option_relations', array( 'brand_id' => self::BRAND_ID ) );
+
+        // Assert
+        $this->assertNull( $ph_product_option_relation );
+    }
+
+    /**
+     * Test Listing all the attributes
+     */
+    public function testListAll() {
+        // Get Stub
+        $stub_user = $this->getMock('User');
+
+        // Create
+        $this->phactory->create('product_options');
+
+        // Determine length
+        $_GET['iDisplayLength'] = 30;
+        $_GET['iSortingCols'] = 0;
+        $_GET['iSortCol_0'] = 0;
+        $_GET['sSortDir_0'] = 'asc';
+
+        $dt = new DataTableResponse( $stub_user );
+        $dt->order_by( '`title`', '`name`', '`type`' );
+        $dt->search( array( '`title`' => true, '`name`' => true, '`type`' => true ) );
+
+        // Get
+        $product_options = $this->product_option->list_all( $dt->get_variables() );
+        $product_option = current( $product_options );
+
+        // Assert
+        $this->assertContainsOnlyInstancesOf( 'ProductOption', $product_options );
+        $this->assertEquals( self::TITLE, $product_option->title );
+
+        // Get rid of everything
+        unset( $user, $_GET, $dt, $product_options );
+    }
+
+    /**
+     * Test counting all the attributes
+     */
+    public function testCountAll() {
+        // Get Stub
+        $stub_user = $this->getMock('User');
+
+        // Create
+        $this->phactory->create('product_options');
+
+        // Determine length
+        $_GET['iDisplayLength'] = 30;
+        $_GET['iSortingCols'] = 0;
+        $_GET['iSortCol_0'] = 0;
+        $_GET['sSortDir_0'] = 'asc';
+
+        $dt = new DataTableResponse( $stub_user );
+        $dt->order_by( '`title`', '`name`', '`type`' );
+        $dt->search( array( '`title`' => true, '`name`' => true, '`type`' => true ) );
+
+        // Get
+        $count = $this->product_option->count_all( $dt->get_count_variables() );
+
+        // Make sure they exist
+        $this->assertGreaterThan( 0, $count );
+
+        // Get rid of everything
+        unset( $user, $_GET, $dt, $count );
+    }
 
     /**
      * Will be executed after every test
