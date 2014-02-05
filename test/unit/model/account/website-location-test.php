4bd8194437726b13e3354232a72acd98a9d07190
Kerry Jones <kerry@studio98.com>
2014-02-05 18:02:37 -0500

    Added Website Location test

--- test/unit/model/account/website-location-test.php
+++ test/unit/model/account/website-location-test.php
@@ -3,6 +3,8 @@
 require_once 'test/base-database-test.php';
 
 class WebsiteLocationTest extends BaseDatabaseTest {
+    const NAME = 'Brooklyn Location';
+
     /**
      * @var WebsiteLocation
      */
@@ -14,13 +16,24 @@ class WebsiteLocationTest extends BaseDatabaseTest {
     public function setUp() {
         $_SERVER['MODEL_PATH'] = basename( __DIR__ );
         $this->website_location = new WebsiteLocation();
+
+        // Define
+        $this->phactory->define( 'website_location', array( 'website_id' => self::WEBSITE_ID, 'name' => self::NAME ) );
+        $this->phactory->recall();
     }
 
     /**
      * Test Get
      */
-    public function testReplace() {
-        // Do Stuff
+    public function testGet() {
+        // Create
+        $ph_website_location = $this->phactory->create('website_location');
+
+        // Get
+        $this->website_location->get( $ph_website_location->id, self::WEBSITE_ID );
+
+        // Assert
+        $this->assertEquals( self::NAME, $this->website_location->name );
     }
 
     /**
