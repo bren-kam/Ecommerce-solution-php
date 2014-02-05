53528baab9dd4ba6ad5f6eca5fe384726ac5b989
Kerry Jones <kerry@studio98.com>
2014-02-05 17:14:59 -0500

    Added Social Media About Us tests

--- test/unit/model/account/social-media-about-us-test.php
+++ test/unit/model/account/social-media-about-us-test.php
@@ -3,6 +3,10 @@
 require_once 'test/base-database-test.php';
 
 class SocialMediaAboutUsTest extends BaseDatabaseTest {
+    const SM_FACEBOOK_PAGE_ID = 5;
+    const FB_PAGE_ID = 3;
+    const WEBSITE_PAGE_ID = 9;
+
     /**
      * @var SocialMediaAboutUs
      */
@@ -14,82 +18,61 @@ class SocialMediaAboutUsTest extends BaseDatabaseTest {
     public function setUp() {
         $_SERVER['MODEL_PATH'] = basename( __DIR__ );
         $this->sm_about_us = new SocialMediaAboutUs();
+
+        // Define
+        $this->phactory->define( 'sm_about_us', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID, 'fb_page_id' => self::FB_PAGE_ID, 'website_page_id' => self::WEBSITE_PAGE_ID ) );
+        $this->phactory->recall();
     }
+
+
     /**
-     * Test Get
+     * Get
      */
-    public function testReplace() {
-        // Do Stuff
+    public function testGet() {
+        // Create
+        $this->phactory->create('sm_about_us');
+
+        // Get
+        $this->sm_about_us->get( self::SM_FACEBOOK_PAGE_ID );
+
+        // Assert
+        $this->assertEquals( self::FB_PAGE_ID, $this->sm_about_us->fb_page_id );
     }
-//
-//    /**
-//     * Get
-//     */
-//    public function testGet() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $fb_page_id = -7;
-//
-//        // Insert
-//        $this->phactory->insert( 'sm_about_us', compact( 'sm_facebook_page_id', 'fb_page_id' ), 'ii' );
-//
-//        // Get
-//        $this->sm_about_us->get( $sm_facebook_page_id );
-//
-//        $this->assertEquals( $fb_page_id, $this->sm_about_us->fb_page_id );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_about_us', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Test create
-//     */
-//    public function testCreate() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $website_page_id = -7;
-//
-//        // Create
-//        $this->sm_about_us->sm_facebook_page_id = $sm_facebook_page_id;
-//        $this->sm_about_us->website_page_id = $website_page_id;
-//        $this->sm_about_us->create();
-//
-//        // Get
-//        $retrieved_website_page_id = $this->phactory->get_var( "SELECT `website_page_id` FROM `sm_about_us` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
-//
-//        $this->assertEquals( $retrieved_website_page_id, $this->sm_about_us->website_page_id );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_about_us', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Save
-//     *
-//     * @depends testCreate
-//     */
-//    public function testSave() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $content = 'Poke';
-//
-//        // Create
-//        $this->sm_about_us->sm_facebook_page_id = $sm_facebook_page_id;
-//        $this->sm_about_us->create();
-//
-//        // Update test
-//        $this->sm_about_us->content = $content;
-//        $this->sm_about_us->save();
-//
-//        // Now check it!
-//        $retrieved_content = $this->phactory->get_var( "SELECT `content` FROM `sm_about_us` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
-//
-//        $this->assertEquals( $retrieved_content, $content );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_about_us', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
+
+    /**
+     * Test create
+     */
+    public function testCreate() {
+        // Create
+        $this->sm_about_us->sm_facebook_page_id = self::SM_FACEBOOK_PAGE_ID;
+        $this->sm_about_us->website_page_id = self::WEBSITE_PAGE_ID;
+        $this->sm_about_us->create();
+
+        // Get
+        $ph_sm_about_us = $this->phactory->get( 'sm_about_us', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID) );
+
+        // Get
+        $this->assertEquals( self::WEBSITE_PAGE_ID, $ph_sm_about_us->website_page_id );
+    }
+
+    /**
+     * Save
+     */
+    public function testSave() {
+        // Create
+        $this->phactory->create('sm_about_us');
+
+        // Save
+        $this->sm_about_us->sm_facebook_page_id = self::SM_FACEBOOK_PAGE_ID;
+        $this->sm_about_us->content = 'Help us help you!';
+        $this->sm_about_us->save();
+
+        // Get
+        $ph_sm_about_us = $this->phactory->get( 'sm_about_us', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID) );
+
+        // Get
+        $this->assertEquals( $this->sm_about_us->content, $ph_sm_about_us->content );
+    }
 
     /**
      * Will be executed after every test
