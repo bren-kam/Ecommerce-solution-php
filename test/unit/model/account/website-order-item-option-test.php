44f6af0066fa6886d2fa0e3cee047e5a4ef1ba5b
Kerry Jones <kerry@studio98.com>
2014-02-05 18:07:38 -0500

    Add website order item option tests

--- test/unit/model/account/website-order-item-option-test.php
+++ test/unit/model/account/website-order-item-option-test.php
@@ -3,6 +3,12 @@
 require_once 'test/base-database-test.php';
 
 class WebsiteOrderItemOptionTest extends BaseDatabaseTest {
+    const WEBSITE_ORDER_ITEM_ID = 5;
+    const OPTION_NAME = 'Queen Mattress';
+
+    // Website Order Item
+    const WEBSITE_ORDER_ID = 13;
+
     /**
      * @var WebsiteOrderItemOption
      */
@@ -14,40 +20,31 @@ class WebsiteOrderItemOptionTest extends BaseDatabaseTest {
     public function setUp() {
         $_SERVER['MODEL_PATH'] = basename( __DIR__ );
         $this->website_order_item_option = new WebsiteOrderItemOption();
+
+        // Define
+        $this->phactory->define( 'website_order_item_options', array( 'website_order_item_id' => self::WEBSITE_ORDER_ITEM_ID, 'option_name' => self::OPTION_NAME ) );
+        $this->phactory->define( 'website_order_items', array( 'website_order_id' => self::WEBSITE_ORDER_ID ) );
+        $this->phactory->recall();
     }
+
+
     /**
-     * Test Get
+     * Get By Order
      */
-    public function testReplace() {
-        // Do Stuff
+    public function testGetByOrder() {
+        // Create
+        $ph_website_order_item = $this->phactory->create('website_order_items');
+        $this->phactory->create( 'website_order_item_options', array( 'website_order_item_id' => $ph_website_order_item->website_order_item_id ) );
+
+        // Get
+        $website_order_item_options = $this->website_order_item_option->get_by_order( self::WEBSITE_ORDER_ID );
+        $website_order_item_option = current( $website_order_item_options );
+
+        // Assert
+        $this->assertContainsOnlyInstancesOf( 'WebsiteOrderItemOption', $website_order_item_options );
+        $this->assertEquals( self::OPTION_NAME, $website_order_item_option->option_name );
+
     }
-//
-//    /**
-//     * Get By Order
-//     */
-//    public function testGetByOrder() {
-//        // Set variables
-//        $website_order_id = -3;
-//
-//        // Insert website order item
-//        $website_order_item_id = $this->phactory->insert( 'website_order_items', array(
-//            'website_order_id' => $website_order_id
-//        ), 'i' );
-//
-//        // Create option
-//        $this->phactory->insert( 'website_order_item_options', array(
-//            'website_order_item_id' => $website_order_item_id
-//        ), 'i' );
-//
-//        // Get options
-//        $options = $this->website_order_item_option->get_by_order( $website_order_id );
-//
-//        $this->assertTrue( current( $options ) instanceof WebsiteOrderItemOption );
-//
-//        // Clean up
-//        $this->phactory->delete( 'website_order_items', array( 'website_order_id' => $website_order_id ), 'i' );
-//        $this->phactory->delete( 'website_order_item_options', array( 'website_order_item_id' => $website_order_item_id ), 'i' );
-//    }
 
     /**
      * Will be executed after every test
