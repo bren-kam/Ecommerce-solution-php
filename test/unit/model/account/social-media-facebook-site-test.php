2f3575307a8070506e07b02bcc2542e46919d801
Kerry Jones <kerry@studio98.com>
2014-02-05 17:26:18 -0500

    Added SM FB Site tests

--- test/unit/model/account/social-media-facebook-site-test.php
+++ test/unit/model/account/social-media-facebook-site-test.php
@@ -3,6 +3,11 @@
 require_once 'test/base-database-test.php';
 
 class SocialMediaFacebookSiteTest extends BaseDatabaseTest {
+    const SM_FACEBOOK_PAGE_ID = 5;
+    const FB_PAGE_ID = 3;
+    const KEY = 'beal';
+    const CONTENT = 'Misty Mountains';
+    
     /**
      * @var SocialMediaFacebookSite
      */
@@ -14,82 +19,63 @@ class SocialMediaFacebookSiteTest extends BaseDatabaseTest {
     public function setUp() {
         $_SERVER['MODEL_PATH'] = basename( __DIR__ );
         $this->sm_facebook_site = new SocialMediaFacebookSite();
+        
+        // Define
+        $this->phactory->define( 'sm_facebook_site', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID, 'fb_page_id' => self::FB_PAGE_ID, 'key' => self::KEY, 'content' => self::CONTENT ) );
+        $this->phactory->recall();
     }
+    
     /**
-     * Test Get
+     * Get
      */
-    public function testReplace() {
-        // Do Stuff
+    public function testGet() {
+        // Create
+        $this->phactory->create('sm_facebook_site');
+
+        // Get
+        $this->sm_facebook_site->get( self::SM_FACEBOOK_PAGE_ID );
+
+        // Assert
+        $this->assertEquals( self::FB_PAGE_ID, $this->sm_facebook_site->fb_page_id );
     }
-//
-//    /**
-//     * Get
-//     */
-//    public function testGet() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $fb_page_id = -7;
-//
-//        // Insert
-//        $this->phactory->insert( 'sm_facebook_site', compact( 'sm_facebook_page_id', 'fb_page_id' ), 'ii' );
-//
-//        // Get
-//        $this->sm_facebook_site->get( $sm_facebook_page_id );
-//
-//        $this->assertEquals( $fb_page_id, $this->sm_facebook_site->fb_page_id );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_facebook_site', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Test create
-//     */
-//    public function testCreate() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $key = 'Poke';
-//
-//        // Create
-//        $this->sm_facebook_site->sm_facebook_page_id = $sm_facebook_page_id;
-//        $this->sm_facebook_site->key = $key;
-//        $this->sm_facebook_site->create();
-//
-//        // Get
-//        $retrieved_key = $this->phactory->get_var( "SELECT `key` FROM `sm_facebook_site` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
-//
-//        $this->assertEquals( $retrieved_key, $this->sm_facebook_site->key );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_facebook_site', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Save
-//     *
-//     * @depends testCreate
-//     */
-//    public function testSave() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $content = 'Poke';
-//
-//        // Create
-//        $this->sm_facebook_site->sm_facebook_page_id = $sm_facebook_page_id;
-//        $this->sm_facebook_site->create();
-//
-//        // Update test
-//        $this->sm_facebook_site->content = $content;
-//        $this->sm_facebook_site->save();
-//
-//        // Now check it!
-//        $retrieved_content = $this->phactory->get_var( "SELECT `content` FROM `sm_facebook_site` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
-//
-//        $this->assertEquals( $retrieved_content, $content );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_facebook_site', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
+
+
+    /**
+     * Test create
+     */
+    public function testCreate() {
+        // Create
+        $this->sm_facebook_site->sm_facebook_page_id = self::SM_FACEBOOK_PAGE_ID;
+        $this->sm_facebook_site->key = self::KEY;
+        $this->sm_facebook_site->create();
+
+        // Get
+        $ph_sm_facebook_site = $this->phactory->get( 'sm_facebook_site', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID) );
+
+        // Get
+        $this->assertEquals( self::KEY, $ph_sm_facebook_site->key );
+    }
+
+    /**
+     * Save
+     *
+     * @depends testCreate
+     */
+    public function testSave() {
+        // Create
+        $this->phactory->create('sm_facebook_site');
+
+        // Save
+        $this->sm_facebook_site->sm_facebook_page_id = self::SM_FACEBOOK_PAGE_ID;
+        $this->sm_facebook_site->content = self::CONTENT;
+        $this->sm_facebook_site->save();
+
+        // Get
+        $ph_sm_facebook_site = $this->phactory->get( 'sm_facebook_site', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID) );
+
+        // Get
+        $this->assertEquals( self::CONTENT, $ph_sm_facebook_site->content );
+    }
 
     /**
      * Will be executed after every test
