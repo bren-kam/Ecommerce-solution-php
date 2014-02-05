36958bd4757ed4a6bb70ab89f9f146530e69517d
Kerry Jones <kerry@studio98.com>
2014-02-05 17:28:20 -0500

    Added SM Fan Offer tests

--- test/unit/model/account/social-media-fan-offer-test.php
+++ test/unit/model/account/social-media-fan-offer-test.php
@@ -3,6 +3,11 @@
 require_once 'test/base-database-test.php';
 
 class SocialMediaFanOfferTest extends BaseDatabaseTest {
+    const SM_FACEBOOK_PAGE_ID = 5;
+    const FB_PAGE_ID = 3;
+    const KEY = 'beal';
+    const BEFORE = 'Misty Mountains';
+    
     /**
      * @var SocialMediaFanOffer
      */
@@ -14,83 +19,64 @@ class SocialMediaFanOfferTest extends BaseDatabaseTest {
     public function setUp() {
         $_SERVER['MODEL_PATH'] = basename( __DIR__ );
         $this->sm_fan_offer = new SocialMediaFanOffer();
+        
+        // Define
+        $this->phactory->define( 'sm_fan_offer', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID, 'fb_page_id' => self::FB_PAGE_ID, 'key' => self::KEY, 'before' => self::BEFORE ) );
+        $this->phactory->recall();
     }
+    
     /**
-     * Test Get
+     * Get
      */
-    public function testReplace() {
-        // Do Stuff
-    }
-//
-//    /**
-//     * Get
-//     */
-//    public function testGet() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $fb_page_id = -7;
-//
-//        // Insert
-//        $this->phactory->insert( 'sm_fan_offer', compact( 'sm_facebook_page_id', 'fb_page_id' ), 'ii' );
-//
-//        // Get
-//        $this->sm_fan_offer->get( $sm_facebook_page_id );
-//
-//        $this->assertEquals( $fb_page_id, $this->sm_fan_offer->fb_page_id );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_fan_offer', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Test create
-//     */
-//    public function testCreate() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $key = 'Poke';
-//
-//        // Create
-//        $this->sm_fan_offer->sm_facebook_page_id = $sm_facebook_page_id;
-//        $this->sm_fan_offer->key = $key;
-//        $this->sm_fan_offer->create();
-//
-//        // Get
-//        $retrieved_key = $this->phactory->get_var( "SELECT `key` FROM `sm_fan_offer` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
-//
-//        $this->assertEquals( $retrieved_key, $this->sm_fan_offer->key );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_fan_offer', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Save
-//     *
-//     * @depends testCreate
-//     */
-//    public function testSave() {
-//        // Declare variables
-//        $sm_facebook_page_id = -5;
-//        $before = 'Poke';
-//
-//        // Create
-//        $this->sm_fan_offer->sm_facebook_page_id = $sm_facebook_page_id;
-//        $this->sm_fan_offer->create();
-//
-//        // Update test
-//        $this->sm_fan_offer->before = $before;
-//        $this->sm_fan_offer->save();
-//
-//        // Now check it!
-//        $retrieved_before = $this->phactory->get_var( "SELECT `before` FROM `sm_fan_offer` WHERE `sm_facebook_page_id` = $sm_facebook_page_id" );
-//
-//        $this->assertEquals( $retrieved_before, $before );
-//
-//        // Clean up
-//        $this->phactory->delete( 'sm_fan_offer', compact( 'sm_facebook_page_id' ), 'i' );
-//    }
+    public function testGet() {
+        // Create
+        $this->phactory->create('sm_fan_offer');
 
+        // Get
+        $this->sm_fan_offer->get( self::SM_FACEBOOK_PAGE_ID );
+
+        // Assert
+        $this->assertEquals( self::FB_PAGE_ID, $this->sm_fan_offer->fb_page_id );
+    }
+
+
+    /**
+     * Test create
+     */
+    public function testCreate() {
+        // Create
+        $this->sm_fan_offer->sm_facebook_page_id = self::SM_FACEBOOK_PAGE_ID;
+        $this->sm_fan_offer->key = self::KEY;
+        $this->sm_fan_offer->create();
+
+        // Get
+        $ph_sm_fan_offer = $this->phactory->get( 'sm_fan_offer', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID) );
+
+        // Get
+        $this->assertEquals( self::KEY, $ph_sm_fan_offer->key );
+    }
+
+    /**
+     * Save
+     *
+     * @depends testCreate
+     */
+    public function testSave() {
+        // Create
+        $this->phactory->create('sm_fan_offer');
+
+        // Save
+        $this->sm_fan_offer->sm_facebook_page_id = self::SM_FACEBOOK_PAGE_ID;
+        $this->sm_fan_offer->before = self::BEFORE;
+        $this->sm_fan_offer->save();
+
+        // Get
+        $ph_sm_fan_offer = $this->phactory->get( 'sm_fan_offer', array( 'sm_facebook_page_id' => self::SM_FACEBOOK_PAGE_ID) );
+
+        // Get
+        $this->assertEquals( self::BEFORE, $ph_sm_fan_offer->before );
+    }
+    
     /**
      * Will be executed after every test
      */
