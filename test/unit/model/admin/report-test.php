91e350fba090c550753927a864a260189de57df7
Kerry Jones <kerry@studio98.com>
2014-02-06 12:19:26 -0500

    Add company packages

--- test/unit/model/admin/report-test.php
+++ test/unit/model/admin/report-test.php
@@ -3,6 +3,23 @@
 require_once 'test/base-database-test.php';
 
 class ReportTest extends BaseDatabaseTest {
+    const TITLE = 'Winfrey Furniture';
+
+    // Products
+    const PRODUCT_NAME = 'Cecile 4x4 Chair';
+
+    // Brands
+    const BRAND_NAME = "Cecile's Chairs";
+
+    // Users
+    const CONTACT_NAME = 'Will Young';
+
+    // Companies
+    const COMPANY_NAME = 'Inventive Marketing';
+
+    // Company Packages
+    const COMPANY_PACKAGE_NAME = 'E-Commerce Solution';
+
     /**
      * @var Report
      */
@@ -14,94 +31,124 @@ class ReportTest extends BaseDatabaseTest {
     public function setUp() {
         $_SERVER['MODEL_PATH'] = basename( __DIR__ );
         $this->report = new Report();
+
+        // Define
+        $this->phactory->define( 'websites', array( 'title' => self::TITLE, 'status' => Account::STATUS_ACTIVE ) );
+        $this->phactory->define( 'products', array( 'name' => self::PRODUCT_NAME ) );
+        $this->phactory->define( 'brands', array( 'name' => self::BRAND_NAME ) );
+        $this->phactory->define( 'website_products' );
+        $this->phactory->define( 'users', array( 'contact_name' => self::CONTACT_NAME, 'role' => User::ROLE_ONLINE_SPECIALIST, 'status' => User::STATUS_ACTIVE ) );
+        $this->phactory->define( 'companies', array( 'name' => self::COMPANY_NAME ) );
+        $this->phactory->define( 'company_packages', array( 'name' => self::COMPANY_PACKAGE_NAME ) );
+        $this->phactory->recall();
     }
+
+
     /**
-     * Test Get
+     * Test search
      */
-    public function testReplace() {
-        // Do Stuff
+    public function testSearch() {
+        // Create
+        $ph_website = $this->phactory->create('websites');
+        $ph_brand = $this->phactory->create('brands');
+        $ph_product = $this->phactory->create( 'products', array( 'brand_id' => $ph_brand->brand_id ) );
+        $this->phactory->create( 'website_products', array( 'website_id' => $ph_website->website_id, 'product_id' => $ph_product->product_id ) );
+
+        // Declare
+        $where = ' AND ( p.`brand_id` IN( ' . $ph_brand->brand_id . ' ) )';
+
+        // Get
+        $accounts = $this->report->search( $where );
+
+        // Assert
+        $this->assertEquals( self::TITLE, $accounts[0]->title );
     }
-//
-//    /**
-//     * Test search
-//     */
-//    public function testSearch() {
-//        // Declare Variables
-//        $where = ' AND ( p.`brand_id` IN( 8 ) )';
-//
-//        // Get report
-//        $accounts = $this->report->search( $where );
-//
-//        $this->assertTrue( isset( $accounts[0]->title ) );
-//    }
-//
-//    /**
-//     * Test Autocomplete for brands
-//     */
-//    public function testAutocompleteBrands() {
-//        // Declare Variables
-//        $query = 'Ashley';
-//
-//        // Get autocomplete entries
-//        $entries = $this->report->autocomplete_brands( $query );
-//
-//        $this->assertTrue( stristr( $entries[0]['brand'], $query ) !== false );
-//    }
-//
-//    /**
-//     * Test Autocomplete for online specialists
-//     */
-//    public function testAutocompleteOnlineSpecialists() {
-//        // Declare variables
-//        $query = 'Heather';
-//        $where = '';
-//
-//        // Get autocomplete entries
-//        $entries = $this->report->autocomplete_online_specialists( $query, $where );
-//
-//        $this->assertTrue( stristr( $entries[0]['online_specialist'], $query ) !== false );
-//    }
-//
-//    /**
-//     * Test Autocomplete for Marketing Specialists
-//     */
-//    public function testAutocompleteMarketingSpecialists() {
-//        // Declare Variables
-//        $query = 'David';
-//
-//        // Get autocomplete entries
-//        $entries = $this->report->autocomplete_marketing_specialists( $query );
-//
-//        $this->assertTrue( stristr( $entries[0]['marketing_specialist'], $query ) !== false );
-//    }
-//
-//    /**
-//     * Test Autocomplete for Companies
-//     */
-//    public function testAutocompleteCompanies() {
-//        // Declare Variables
-//        $query = 'Imagine';
-//        $where = '';
-//
-//        // Get autocomplete entries
-//        $entries = $this->report->autocomplete_companies( $query, $where );
-//
-//        $this->assertTrue( stristr( $entries[0]['company'], $query ) !== false );
-//    }
-//
-//    /**
-//     * Test Autocomplete for Company Packages
-//     */
-//    public function testAutocompleteCompanyPackages() {
-//        // Declare Variables
-//        $query = 'Furnish';
-//        $where = '';
-//
-//        // Get autocomplete entries
-//        $entries = $this->report->autocomplete_company_packages( $query, $where );
-//
-//        $this->assertTrue( stristr( $entries[0]['package'], $query ) !== false );
-//    }
+
+    /**
+     * Test Autocomplete for brands
+     */
+    public function testAutocompleteBrands() {
+        // Create
+        $this->phactory->create('brands');
+
+        // Declare
+        $query = substr( self::BRAND_NAME, 0 , 3 );
+
+        // Get
+        $entries = $this->report->autocomplete_brands( $query );
+
+        // Assert
+        $this->assertEquals( self::BRAND_NAME, $entries[0]['brand'] );
+    }
+
+    /**
+     * Test Autocomplete for online specialists
+     */
+    public function testAutocompleteOnlineSpecialists() {
+        // Create
+        $ph_user = $this->phactory->create('users');
+        $this->phactory->create( 'websites', array( 'os_user_id' => $ph_user->user_id ) );
+
+        // Declare
+        $query = substr( self::CONTACT_NAME, 0, 3 );
+
+        // Get
+        $entries = $this->report->autocomplete_online_specialists( $query );
+
+        // Assert
+        $this->assertEquals( self::CONTACT_NAME, $entries[0]['online_specialist'] );
+    }
+
+    /**
+     * Test Autocomplete for Marketing Specialists
+     */
+    public function testAutocompleteMarketingSpecialists() {
+        // Create
+        $this->phactory->create( 'users', array( 'role' => User::ROLE_MARKETING_SPECIALIST ) );
+
+        // Declare
+        $query = substr( self::CONTACT_NAME, 0, 3 );
+
+        // Get
+        $entries = $this->report->autocomplete_marketing_specialists( $query );
+
+        // Assert
+        $this->assertEquals( self::CONTACT_NAME, $entries[0]['marketing_specialist'] );
+    }
+
+    /**
+     * Test Autocomplete for Companies
+     */
+    public function testAutocompleteCompanies() {
+        // Create
+        $this->phactory->create('companies');
+
+        // Declare
+        $query = substr( self::COMPANY_NAME, 0, 3 );
+
+        // Get
+        $entries = $this->report->autocomplete_companies( $query );
+
+        // Assert
+        $this->assertEquals( self::COMPANY_NAME, $entries[0]['company'] );
+    }
+
+    /**
+     * Test Autocomplete for Company Packages
+     */
+    public function testAutocompleteCompanyPackages() {
+        // Create
+        $this->phactory->create('company_packages');
+
+        // Declare
+        $query = substr( self::COMPANY_PACKAGE_NAME, 0, 3 );
+
+        // Get
+        $entries = $this->report->autocomplete_company_packages( $query );
+
+        // Assert
+        $this->assertEquals( self::COMPANY_PACKAGE_NAME, $entries[0]['package'] );
+    }
 
     /**
      * Will be executed after every test
