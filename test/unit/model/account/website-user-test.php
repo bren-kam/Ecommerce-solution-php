ebbe119ba4112a62550b9dd50864b64fc0d5d5f0
Kerry Jones <kerry@studio98.com>
2014-02-06 11:56:49 -0500

    Added Website User tests

--- test/unit/model/account/website-user-test.php
+++ test/unit/model/account/website-user-test.php
@@ -3,6 +3,9 @@
 require_once 'test/base-database-test.php';
 
 class WebsiteUserTest extends BaseDatabaseTest {
+    const EMAIL = 'test@greysuitretail.com';
+    const BILLING_FIRST_NAME = 'Tommy Dickinson';
+
     /**
      * @var WebsiteUser
      */
@@ -14,183 +17,161 @@ class WebsiteUserTest extends BaseDatabaseTest {
     public function setUp() {
         $_SERVER['MODEL_PATH'] = basename( __DIR__ );
         $this->website_user = new WebsiteUser();
+
+        // Define
+        $this->phactory->define( 'website_users', array( 'website_id' => self::WEBSITE_ID, 'email' => self::EMAIL, 'billing_first_name' => self::BILLING_FIRST_NAME ) );
+        $this->phactory->recall();
     }
+
+
+    /**
+     * Get
+     */
+    public function testGet() {
+        // Create
+        $ph_website_user = $this->phactory->create('website_users');
+
+        // Get
+        $this->website_user->get( $ph_website_user->website_user_id, self::WEBSITE_ID );
+
+        // Assert
+        $this->assertEquals( self::EMAIL, $this->website_user->email );
+    }
+
     /**
      * Test Get
      */
-    public function testReplace() {
-        // Do Stuff
+    public function testGetByEmail() {
+        // Create
+        $this->phactory->create('website_users');
+
+        // Get
+        $this->website_user->get_by_email( self::EMAIL, self::WEBSITE_ID );
+
+        // Assert
+        $this->assertEquals( self::BILLING_FIRST_NAME, $this->website_user->billing_first_name );
     }
-//
-//    /**
-//     * Get
-//     */
-//    public function testGet() {
-//        // Set variables
-//        $website_id = -7;
-//        $email = 'cranky@crank.com';
-//
-//        // Create
-//        $website_user_id = $this->phactory->insert( 'website_users', compact( 'website_id', 'email' ), 'is' );
-//
-//        // Get
-//        $this->website_user->get( $website_user_id, $website_id );
-//
-//        // Make sure we grabbed the right one
-//        $this->assertEquals( $email, $this->website_user->email );
-//
-//        // Clean up
-//        $this->phactory->delete( 'website_users', compact( 'website_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Test Get
-//     */
-//    public function testGetByEmail() {
-//        // Set variables
-//        $website_id = -7;
-//        $email = 'cranky@crank.com';
-//
-//        // Create
-//        $this->phactory->insert( 'website_users', compact( 'website_id', 'email' ), 'is' );
-//
-//        // Get
-//        $this->website_user->get_by_email( $email, $website_id );
-//
-//        // Make sure we grabbed the right one
-//        $this->assertEquals( $email, $this->website_user->email );
-//
-//        // Clean up
-//        $this->phactory->delete( 'website_users', compact( 'website_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Save
-//     *
-//     * @depends testGet
-//     */
-//    public function testSave() {
-//        // Set variables
-//        $website_id = -7;
-//        $email = 'stranger@strange.com';
-//
-//        // Create
-//        $website_user_id = $this->phactory->insert( 'website_users', compact( 'website_id' ), 'i' );
-//
-//        // Get
-//        $this->website_user->get( $website_user_id, $website_id );
-//        $this->website_user->email = $email;
-//        $this->website_user->save();
-//
-//        // Now check it!
-//        $retrieved_email = $this->phactory->get_var( 'SELECT `email` FROM `website_users` WHERE `website_user_id` = ' . (int) $website_user_id );
-//
-//        $this->assertEquals( $retrieved_email, $email );
-//
-//        // Clean up
-//        $this->phactory->delete( 'website_users', compact( 'website_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Set Password
-//     *
-//     * @depends testGet
-//     */
-//    public function testSetPassword() {
-//        // Set variables
-//        $website_id = -7;
-//        $password = '12345';
-//        $md5_password = md5( $password );
-//
-//        // Create
-//        $website_user_id = $this->phactory->insert( 'website_users', compact( 'website_id' ), 'i' );
-//
-//        // Get
-//        $this->website_user->get( $website_user_id, $website_id );
-//        $this->website_user->set_password( $password );
-//
-//        // Now check it!
-//        $retrieved_password = $this->phactory->get_var( 'SELECT `password` FROM `website_users` WHERE `website_user_id` = ' . (int) $website_user_id );
-//
-//        $this->assertEquals( $retrieved_password, $md5_password );
-//
-//        // Clean up
-//        $this->phactory->delete( 'website_users', compact( 'website_id' ), 'i' );
-//    }
-//
-//    /**
-//     * Remove
-//     *
-//     * @depends testGet
-//     */
-//    public function testRemove() {
-//        // Set variables
-//        $website_id = -7;
-//        $email = 'stranger@strange.com';
-//
-//        // Create
-//        $website_user_id = $this->phactory->insert( 'website_users', compact( 'website_id', 'email' ), 'is' );
-//
-//        // Get
-//        $this->website_user->get( $website_user_id, $website_id );
-//
-//        // Remove/Delete
-//        $this->website_user->remove();
-//
-//        $retrieved_email = $this->phactory->get_var( 'SELECT `email` FROM `website_users` WHERE `website_user_id` = ' . (int) $website_user_id );
-//
-//        $this->assertFalse( $retrieved_email );
-//    }
-//
-//    /**
-//     * List All
-//     */
-//    public function testListAll() {
-//        $user = new User();
-//        $user->get_by_email('test@greysuitretail.com');
-//
-//        // Determine length
-//        $_GET['iDisplayLength'] = 30;
-//        $_GET['iSortingCols'] = 1;
-//        $_GET['iSortCol_0'] = 1;
-//        $_GET['sSortDir_0'] = 'asc';
-//
-//        $dt = new DataTableResponse( $user );
-//        $dt->order_by( '`email`', '`billing_first_name`', '`date_registered`' );
-//
-//        $website_users = $this->website_user->list_all( $dt->get_variables() );
-//
-//        // Make sure we have an array
-//        $this->assertTrue( current( $website_users ) instanceof WebsiteUser );
-//
-//        // Get rid of everything
-//        unset( $user, $_GET, $dt, $emails );
-//    }
-//
-//    /**
-//     * Count All
-//     */
-//    public function testCountAll() {
-//        $user = new User();
-//        $user->get_by_email('test@greysuitretail.com');
-//
-//        // Determine length
-//        $_GET['iDisplayLength'] = 30;
-//        $_GET['iSortingCols'] = 1;
-//        $_GET['iSortCol_0'] = 1;
-//        $_GET['sSortDir_0'] = 'asc';
-//
-//        $dt = new DataTableResponse( $user );
-//        $dt->order_by( '`email`', '`billing_first_name`', '`date_registered`' );
-//
-//        $count = $this->website_user->count_all( $dt->get_count_variables() );
-//
-//        // Make sure they exist
-//        $this->assertGreaterThan( 0, $count );
-//
-//        // Get rid of everything
-//        unset( $user, $_GET, $dt, $count );
-//    }
+
+    /**
+     * Save
+     */
+    public function testSave() {
+        // Create
+        $ph_website_user = $this->phactory->create('website_users');
+
+        // Get
+        $this->website_user->id = $ph_website_user->website_user_id;
+        $this->website_user->email = 'sweet@greysuitretail.com';
+        $this->website_user->save();
+
+        // Get
+        $ph_website_user = $this->phactory->get( 'website_users', array( 'website_user_id' => $ph_website_user->website_user_id ) );
+
+        // Assert
+        $this->assertEquals( $this->website_user->email, $ph_website_user->email );
+    }
+
+    /**
+     * Set Password
+     */
+    public function testSetPassword() {
+        // Declare
+        $password = '12345';
+        $expected_password = md5( $password );
+
+        // Create
+        $ph_website_user = $this->phactory->create('website_users');
+
+        // Get
+        $this->website_user->id = $ph_website_user->website_user_id;
+        $this->website_user->set_password( $password );
+
+        // Get
+        $ph_website_user = $this->phactory->get( 'website_users', array( 'website_user_id' => $ph_website_user->website_user_id ) );
+
+        // Assert
+        $this->assertEquals( $expected_password, $ph_website_user->password );
+    }
+
+    /**
+     * Remove
+     *
+     * @depends testGet
+     */
+    public function testRemove() {
+        // Create
+        $ph_website_user = $this->phactory->create('website_users');
+
+        // Delete
+        $this->website_user->id = $ph_website_user->website_user_id;
+        $this->website_user->remove();
+
+        // Get
+        $ph_website_user = $this->phactory->get( 'website_users', array( 'website_user_id' => $ph_website_user->website_user_id ) );
+
+        // Assert
+        $this->assertNull( $ph_website_user );
+    }
+
+    /**
+     * List All
+     */
+    public function testListAll() {
+        // Stub
+        $stub_user = $this->getMock('User');
+
+        // Create
+        $this->phactory->create('website_users');
+
+        // Determine length
+        $_GET['iDisplayLength'] = 30;
+        $_GET['iSortingCols'] = 1;
+        $_GET['iSortCol_0'] = 1;
+        $_GET['sSortDir_0'] = 'asc';
+
+        $dt = new DataTableResponse( $stub_user );
+        $dt->order_by( '`email`', '`billing_first_name`', '`date_registered`' );
+
+        // Get
+        $website_users = $this->website_user->list_all( $dt->get_variables() );
+        $website_user = current( $website_users );
+
+        // Assert
+        $this->assertContainsOnlyInstancesOf( 'WebsiteUser', $website_users );
+        $this->assertEquals( self::EMAIL, $website_user->email );
+
+        // Get rid of everything
+        unset( $user, $_GET, $dt, $emails );
+    }
+
+    /**
+     * Count All
+     */
+    public function testCountAll() {
+        // Stub
+        $stub_user = $this->getMock('User');
+
+        // Create
+        $this->phactory->create('website_users');
+
+        // Determine length
+        $_GET['iDisplayLength'] = 30;
+        $_GET['iSortingCols'] = 1;
+        $_GET['iSortCol_0'] = 1;
+        $_GET['sSortDir_0'] = 'asc';
+
+        $dt = new DataTableResponse( $stub_user );
+        $dt->order_by( '`email`', '`billing_first_name`', '`date_registered`' );
+
+        // Get
+        $count = $this->website_user->count_all( $dt->get_count_variables() );
+
+        // Assert
+        $this->assertGreaterThan( 0, $count );
+
+        // Get rid of everything
+        unset( $user, $_GET, $dt, $count );
+    }
 
     /**
      * Will be executed after every test
