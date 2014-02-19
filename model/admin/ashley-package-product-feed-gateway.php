bc06f772cb27ed38ee4a4bc669ea7a26796f1751
Kerry Jones <kerry@studio98.com>
2014-02-18 19:09:48 -0600

    Changed all new Ashley Products to come in as public products

--- model/admin/ashley-package-product-feed-gateway.php
+++ model/admin/ashley-package-product-feed-gateway.php
@@ -517,7 +517,7 @@ class AshleyPackageProductFeedGateway extends ProductFeedGateway {
             // Change publish visibility to private if there are no images
             if ( 0 == count( $images ) && 'private' != $product->publish_visibility ) {
                 $this->not_identical[] = 'publish_visibility';
-                $product->publish_visibility = 'private';
+                $product->publish_visibility = 'public';
             }
 
             /***** SKIP PRODUCT IF IDENTICAL *****/
