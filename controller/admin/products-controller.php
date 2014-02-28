0151392310f626107cdc61f91baa693e7c261d26
Gabriel Brunacci <gbrunacci@gmail.com>
2014-02-28 14:57:35 -0300

    GSRA-126 price regex and upload images on create

--- controller/admin/products-controller.php
+++ controller/admin/products-controller.php
@@ -818,29 +818,36 @@ ProductsController extends BaseController {
                 $valid = false;
             }
 
-            if ( !regexp::match( $r['image'], 'url' ) ) {
-                $r['reason'] = (isset( $r['reason'] ) ? $r['reason'] : '') . "Bad image URL. ";
-                $valid = false;
-            }
-
             if ( !$valid ) {
                 $skipped_products[] = $r;
                 continue;
             }
 
-            // check if remote file exists
-            $file_exists = curl::check_file( $r['image'] );
-            if ( !$file_exists ) {
-                $r['reason'] = (isset( $r['reason'] ) ? $r['reason'] : '') . "Image not found.";
-                $skipped_products[] = $r;
-                continue;
+            // see if the product exists
+            $matching_product = new Product();
+            $matching_product->get_by_sku_by_brand( $r['sku'], $brand_id );
+            // we will only load images for new products
+            if ( !$matching_product->id ) {
+                if ( !regexp::match( $r['image'], 'url' ) ) {
+                    $r['reason'] = (isset( $r['reason'] ) ? $r['reason'] : '') . "Bad image URL. ";
+                    $valid = false;
+                }
+
+                // check if remote file exists
+                $file_exists = curl::check_file( $r['image'] );
+                if ( !$file_exists ) {
+                    $r['reason'] = (isset( $r['reason'] ) ? $r['reason'] : '') . "Image not found.";
+                    $skipped_products[] = $r;
+                    continue;
+                }                
             }
 
             $product = array_slice($r, 0, 9);
-            $product['price_map'] = (float)$r['price_map'];
-            $product['price_map'] = $r['price_map'] ? $r['price_map'] : 0;
-            $product['price_wholesale'] = (float)$r['price_wholesale'];
-            $product['price_wholesale'] = $r['price_wholesale'] ? $r['price_wholesale'] : 0;
+            
+            $product['price_map'] = (float) preg_replace( '/[^0-9.]/', '', $r['price_map']);
+            $product['price_map'] = $product['price_map'] ? $product['price_map'] : 0;
+            $product['price_wholesale'] = (float) preg_replace( '/[^0-9.]/', '', $r['price_wholesale']);
+            $product['price_wholesale'] = $product['price_wholesale'] ? $product['price_wholesale'] : 0;
             $product['category_id'] = $category_id;
             $product['industry_id'] = $industry_id;
             $product['brand_id'] = $brand_id;
@@ -946,12 +953,20 @@ ProductsController extends BaseController {
             $product->user_id_modified = $this->user->id;
             $product->weight = 0;
             
+            // a new product?
             if ( $product->id == null ) {
-                $product->publish_visibility = 'public';
-                $product->publish_date = date( 'Y-m-d H:i:s' );
                 $product->user_id_created = $this->user->id;
 
                 $product->create();
+
+                $product->publish_visibility = 'public';
+                $product->publish_date = date( 'Y-m-d H:i:s' );
+
+                // we only upload images if its a new product
+                $slug = f::strip_extension( f::name( $p->image ) );
+                $industry = format::slug( $p->industry_name );
+                $image_name = $product->upload_image( $p->image, $slug, $industry );
+                $product->add_images( array( $image_name ) );
             }
 
             $product->save();
@@ -961,12 +976,6 @@ ProductsController extends BaseController {
             if ( $product_specifications ) {
                 $product->add_specifications($product_specifications);
             }
-
-            $slug = f::strip_extension( f::name( $p->image ) );
-            $industry = format::slug( $p->industry_name );
-
-            $image_name = $product->upload_image( $p->image, $slug, $industry );
-            $product->add_images( array( $image_name ) );
 
         }
         $product_import->delete_all();
