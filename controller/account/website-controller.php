7efea629d63622696a9cb562112bb7e27247968b
Gabriel Brunacci <gbrunacci@gmail.com>
2014-03-27 15:18:49 -0300

    Fixed disable fadeout option

--- controller/account/website-controller.php
+++ controller/account/website-controller.php
@@ -621,7 +621,7 @@ class WebsiteController extends BaseController {
         // Get settings
         $settings_array = array(
             'banner-width', 'banner-height', 'banner-speed', 'banner-background-color'
-            , 'banner-effect', 'banner-hide-scroller', 'sidebar-image-width', 'timezone', 'images-alt'
+            , 'banner-effect', 'banner-hide-scroller', 'disable-banner-fade-out', 'sidebar-image-width', 'timezone', 'images-alt'
             , 'sm-facebook-link', 'sm-twitter-link', 'sm-google-link', 'sm-pinterest-link', 'sm-linkedin-link'
         );
         $settings = $this->user->account->get_settings( $settings_array );
