1cbc80ed1f2e891885dbc07396e24dad0c942759
Gabriel Brunacci <gbrunacci@gmail.com>
2014-05-21 16:22:46 -0300

    GSRA-184 checkbox fix in OSX

--- view/account/email-marketing/campaigns/_step2.php
+++ view/account/email-marketing/campaigns/_step2.php
@@ -59,8 +59,10 @@
             </ul>
         </div>
         <div id="email-settings" class="tab-content">
-            <input type="checkbox" class="cb" name="no-template" id="no-template" value="1" <?php if ( !$campaign->email_template_id ) echo 'checked="checked"' ?>>
-            <label for="no-template">Remove Header/Footer</label>
+            <label for="no-template">
+                <input type="checkbox" class="cb" name="no-template" id="no-template" value="1" <?php if ( !$campaign->email_template_id ) echo 'checked="checked"' ?>>
+                Remove Header/Footer
+            </label>
         </div>
     </div><!-- .email-marketing-right -->
 
