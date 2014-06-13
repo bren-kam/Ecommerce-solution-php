152c1c0adeca7e8d5f88acb05990874ba045abab
Gabriel Brunacci <gbrunacci@gmail.com>
2014-06-13 18:24:02 -0300

    Analytics date range start date

--- view/account/js/analytics/analytics.js
+++ view/account/js/analytics/analytics.js
@@ -23,7 +23,7 @@ jQuery(function($) {
     head.load( 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js', function() {
         // Date Picker
         $('#tDateStart').datepicker({
-            maxDate: -2
+            maxDate: -3
             , dateFormat: 'M d, yy'
             , altFormat: 'yy-mm-dd'
             , onSelect: function( dateText, dp ) {
