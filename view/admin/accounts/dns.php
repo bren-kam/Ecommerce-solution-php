72088549129fd38e93b7de41c0c39b80e141125f
Gabriel Brunacci <gbrunacci@gmail.com>
2014-07-02 22:37:35 -0300

    DNS

---
+++ view/admin/accounts/dns.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * @package Grey Suit Retail
+ * @page Edit Account > DNS
+ *
+ * Declare the variables we have available from other sources
+ * @var Resources $resources
+ * @var Template $template
+ * @var User $user
+ * @var Account $account
+ * @var int $zone_id
+ * @var string $errs
+ * @var string $domain_name,
+ * @var string $full_domain_name,
+ * @var array $records
+ */
+
+?>
+
+<div class="row-fluid">
+    <div class="col-lg-12">
+        <section class="panel">
+
+            <header class="panel-heading">
+                <ul class="nav nav-tabs tab-bg-dark-navy-blue" role="tablist">
+                    <li><a href="/accounts/edit/?aid=<?php echo $account->id ?>">Account</a></li>
+                    <li><a href="/accounts/website-settings/?aid=<?php echo $account->id ?>">Website</a></li>
+                    <li><a href="/accounts/other-settings/?aid=<?php echo $account->id ?>">Other</a></li>
+                    <li><a href="/accounts/actions/?aid=<?php echo $account->id ?>">Actions</a></li>
+
+                    <?php if ( $account->craigslist ): ?>
+                        <div class="tab-link"><a href="/accounts/craigslist/?aid=<?php echo $account->id; ?>" title="<?php echo _('Craigslist'); ?>"><?php echo _('Craigslist'); ?></a></div>
+                    <?php endif; ?>
+
+                    <?php if ( $user->has_permission( User::ROLE_SUPER_ADMIN ) ): ?>
+                        <li class="active"><a href="/accounts/dns/?aid=<?php echo $account->id ?>">DNS</a></li>
+                    <?php endif; ?>
+                </ul>
+                <h3>DNS: <?php echo $account->title ?></h3>
+            </header>
+
+            <div class="panel-body">
+
+
+<?php if ( $errs ): ?>
+    <div class="alert alert-danger"><?php echo $errs ?></div>
+<?php endif; ?>
+
+<?php if ( !empty( $zone_id ) ): ?>
+
+    <h4>Records</h4>
+
+    <form name="fEditDNS" id="fEditDNS" action="" method="post" err="<?php echo 'The records you have entered do not match the type you have selected.'; ?>" role="form">
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th><?php echo _('Name'); ?></th>
+                    <th><?php echo _('Type'); ?></th>
+                    <th><?php echo _('TTL'); ?></th>
+                    <th><?php echo _('Records'); ?></th>
+                    <th>&nbsp;</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            if ( is_array( $records['ResourceRecordSets'] ) )
+                foreach ( $records['ResourceRecordSets'] as $r ):
+                    $no_delete =  $full_domain_name == $r['Name'] && ( 'NS' == $r['Type'] || 'SOA' == $r['Type'] );
+                    ?>
+                    <tr>
+                        <td>
+                            <?php if ( $no_delete ) { echo $r['Name']; } else { ?>
+                                <input type="text" name="changes[name][]" class="form-control disabled" disabled="disabled" placeholder="<?php echo _('Domain'); ?>" value="<?php echo $r['Name']; ?>" />
+                                <input class="action disabled" type="hidden" disabled="disabled" name="changes[action][]" value="" />
+                            <?php } ?>
+                        </td>
+                        <td>
+                            <?php if ( $no_delete ) { echo $r['Type']; } else { ?>
+                                <select name="changes[type][]" class="form-control disabled changes-type" disabled="disabled">
+                                    <?php
+                                    $types = array( 'A', 'CNAME', 'MX', 'NS', 'TXT' );
+                                    foreach ( $types as $type ) {
+                                        $selected = ( $type == $r['Type'] ) ? ' selected="Selected"' : '';
+                                        ?>
+                                        <option value="<?php echo $type; ?>"<?php echo $selected; ?>><?php echo $type; ?></option>
+                                    <?php } ?>
+                                </select>
+                            <?php } ?>
+                        </td>
+                        <td>
+                            <?php if ( $no_delete ) { echo $r['TTL']; } else { ?>
+                                <input type="text" name="changes[ttl][]" class="form-control disabled" placeholder="TTL" value="<?php echo $r['TTL']; ?>" disabled="disabled" />
+                            <?php } ?>
+                        </td>
+                        <td>
+                            <?php if ( $no_delete ) { echo implode( "<br />\n", preg_replace( '/\.$/', '', $r['ResourceRecords'] ) ); } else { ?>
+                                <textarea name="changes[records][]" class="form-control disabled changes-records" cols="40" rows="3" placeholder="<?php echo _('Records - 1 per line'); ?>" disabled="disabled"><?php echo implode( "\n", $r['ResourceRecords'] ); ?></textarea>
+                            <?php } ?>
+                        </td>
+                        <td>
+                            <?php if ( !$no_delete ) { ?>
+                                <a href="#" class="edit-record" title="<?php echo _('Edit Record'); ?>"><i class="fa fa-pencil"></i></a>
+                                <a href="#" class="delete-record" title="<?php echo _('Delete Record'); ?>"><i class="fa fa-trash-o"></i></a>
+                            <?php } ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <p><a href="#" id="aAddRecord" class="btn btn-default" title="<?php echo _('Add Record'); ?>"><?php echo _('Add Record'); ?></a></p>
+
+        <p>
+            <a href="<?php echo url::add_query_arg( 'a', 'delete' ); ?>" class="btn btn-warning" title="<?php echo _('Delete Zone File'); ?>" confirm="Delete zone file?"><?php echo _('Delete Zone File'); ?></a>
+            <button type="submit" class="btn btn-primary pull-right">Save DNS</button>
+        </p>
+
+        <?php nonce::field('dns'); ?>
+    </form>
+
+<?php else: ?>
+
+    <p>No zone file found.</p>
+    <a href="<?php echo url::add_query_arg('a', 'create'); ?>" class="btn btn-primary btn-lg" title="<?php echo _('Create Zone File'); ?>"><?php echo _('Create Zone File'); ?></a>
+
+<?php endif; ?>
+
+
+
+
+</div>
+        </section>
+    </div>
+</div>
+
+
---
+++ view/admin/js/accounts/dns.js
@@ -0,0 +1,109 @@
+var DNS = {
+    _template: null
+
+    , init: function() {
+        // Generate row template
+        DNS._template = $('.edit-record:first').parents('tr').clone();
+        DNS._template.find('input').removeAttr('value');
+        DNS._template.find('select').prop('selectedIndex', 0);
+        DNS._template.find('textarea').text('');
+        DNS._template.find('*').removeClass('disabled').removeAttr('disabled');
+
+        $(document).on( 'click', '.edit-record', DNS.editRecord );
+        $(document).on( 'click', '.delete-record', DNS.deleteRecord );
+        $('#aAddRecord').click( DNS.addRecord );
+        $('#fEditDNS').submit( DNS.submit );
+    }
+
+    , editRecord: function(e) {
+        e.preventDefault();
+
+        var row = $(this).parents('tr:first');
+        var new_row = row.clone();
+
+        // current row action=0 (delete)
+        row.find('*').removeClass('disabled').removeAttr('disabled');
+        row.find('.action').val('0');
+
+        // new row action=1 (add)
+        new_row.find('*').removeClass('disabled').removeAttr('disabled');
+        new_row.find('.action').val('1');
+
+        // show
+        row.after(new_row);
+        row.hide();
+    }
+
+    , deleteRecord: function(e) {
+        e.preventDefault();
+
+        var row = $(this).parents('tr:first');
+
+        // current row action=0 (delete)
+        row.find('*').removeClass('disabled').removeAttr('disabled');
+        row.find('.action').val('0');
+
+        row.hide();
+    }
+
+    , addRecord: function(e) {
+        e.preventDefault();
+        var row = DNS._template.clone();
+
+        // new row action=1 (add)
+        row.find('.action').val('1');
+
+        // show
+        $('#fEditDNS tbody').append(row);
+    }
+
+    , submit: function(e) {
+        var form = $(this);
+
+        form.find('.changes-type:visible').each( function() {
+            var changeType = $(this).val()
+            var record_input = $(this).parents('tr:first').find('.changes-records:first')
+            var records = record_input.val().split("\n");
+
+            if ( !DNS.validateType( changeType, records ) ) {
+                alert('The records you have entered do not match the type you have selected.');
+                record_input.focus();
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    , validateType: function( changeType, records ) {
+        switch ( changeType ) {
+            case 'A':
+                // Check for IPs
+                var regex = /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/;
+                break;
+
+            case 'CNAME':
+                // Check domains
+                var regex = /^(?:[-a-zA-Z0-9]+\.)*([-a-zA-Z0-9]+\.[a-zA-Z]{2,3}){1,2}$/;
+                break;
+
+            default:
+                return true;
+                break;
+        }
+
+        for ( var i in records ) {
+            var charPos = records[i].match( regex );
+
+            if ( null == charPos )
+                return false;
+        }
+
+        return true;
+    }
+
+}
+
+jQuery(function(){
+    DNS.init();
+});
