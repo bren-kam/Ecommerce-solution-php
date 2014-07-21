0cc3595fed46920753a98a835569afe8b5ed5220
Gabriel Brunacci <gbrunacci@gmail.com>
2014-07-21 18:26:17 -0300

    GSR Header logo

--- lib/labels/greysuitretail.com.php
+++ lib/labels/greysuitretail.com.php
@@ -9,6 +9,6 @@
  */
 
 define( 'TITLE', 'Grey Suit Retail' );
-define( 'LOGO_WIDTH', 384 );
-define( 'LOGO_HEIGHT', 58 );
+define( 'LOGO_WIDTH', 29 );
+define( 'LOGO_HEIGHT', 33 );
 define( 'COMPANY_ID', 4 );
