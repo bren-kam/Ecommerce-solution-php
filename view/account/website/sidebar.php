048a047295b731c8d40cd42fd3b687be102b0024
Gabriel Brunacci <gbrunacci@gmail.com>
2014-07-31 18:01:52 -0300

    Hidden footer navigation and html head settings for old theme sites

--- view/account/website/sidebar.php
+++ view/account/website/sidebar.php
@@ -32,9 +32,11 @@ $industries = $user->account->get_industries();
         <a href="/website/settings/" title="<?php echo _('Settings'); ?>" class="sub<?php $template->select('page-settings'); ?>"><?php echo _('Settings'); ?></a>
         <a href="/website/home-page-layout/" title="<?php echo _('Home Page Layout'); ?>" class="sub<?php $template->select('home-page-layout'); ?>"><?php echo _('Home Page Layout'); ?></a>
         <a href="/website/navigation/" title="<?php echo _('Header Navigation'); ?>" class="sub <?php $template->select('sidebar-navigation'); ?>"><?php echo _('Header Navigation'); ?></a>
-        <a href="/website/footer-navigation/" title="<?php echo _('Footer Navigation'); ?>" class="sub <?php $template->select('footer-navigation'); ?>"><?php echo _('Footer Navigation'); ?></a>
+        <?php if ( $user->account->is_new_template() ) { ?>
+            <a href="/website/footer-navigation/" title="<?php echo _('Footer Navigation'); ?>" class="sub <?php $template->select('footer-navigation'); ?>"><?php echo _('Footer Navigation'); ?></a>
+        <?php } ?>
         <a href="/website/header/" title="<?php echo _('Header'); ?>" class="sub <?php $template->select('website-header'); ?>"><?php echo _('Website Header'); ?></a>
-        <?php if ( $user->has_permission( User::ROLE_ONLINE_SPECIALIST ) ) { ?>
+        <?php if ( $user->account->is_new_template() && $user->has_permission( User::ROLE_ONLINE_SPECIALIST ) ) { ?>
             <a href="/website/html-header/" title="<?php echo _('HTML &lt;head&gt;'); ?>" class="sub <?php $template->select('html-header'); ?>"><?php echo _('HTML &lt;head&gt;'); ?></a>
         <?php } ?>
 
