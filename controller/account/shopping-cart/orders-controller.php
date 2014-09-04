1f01ac39d3ec86729e035e5d2e9b82b807a565bf
Gabriel Brunacci <gbrunacci@gmail.com>
2014-09-04 18:04:15 -0300

    Order List - Ashley Express non clickable

--- controller/account/shopping-cart/orders-controller.php
+++ controller/account/shopping-cart/orders-controller.php
@@ -111,13 +111,13 @@ class OrdersController extends BaseController {
 
             $date = new DateTime( $order->date_created );
 
-            $link_text = $order->id;
+            $link_text = '';
             if ( $order->website_shipping_method_id == WebsiteOrder::get_ashley_express_shipping_method()->id ) {
-                $link_text .= " Ashley Express";
+                $link_text = " - Ashley Express";
             }
 
             $data[] = array(
-                '<a href="' . url::add_query_arg( 'woid', $order->id, '/shopping-cart/orders/view/' ) . '" title="' . _('View') . '">' . $link_text . '</a>'
+                '<a href="' . url::add_query_arg( 'woid', $order->id, '/shopping-cart/orders/view/' ) . '" title="' . _('View') . '">' . $order->id . '</a>' . $link_text
                 , $order->name
                 , '$' . number_format( $order->total_cost, 2 )
                 , $status
