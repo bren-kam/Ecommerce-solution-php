6c0da86c270f988abb21921f5d7cd7662c03715c
Gabriel Brunacci <gbrunacci@gmail.com>
2014-09-09 15:29:33 -0300

    Fixed bad setInterval call

--- view/admin/js/tickets/index.js
+++ view/admin/js/tickets/index.js
@@ -24,7 +24,7 @@ var TicketList = {
         });
 
         // Refresh the page every 5 minutes
-        setInterval( TicketList.reloadDataTable(), 300000 );
+        setInterval( TicketList.reloadDataTable, 300000 );
     }
 
     , reloadDataTable: function() {
