6401fc312c39e7ce15e6228035b7e4a1da56848e
Gabriel Brunacci <gbrunacci@gmail.com>
2014-10-15 16:01:36 -0300

    CKEditor in modal fix

--- view/account/js/email-marketing/campaigns/create.js
+++ view/account/js/email-marketing/campaigns/create.js
@@ -382,6 +382,22 @@ var CampaignForm = {
         CampaignForm.initStep1();
         CampaignForm.initStep2();
         CampaignForm.initStep3();
+
+        // bootstrap-ckeditor-modal-fix.js
+        // hack to fix ckeditor/bootstrap compatiability bug when ckeditor appears in a bootstrap modal dialog
+        //
+        // Include this AFTER both bootstrap and ckeditor are loaded.
+
+        $.fn.modal.Constructor.prototype.enforceFocus = function() {
+            modal_this = this
+            $(document).on('focusin.modal', function (e) {
+                if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
+                    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
+                    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
+                    modal_this.$element.focus()
+                }
+            })
+        };
     }
 
     , initStep1: function() {
