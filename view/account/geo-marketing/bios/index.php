7ddcc86454b2eecf4a865c30cccfb3ec580159a8
Gabriel Brunacci <gbrunacci@gmail.com>
2014-12-05 16:37:38 -0300

    GSRA-228

---
+++ view/account/geo-marketing/bios/index.php
@@ -0,0 +1,23 @@
+<div class="row-fluid">
+    <div class="col-lg-12">
+        <section class="panel">
+            <header class="panel-heading">
+                Bios
+                <a href="/geo-marketing/bios/add-edit/" class="btn btn-primary btn-sm pull-right"l><i class="fa fa-plus"></i> <span class="hidden-xs">New Bio</span></a>
+            </header>
+
+            <div class="panel-body">
+
+                <div class="adv-table">
+                    <table class="display table table-bordered table-striped" ajax="/geo-marketing/bios/list-all/" perPage="30,50,100">
+                        <thead>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Last Updated</th>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
---
+++ view/account/geo-marketing/listings/index.php
@@ -0,0 +1,40 @@
+<?php
+    nonce::field('store_session', '_store_session');
+?>
+<div class="row-fluid">
+    <div class="col-lg-12">
+        <section class="panel">
+            <header class="panel-heading">
+                Listings
+            </header>
+
+            <div class="panel-body">
+
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label for="location-id">Location:</label>
+                            <select class="form-control" id="location-id">
+                                <option>All Locations</option>
+                                <?php foreach ( $locations as $location ): ?>
+                                    <option value="<?php echo $location->yext_id ?>"><?php echo $locatio->yext_id ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="adv-table">
+                    <table class="display table table-bordered table-striped" ajax="/geo-marketing/listings/list-all/" perPage="30,50,100">
+                        <thead>
+                            <th>Location</th>
+                            <th>Site</th>
+                            <th>Status</th>
+                            <th>URL</th>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
