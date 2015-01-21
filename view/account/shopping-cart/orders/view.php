9e2c78a684d5643ab414883b985906132dffbcaf
Kerry Jones <kerry@studio98.com>
2015-01-20 17:03:08 -0800

    Updated text alignment

--- view/account/shopping-cart/orders/view.php
+++ view/account/shopping-cart/orders/view.php
@@ -138,7 +138,7 @@
 
             <div class="panel-body">
                 <div class="row">
-                    <div class="col-lg-2"><strong>Image</strong></div>
+                    <div class="col-lg-2 text-center"><strong>Image</strong></div>
                     <div class="col-lg-7"><strong>Description</strong></div>
                     <div class="col-lg-1 text-right"><strong>Price</strong></div>
                     <div class="col-lg-1 text-right"><strong>Quantity</strong></div>
