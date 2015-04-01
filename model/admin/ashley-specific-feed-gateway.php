675943f44d535a7d22e8b9ee76c51f678e31089d
Gabriel Brunacci <gbrunacci@gmail.com>
2015-03-31 21:08:28 -0300

    Normal Users can't create pages

--- model/admin/ashley-specific-feed-gateway.php
+++ model/admin/ashley-specific-feed-gateway.php
@@ -1550,9 +1550,17 @@ class AshleySpecificFeedGateway extends ActiveRecordBase {
                 }
             }
 
+            // Carton (Set of X)
             $is_carton = strpos($product->name, '(Set of ');
             if ( $is_carton !== false ) {
                 $multiplier = (int) substr($product->name, $is_carton + 8, 1);
+                $item['price'] *= $multiplier;
+            }
+
+            // Carton (X/CN)
+            $is_carton = strpos($product->name, '/CN)');
+            if ( $is_carton !== false ) {
+                $multiplier = (int) substr($product->name, $is_carton - 1, 1);
                 $item['price'] *= $multiplier;
             }
 
