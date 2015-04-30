a70d1652c0cdc1c4a5ce78c8d1edad77267c4c46
Gabriel Brunacci <gbrunacci@gmail.com>
2015-04-30 16:58:25 -0300

    remarketing report - made label column larger

--- view/account/shopping-cart/remarketing/index.php
+++ view/account/shopping-cart/remarketing/index.php
@@ -11,20 +11,20 @@
                     <h4>Overview Since <?php echo $since->format('F jS, Y h:ia') ?></h4>
 
                     <div class="row-fluid">
-                        <div class="col-lg-2">
+                        <div class="col-lg-3">
                             <span class="label label-primary"><?php echo $overview['converted_count'] ?> out of <?php echo $overview['total_count'] ?> Carts Converted</span>
                         </div>
-                        <div class="col-lg-10">
+                        <div class="col-lg-9">
                             <div class="progress">
                                 <div class="progress-bar" style="width: <?php echo $overview['converted_count'] / $overview['total_count'] * 100 ?>%"></div>
                             </div>
                         </div>
                     </div>
                     <div class="row-fluid">
-                        <div class="col-lg-2">
+                        <div class="col-lg-3">
                             <span class="label label-success">$<?php echo number_format($overview['converted_amount'], 2) ?> out of $<?php echo number_format($overview['total_amount'], 2) ?> in carts converted</span>
                         </div>
-                        <div class="col-lg-10">
+                        <div class="col-lg-9">
                             <div class="progress">
                                 <div class="progress-bar progress-bar-success" style="width: <?php echo $overview['converted_amount'] / $overview['total_amount'] * 100 ?>%"></div>
                             </div>
