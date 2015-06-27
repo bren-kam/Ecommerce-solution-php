3a8156b2c4b81a6811a9c66b67b29eacc9ed6668
Kerry Jones <kerry@studio98.com>
2015-06-26 18:25:41 -0700

    Created a place to store name and type

--- lib/models/product-option.php
+++ lib/models/product-option.php
@@ -9,4 +9,34 @@ class ProductOption extends ActiveRecordBase {
     public function __construct() {
         parent::__construct( 'product_option' );
     }
+
+    /**
+     * Create
+     */
+    public function create() {
+        $this->id = $this->insert([
+            'website_id' => $this->website_id
+            , 'name' => $this->name
+            , 'type' => $this->type
+        ], 'iss');
+    }
+
+    /**
+     * Add Product Option > Product Relations
+     *
+     * @param array $product_ids
+     * @throws ModelException
+     */
+    public function add_relations( array $product_ids ) {
+        $values = '';
+
+        foreach ( $product_ids as &$product_id ) {
+            if ( !empty( $values ) )
+                $values .= ',';
+
+            $values .= '('  . (int) $this->id  . ', ' . (int) $product_id . ')';
+        }
+
+        $this->query( 'INSERT INTO `product_option_product` ( `product_option_id`, `product_id` ) VALUES ' . $values );
+    }
 }
