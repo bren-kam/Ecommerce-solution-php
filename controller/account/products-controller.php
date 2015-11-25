b511df5781bb476e53d3b4bbeaa3a397ed64a293
Kerry Jones <kerry@studio98.com>
2015-11-25 13:50:54 -0800

    Updated to add the product options parent id

--- controller/account/products-controller.php
+++ controller/account/products-controller.php
@@ -2909,6 +2909,8 @@ class ProductsController extends BaseController {
             $account_products[$account_product->product_id] = $account_product;
         }
 
+        $last_parent_product = null;
+
         foreach ( $products as $p ) {
             $product = new Product();
             $product->get_by_id_by_website( $p->product_id, $this->user->account->id );
@@ -2921,6 +2923,7 @@ class ProductsController extends BaseController {
             if ( 'option' == $p->type ) {
                 preg_match( '/(\[[^\]]+])(.+)/', $p->name, $product_option_matches );
                 $product->name =  $product_option_matches[2];
+                $product->parent_product_id = $last_parent_product->product_id;
             } else {
                 $product->name = $p->name;
             }
@@ -3005,6 +3008,9 @@ class ProductsController extends BaseController {
             }
             $product->add_images( $product_images );
             $product->save();
+
+            if ( 'product' == $p->type )
+                $last_parent_product = $product;
 
             $product_specifications = json_decode( $p->product_specifications, true );
             $product->delete_specifications(); // should I ?
