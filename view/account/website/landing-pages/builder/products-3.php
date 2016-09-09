82d2e2ad781bdc1e9efa8107c5a4943dfd406f88
Juan F. Giménez Silva <juanfgs@gmail.com>
2016-09-09 14:05:07 -0300

    added formatting plugin added link to products-3 images

--- view/account/website/landing-pages/builder/products-3.php
+++ view/account/website/landing-pages/builder/products-3.php
@@ -7,23 +7,34 @@
 		    <div class="product-box">
 			<h3 class="product-title">Product Name</h3>
 			<div class="product-img">
+                      <a href="#" class="product-add-to-cart">
+
 			    <img class="img-responsive" src="http://placehold.it/600x300" />
+               </a>
 			</div>
+               
 		    <p class="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse feugiat eget elit ut scelerisque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean ut nisi felis. </p>
 		    </div>
-		    <div class="add-to-cart">
-            </div>
+		                         <a href="#" class="product-add-to-cart btn-sm btn btn-primary">
+               Buy now
+      </a>
+
 		</div>
 		<div class="col-lg-6 product" data-product="">
 		    <div class="product-box">
 			<h3 class="product-title">Product Name</h3>
 			<div class="product-img">
-			    <img  class="img-responsive" src="http://placehold.it/600x300" />
+			            <a href="#" class="product-add-to-cart">
+
+					<img  class="img-responsive" src="http://placehold.it/600x300" />
+				    </a>
 			</div>
 		    <p class="product-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse feugiat eget elit ut scelerisque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean ut nisi felis. </p>
 		    </div>
-      <div class="add-to-cart">
-      </div>
+		             <a href="#" class="product-add-to-cart btn-sm btn btn-primary">
+               Buy now
+      </a>
+
 
 	    
 		</div>
