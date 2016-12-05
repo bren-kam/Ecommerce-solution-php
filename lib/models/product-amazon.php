bcec044b4052c08ec685a803bb7749728a280b6f
Deleu <deleugyn@gmail.com>
2016-12-05 14:52:38 -0200

    Amazon product now triggers insert for child products

--- lib/models/product-amazon.php
+++ lib/models/product-amazon.php
@@ -18,6 +18,8 @@ class ProductAmazon extends ActiveRecordBase {
      * @return int $product_amazon_id
      */
     public function create() {
+        $sql = 'INSERT INTO products_amazon (product_id) SELECT product_id FROM products WHERE parent_product_id = :product_id ON DUPLICATE KEY UPDATE product_id = product_id';
+        $this->prepare($sql, 'i', array( ':product_id' => $this->product_id ));
         return $this->insert(
             ['product_id' => $this->product_id]
             , 'i'
